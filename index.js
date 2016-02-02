95cea9230b88feae1f0a6cb51686af7522de0193
Taylor Stine <taylorstine@gmail.com>
2016-02-02 07:51:19 -0500

    Added detail comment to the ParseServer constructor for cloud function

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ addParseCloud();
 //                 and delete
 // "databaseURI": a uri like mongodb://localhost:27017/dbname to tell us
 //          what database this Parse API connects to.
-// "cloud": relative location to cloud code to require
+// "cloud": relative location to cloud code to require, or a function
+//          that is given an instance of Parse as a parameter.  Use this instance of Parse
+//          to register your cloud code hooks and functions.
 // "appId": the application id to host
 // "masterKey": the master key for requests to this app
 // "facebookAppIds": an array of valid Facebook Application IDs, required
