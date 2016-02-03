33a881764dababc71c24a53cc4e00802014effee
Drew Gross <drew.a.gross@gmail.com>
2016-02-03 12:10:50 -0800

    Tabs -> spaces

--- schemas.js
+++ schemas.js
@@ -6,62 +6,62 @@ var express = require('express'),
 var router = new PromiseRouter();
 
 function mongoFieldTypeToApiResponseType(type) {
-	if (type[0] === '*') {
-		return {
-			type: 'Pointer',
-			targetClass: type.slice(1),
-		};
-	}
-	if (type.startsWith('relation<')) {
-		return {
-			type: 'Relation',
-			targetClass: type.slice('relation<'.length, type.length - 1),
-		};
-	}
-	switch (type) {
-		case 'number':   return {type: 'Number'};
-		case 'string':   return {type: 'String'};
-		case 'boolean':  return {type: 'Boolean'};
-		case 'date':     return {type: 'Date'};
-		case 'object':   return {type: 'Object'};
-		case 'array':    return {type: 'Array'};
-		case 'geopoint': return {type: 'GeoPoint'};
-		case 'file':     return {type: 'File'};
-	}
+  if (type[0] === '*') {
+    return {
+      type: 'Pointer',
+      targetClass: type.slice(1),
+    };
+  }
+  if (type.startsWith('relation<')) {
+    return {
+      type: 'Relation',
+      targetClass: type.slice('relation<'.length, type.length - 1),
+    };
+  }
+  switch (type) {
+    case 'number':   return {type: 'Number'};
+    case 'string':   return {type: 'String'};
+    case 'boolean':  return {type: 'Boolean'};
+    case 'date':     return {type: 'Date'};
+    case 'object':   return {type: 'Object'};
+    case 'array':    return {type: 'Array'};
+    case 'geopoint': return {type: 'GeoPoint'};
+    case 'file':     return {type: 'File'};
+  }
 }
 
 function mongoSchemaAPIResponseFields(schema) {
-	fieldNames = Object.keys(schema).filter(key => key !== '_id');
-	response = {};
-	fieldNames.forEach(fieldName => {
-		response[fieldName] = mongoFieldTypeToApiResponseType(schema[fieldName]);
-	});
-	response.ACL = {type: 'ACL'};
-	response.createdAt = {type: 'Date'};
-	response.updatedAt = {type: 'Date'};
-	response.objectId = {type: 'String'};
-	return response;
+  fieldNames = Object.keys(schema).filter(key => key !== '_id');
+  response = {};
+  fieldNames.forEach(fieldName => {
+    response[fieldName] = mongoFieldTypeToApiResponseType(schema[fieldName]);
+  });
+  response.ACL = {type: 'ACL'};
+  response.createdAt = {type: 'Date'};
+  response.updatedAt = {type: 'Date'};
+  response.objectId = {type: 'String'};
+  return response;
 }
 
 function mongoSchemaToSchemaAPIResponse(schema) {
-	return {
-		className: schema._id,
-		fields: mongoSchemaAPIResponseFields(schema),
-	};
+  return {
+    className: schema._id,
+    fields: mongoSchemaAPIResponseFields(schema),
+  };
 }
 
 function getAllSchemas(req) {
-	if (!req.auth.isMaster) {
-		return Promise.resolve({
-			status: 401,
-			response: {error: 'unauthorized'},
-		});
-	}
-	return req.config.database.collection('_SCHEMA')
-	.then(coll => coll.find({}).toArray())
-	.then(schemas => ({response: {
-		results: schemas.map(mongoSchemaToSchemaAPIResponse)
-	}}));
+  if (!req.auth.isMaster) {
+    return Promise.resolve({
+      status: 401,
+      response: {error: 'unauthorized'},
+    });
+  }
+  return req.config.database.collection('_SCHEMA')
+  .then(coll => coll.find({}).toArray())
+  .then(schemas => ({response: {
+    results: schemas.map(mongoSchemaToSchemaAPIResponse)
+  }}));
 }
 
 router.route('GET', '/schemas', getAllSchemas);
