3d989501b2444753d51781216692009e5e1aa49e
Drew Gross <drew.a.gross@gmail.com>
2016-02-03 11:55:44 -0800

    Get all schemas, with tests, and matches parse.com schemas API

--- schemas.js
+++ schemas.js
@@ -5,21 +5,58 @@ var express = require('express'),
 
 var router = new PromiseRouter();
 
-function mongoSchemaToSchemaAPIResponse(schema) {
+function mongoFieldTypeToApiResponseType(type) {
+	if (type[0] === '*') {
+		return {
+			type: 'Pointer',
+			targetClass: type.slice(1),
+		};
+	}
+	if (type.startsWith('relation<')) {
+		return {
+			type: 'Relation',
+			targetClass: type.slice('relation<'.length, type.length - 1),
+		};
+	}
+	switch (type) {
+		case 'number':   return {type: 'Number'};
+		case 'string':   return {type: 'String'};
+		case 'boolean':  return {type: 'Boolean'};
+		case 'date':     return {type: 'Date'};
+		case 'object':   return {type: 'Object'};
+		case 'array':    return {type: 'Array'};
+		case 'geopoint': return {type: 'GeoPoint'};
+		case 'file':     return {type: 'File'};
+	}
+}
+
+function mongoSchemaAPIResponseFields(schema) {
 	fieldNames = Object.keys(schema).filter(key => key !== '_id');
+	response = {};
+	fieldNames.forEach(fieldName => {
+		response[fieldName] = mongoFieldTypeToApiResponseType(schema[fieldName]);
+	});
+	response.ACL = {type: 'ACL'};
+	response.createdAt = {type: 'Date'};
+	response.updatedAt = {type: 'Date'};
+	response.objectId = {type: 'String'};
+	return response;
+}
+
+function mongoSchemaToSchemaAPIResponse(schema) {
 	return {
 		className: schema._id,
-		fields: fieldNames.map(name => {
-			result = {};
-			result[name] = {
-				type: schema[name],
-			};
-			return result;
-		}),
+		fields: mongoSchemaAPIResponseFields(schema),
 	};
 }
 
 function getAllSchemas(req) {
+	if (!req.auth.isMaster) {
+		return Promise.resolve({
+			status: 401,
+			response: {error: 'unauthorized'},
+		});
+	}
 	return req.config.database.collection('_SCHEMA')
 	.then(coll => coll.find({}).toArray())
 	.then(schemas => ({response: {
