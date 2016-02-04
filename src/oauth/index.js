e010fd82f2b4d32c1ded9f494b29508c9dcf36c1
Florent Vilmart <florentvilmart@me.com>
2016-02-04 14:03:39 -0500

    Generic OAuth provider support
    Refactors facebook login into oauth generic login
    
    Adds additional oauth2 providers
    
    adds ability to pass an oAuth validator in the config
    
    Adds Twitter validation support + OAuth 1 client
    
    Support auth_token instead of access_token for twitter
    
    Improves code coverage of OAuth
    
    Adds validation of oauth provider structures
    
    Better coverage of the OAuth spec
    
    100% coverage of OAuth1.js
    
    Adds passing auth_token_secret for Twitter auth.
    
    Refactors auth validation methods to include authData parameter
    
    - Adds ability to extens oauth validator through configuration
    - Adds ability to extend oauth validator through external module (file or package)
    - Adds more tests
    - Adds tests to login with custom auth provider
    
    Adds more tests for REST API
    
    fixes twitter auth_token
    
    f

---
+++ src/oauth/index.js
@@ -0,0 +1,17 @@
+var facebook = require('./facebook');
+var instagram = require("./instagram");
+var linkedin = require("./linkedin");
+var meetup = require("./meetup");
+var google = require("./google");
+var github = require("./github");
+var twitter = require("./twitter");
+
+module.exports = {
+	facebook: facebook,
+	github: github,
+	google: google,
+	instagram: instagram,
+	linkedin: linkedin, 
+	meetup: meetup,
+	twitter: twitter
+}
