af3ac4c6ecbaa10572c0dfb278b3f2386f297079
Drew Gross <drew.a.gross@gmail.com>
2016-02-05 09:42:35 -0800

    Add class creation logic with validation

--- spec/Schema.spec.js
+++ spec/Schema.spec.js
@@ -131,4 +131,102 @@ describe('Schema', () => {
       });
     });
   });
+
+  it('can add classes without needing an object', done => {
+    config.database.loadSchema()
+    .then(schema => schema.addClassIfNotExists('NewClass', {
+      foo: {type: 'String'}
+    }))
+    .then(result => {
+      expect(result).toEqual({
+        _id: 'NewClass',
+        objectId: 'string',
+        updatedAt: 'string',
+        createdAt: 'string'
+      })
+      done();
+    });
+  });
+
+  it('will fail to create a class if that class was already created by an object', done => {
+    config.database.loadSchema()
+    .then(schema => {
+      schema.validateObject('NewClass', {foo: 7})
+      .then(() => {
+        schema.addClassIfNotExists('NewClass', {
+          foo: {type: 'String'}
+        }).catch(error => {
+          expect(error.code).toEqual(Parse.Error.INVALID_CLASS_NAME)
+          expect(error.error).toEqual('class NewClass already exists');
+          done();
+        });
+      });
+    })
+  });
+
+  it('will resolve class creation races appropriately', done => {
+    // If two callers race to create the same schema, the response to the
+    // loser should be the same as if they hadn't been racing. Furthermore,
+    // The caller that wins the race should resolve it's promise before the
+    // caller that loses the race.
+    config.database.loadSchema()
+    .then(schema => {
+      var p1 = schema.addClassIfNotExists('NewClass', {foo: {type: 'String'}});
+      var p2 = schema.addClassIfNotExists('NewClass', {foo: {type: 'String'}});
+      var raceWinnerHasSucceeded = false;
+      var raceLoserHasFailed = false;
+      Promise.race([p1, p2]) //Use race because we expect the first completed promise to be the successful one
+      .then(response => {
+        raceWinnerHasSucceeded = true;
+        expect(raceLoserHasFailed).toEqual(false);
+        expect(response).toEqual({
+          _id: 'NewClass',
+          objectId: 'string',
+          updatedAt: 'string',
+          createdAt: 'string'
+        });
+      });
+      Promise.all([p1,p2])
+      .catch(error => {
+        expect(raceWinnerHasSucceeded).toEqual(true);
+        expect(error.code).toEqual(Parse.Error.INVALID_CLASS_NAME);
+        expect(error.error).toEqual('class NewClass already exists');
+        done();
+        raceLoserHasFailed = true;
+      });
+    });
+  });
+
+  it('refuses to create classes with invalid names', done => {
+    config.database.loadSchema()
+    .then(schema => {
+      schema.addClassIfNotExists('_InvalidName', {foo: {type: 'String'}})
+      .catch(error => {
+        expect(error.error).toEqual(
+          'Invalid classname: _InvalidName, classnames can only have alphanumeric characters and _, and must start with an alpha character '
+        );
+        done();
+      });
+    });
+  });
+
+  it('refuses to add fields with invalid names', done => {
+    config.database.loadSchema()
+    .then(schema => schema.addClassIfNotExists('NewClass', {'0InvalidName': {type: 'String'}}))
+    .catch(error => {
+      expect(error.code).toEqual(Parse.Error.INVALID_KEY_NAME);
+      expect(error.error).toEqual('invalid field name: 0InvalidName');
+      done();
+    });
+  });
+
+  it('refuses to explicitly create the default fields', done => {
+    config.database.loadSchema()
+    .then(schema => schema.addClassIfNotExists('_Installation', {localeIdentifier: {type: 'String'}}))
+    .catch(error => {
+      expect(error.code).toEqual(136);
+      expect(error.error).toEqual('field localeIdentifier cannot be added');
+      done();
+    });
+  });
 });
