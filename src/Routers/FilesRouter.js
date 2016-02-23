61e8f2aeeae7422edf1c56898d3ceddeb867f5fd
Florent Vilmart <florentvilmart@me.com>
2016-02-23 11:49:21 -0500

    Adds maxUploadSize option

--- src/Routers/FilesRouter.js
+++ src/Routers/FilesRouter.js
@@ -8,7 +8,7 @@ import Config from '../Config';
 
 export class FilesRouter {
 
-  getExpressRouter() {
+  getExpressRouter(options = {}) {
     var router = express.Router();
     router.get('/files/:appId/:filename', this.getHandler);
 
@@ -19,7 +19,7 @@ export class FilesRouter {
 
     router.post('/files/:filename',
       Middlewares.allowCrossDomain,
-      BodyParser.raw({type: () => { return true; }, limit: '20mb'}), // Allow uploads without Content-Type, or with any Content-Type.
+      BodyParser.raw({type: () => { return true; }, limit: options.maxUploadSize || '20mb'}), // Allow uploads without Content-Type, or with any Content-Type.
       Middlewares.handleParseHeaders,
       this.createHandler
     );
