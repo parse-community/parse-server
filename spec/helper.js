4643be70864f861bbc1e2c78abdc329595ac14e2
Nikita Lutsenko <nlutsenko@me.com>
2016-02-23 22:00:25 -0800

    Remove duplicate import in helper.js.

--- spec/helper.js
+++ spec/helper.js
@@ -7,7 +7,6 @@ var DatabaseAdapter = require('../src/DatabaseAdapter');
 var express = require('express');
 var facebook = require('../src/oauth/facebook');
 var ParseServer = require('../src/index').ParseServer;
-var DatabaseAdapter = require('../src/DatabaseAdapter');
 
 var databaseURI = process.env.DATABASE_URI;
 var cloudMain = process.env.CLOUD_CODE_MAIN || '../spec/cloud/main.js';
