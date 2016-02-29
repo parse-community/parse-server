1ea5edc07f31c8bedc56b8b478d5b27851cb6140
Florent Vilmart <florentvilmart@me.com>
2016-02-28 19:36:16 -0500

    refactors filesAdapter tests in factories

--- spec/FilesController.spec.js
+++ spec/FilesController.spec.js
@@ -1,29 +1,33 @@
 var FilesController = require('../src/Controllers/FilesController').FilesController;
 var GridStoreAdapter = require("../src/Adapters/Files/GridStoreAdapter").GridStoreAdapter;
+var S3Adapter = require("../src/Adapters/Files/S3Adapter").S3Adapter;
 var Config = require("../src/Config");
+
+var FCTestFactory = require("./FilesControllerTestFactory");
+
 
 // Small additional tests to improve overall coverage
 describe("FilesController",()=>{
   
-  it("should properly expand objects", (done) => {
-    var config = new Config(Parse.applicationId);
-    var adapter = new GridStoreAdapter();
-    var filesController = new FilesController(adapter);
-    var result = filesController.expandFilesInObject(config, function(){});
+  // Test the grid store adapter
+  var gridStoreAdapter = new GridStoreAdapter();
+  FCTestFactory.testAdapter("GridStoreAdapter", gridStoreAdapter);
+  
+  if (process.env.S3_ACCESS_KEY && process.env.S3_SECRET_KEY) {
     
-    expect(result).toBeUndefined();
+    // Test the S3 Adapter
+    var s3Adapter = new S3Adapter(process.env.S3_ACCESS_KEY, process.env.S3_SECRET_KEY, 'parse.server.tests');
     
-    var fullFile = {
-      type: '__type',
-      url: "http://an.url"
-    }
+    FCTestFactory.testAdapter("S3Adapter",s3Adapter);
     
-    var anObject = {
-      aFile: fullFile
-    }
-    filesController.expandFilesInObject(config, anObject);
-    expect(anObject.aFile.url).toEqual("http://an.url");
+    // Test S3 with direct access
+    var s3DirectAccessAdapter = new S3Adapter(process.env.S3_ACCESS_KEY, process.env.S3_SECRET_KEY, 'parse.server.tests', {
+      directAccess: true
+    });
     
-    done();
-  }) 
-})
+    FCTestFactory.testAdapter("S3AdapterDirect", s3DirectAccessAdapter);
+    
+  } else if (!process.env.TRAVIS) {
+    console.log("set S3_ACCESS_KEY and S3_SECRET_KEY to test S3Adapter")
+  }
+});
