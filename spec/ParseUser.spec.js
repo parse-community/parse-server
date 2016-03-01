5a0bfafc0e8f390cca3afec3f5f37243b0c8bbb4
Francis Lessard <flessard@propage.com>
2016-03-01 15:43:00 -0500

    Become on non existing user
    Fix bug where become return invalid session if the pointer to user
    doesn't exist

--- spec/ParseUser.spec.js
+++ spec/ParseUser.spec.js
@@ -177,6 +177,40 @@ describe('Parse.User testing', () => {
     });
   });
 
+ it("become on non existing user", (done) => {
+    var user = null;
+    var sessionToken = null;
+    var userPointer = null;
+
+    Parse.Promise.as().then(function() {
+      return Parse.User.signUp("9090", "-----");
+
+    }).then(function(newUser) {
+      equal(Parse.User.current(), newUser);
+
+      user = newUser;
+      userPointer = user.toPointer();
+      sessionToken = newUser.getSessionToken();
+      ok(sessionToken);
+
+            var u = Parse.Object.fromJSON(userPointer);
+      return u.destroy({useMasterKey:true});
+
+    }).then(function() {
+      return Parse.User.become(sessionToken);
+    }).then((user) => {
+      ok(false, "Shouldn't have been able to log in with non existing user.");
+    }, function(error) {
+      ok(error);
+      return Parse.Promise.as();
+    }).then(function() {
+      done();
+    }, function(error) {
+      ok(false, error);
+      done();
+    });
+  });
+ 
   it("cannot save non-authed user", (done) => {
     var user = new Parse.User();
     user.set({
