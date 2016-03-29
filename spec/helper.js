cafd8ee6d2d1289669b283482bf0de7f36e484a6
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-03-29 14:09:23 -0400

    :construction: enable debugging

--- spec/helper.js
+++ spec/helper.js
@@ -78,6 +78,8 @@ Parse.serverURL = 'http://localhost:' + port + '/1';
 // TODO: update tests to work in an A+ way
 Parse.Promise.disableAPlusCompliant();
 
+process.env.DEBUG = 'mon*';
+
 // Travis will set this environment variable so we always test against
 // MongoDB 2.6.11 and 3.0.8.  Set the default here so when devs just
 // run `npm test` locally MongoDB@3.0.8 will be installed if needed and
