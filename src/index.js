1eb2a87f9f0b5328ebeaaf0eb2f82b86150f2b13
Florent Vilmart <flovilmart@users.noreply.github.com>
2016-04-05 18:18:49 -0400

    Removes GCS Adapter from provided adapters
    * :bomb: removes exposed GCS files adapter
    
    - Should fix problems with GCS
    - Throws error that shows the right repo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import winston           from 'winston';
 import ParseServer       from './ParseServer';
-import GCSAdapter        from 'parse-server-gcs-adapter';
-import S3Adapter         from 'parse-server-s3-adapter';
-import FileSystemAdapter from 'parse-server-fs-adapter';
+import S3Adapter         from 'parse-server-s3-adapter'
+import FileSystemAdapter from 'parse-server-fs-adapter'
+import { useExternal }   from './deprecated'
 
 if (process.env.VERBOSE || process.env.VERBOSE_PARSE_SERVER) {
   winston.level = 'silly';
@@ -19,6 +19,8 @@ let _ParseServer = function(options) {
 }
 // Mount the create liveQueryServer
 _ParseServer.createLiveQueryServer = ParseServer.createLiveQueryServer;
+
+let GCSAdapter = useExternal('GCSAdapter', 'parse-server-gcs-adapter');
 
 export default ParseServer;
 export { S3Adapter, GCSAdapter, FileSystemAdapter, _ParseServer as ParseServer };
