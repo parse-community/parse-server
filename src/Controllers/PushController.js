e037011cea8b743bb3f43e47958e31e316542e85
Florent Vilmart <florentvilmart@me.com>
2016-04-07 17:47:25 -0400

    nits

--- src/Controllers/PushController.js
+++ src/Controllers/PushController.js
@@ -38,7 +38,7 @@ export class PushController extends AdaptableController {
     return !!this.adapter;
   }
 
-  sendPush(body = {}, where = {}, config, auth, pushStatusIdCallback = () => {}) {
+  sendPush(body = {}, where = {}, config, auth, onPushStatusSaved = () => {}) {
     var pushAdapter = this.adapter;
     if (!this.pushIsAvailable) {
       throw new Parse.Error(Parse.Error.PUSH_MISCONFIGURED,
@@ -84,7 +84,7 @@ export class PushController extends AdaptableController {
     return Promise.resolve().then(() => {
       return pushStatus.setInitial(body, where);
     }).then(() => {
-      pushStatusIdCallback(pushStatus.objectId);
+      onPushStatusSaved(pushStatus.objectId);
       return badgeUpdate();
     }).then(() => {
       return rest.find(config, auth, '_Installation', where);
