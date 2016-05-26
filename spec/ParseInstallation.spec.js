94e46de35614a5df9a8d6772443e2bed897badea
Drew Gross <drew.a.gross@gmail.com>
2016-05-25 19:01:39 -0700

    remove more mongoFind

--- spec/ParseInstallation.spec.js
+++ spec/ParseInstallation.spec.js
@@ -677,10 +677,9 @@ fdescribe('Installations', () => {
         'deviceType': 'ios'
       };
       return rest.create(config, auth.nobody(config), '_Installation', input);
-    }).then(() => {
-      return database.mongoFind('_Installation',
-                     {deviceToken: t}, {});
-    }).then((results) => {
+    })
+    .then(() => database.adapter.find('_Installation', { deviceToken: t }, installationSchema, {}))
+    .then(results => {
       expect(results.length).toEqual(1);
       input = {
         'deviceToken': t,
@@ -723,9 +722,9 @@ fdescribe('Installations', () => {
         'deviceType': 'ios'
       };
       return rest.create(config, auth.nobody(config), '_Installation', input);
-    }).then(() => {
-      return database.mongoFind('_Installation', {deviceToken: t}, {});
-    }).then((results) => {
+    })
+    .then(() => database.adapter.find('_Installation', { deviceToken: t }, installationSchema, {}))
+    .then(results => {
       expect(results.length).toEqual(1);
       tokenObj = results[0];
       input = {
@@ -733,11 +732,10 @@ fdescribe('Installations', () => {
         'deviceToken': t,
         'deviceType': 'ios'
       };
-      return rest.update(config, auth.nobody(config), '_Installation',
-                         installObj._id, input);
-    }).then(() => {
-      return database.mongoFind('_Installation', {_id: tokenObj._id}, {});
-    }).then((results) => {
+      return rest.update(config, auth.nobody(config), '_Installation', installObj.objectId, input);
+    })
+    .then(() => database.adapter.find('_Installation', { objectId: tokenObj.objectId }, installationSchema, {}))
+    .then(results => {
       expect(results.length).toEqual(1);
       expect(results[0].installationId).toEqual(installId);
       expect(results[0].deviceToken).toEqual(t);
