2fc07f97cd787b4032967d17aa6e6c6bc6d7496e
Florent Vilmart <florentvilmart@me.com>
2016-06-24 16:21:05 -0400

    Use the callback body instead of response.body that may not be set

--- src/cloud-code/HTTPResponse.js
+++ src/cloud-code/HTTPResponse.js
@@ -1,15 +1,27 @@
 
 export default class HTTPResponse {
-  constructor(response) {
+  constructor(response, body) {
     this.status = response.statusCode;
     this.headers = response.headers;
-    this.buffer = response.body;
     this.cookies = response.headers["set-cookie"];
+    
+    this.body = body;
+    if (typeof body == 'string') {
+      this._text = body;
+    } else if (Buffer.isBuffer(body)) {
+      this.buffer = body;
+    } else if (typeof body == 'object') {
+      this._data = body;
+    }
   }
   
   get text() {
-    return this.buffer.toString('utf-8');
+    if (!this._text && this.buffer) {
+      this._text = this.buffer.toString('utf-8');
+    }
+    return this._text;
   }
+
   get data() {
     if (!this._data) {
       try {
