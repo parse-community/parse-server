7c8bab4ab6635f6bc7711e9c7e31433de3897404
Florent Vilmart <florentvilmart@me.com>
2016-07-12 08:22:59 -0400

    Adds support for multiple twitter auths options

--- src/authDataManager/twitter.js
+++ src/authDataManager/twitter.js
@@ -1,9 +1,26 @@
 // Helper functions for accessing the twitter API.
 var OAuth = require('./OAuth1Client');
 var Parse = require('parse/node').Parse;
+var logger = require('../logger');
 
 // Returns a promise that fulfills iff this user id is valid.
 function validateAuthData(authData, options) {
+  if (Array.isArray(options)) {
+    let consumer_key = authData.consumer_key;
+    if (!consumer_key) {
+      logger.error('Twitter Auth', 'Multiple twitter configurations are available, by no consumer_key was sent by the client.');
+      throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Twitter auth is invalid for this user.');
+    }
+    options = options.filter((option) => {
+      return option.consumer_key == consumer_key;
+    });
+
+    if (options.length == 0) {
+      logger.error('Twitter Auth','Cannot find a configuration for the provided consumer_key');
+      throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Twitter auth is invalid for this user.');
+    }
+    options = options[0];
+  }
   var client = new OAuth(options);
   client.host = "api.twitter.com";
   client.auth_token = authData.auth_token;
