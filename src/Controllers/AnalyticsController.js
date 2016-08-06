8110d0c438fec9c126f7fcfe844c94fa8c591c68
Florent Vilmart <florentvilmart@me.com>
2016-08-06 15:28:52 -0400

    nit

--- src/Controllers/AnalyticsController.js
+++ src/Controllers/AnalyticsController.js
@@ -4,7 +4,7 @@ import { AnalyticsAdapter } from '../Adapters/Analytics/AnalyticsAdapter';
 export class AnalyticsController extends AdaptableController {
   appOpened(req) {
     return Promise.resolve().then(() => {
-      this.adapter.appOpened(req.body, req);
+      return this.adapter.appOpened(req.body, req);
     }).then((response) => {
       return { response: response || {} };
     }).catch((err) => {
