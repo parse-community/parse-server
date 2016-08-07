4f1f346d583ff1947a789d7a874c9fbb95870f18
Florent Vilmart <florentvilmart@me.com>
2016-08-07 17:09:17 -0400

    investigate...

--- spec/Logger.spec.js
+++ spec/Logger.spec.js
@@ -17,46 +17,47 @@ describe('Logger', () => {
     logging.logger.info('hi');
     expect(testTransport.log).toHaveBeenCalled();
     logging.removeTransport(testTransport);
+    expect(Object.keys(logging.logger.transports).length).toBe(2);
   });
 
-  it('should have files transports', (done) => {
-    reconfigureServer().then(() => {
-      let transports = logging.logger.transports;
-      let transportKeys = Object.keys(transports);
-      expect(transportKeys.length).toBe(2);
-      done();
-    });
-  });
+  // xit('should have files transports', (done) => {
+  //   reconfigureServer().then(() => {
+  //     let transports = logging.logger.transports;
+  //     let transportKeys = Object.keys(transports);
+  //     expect(transportKeys.length).toBe(2);
+  //     done();
+  //   });
+  // });
 
-  it('should disable files logs', (done) => {
-    reconfigureServer({
-      logsFolder: null
-    }).then(() => {
-      let transports = logging.logger.transports;
-      let transportKeys = Object.keys(transports);
-      expect(transportKeys.length).toBe(0);
-      done();
-    });
-  });
+  // xit('should disable files logs', (done) => {
+  //   reconfigureServer({
+  //     logsFolder: null
+  //   }).then(() => {
+  //     let transports = logging.logger.transports;
+  //     let transportKeys = Object.keys(transports);
+  //     expect(transportKeys.length).toBe(0);
+  //     done();
+  //   });
+  // });
 
-  xit('should enable JSON logs', (done) => {
-    // Force console transport
-    process.env.VERBOSE=1;
-    reconfigureServer({
-      logsFolder: null,
-      jsonLogs: true
-    }).then(() => {
-      let spy = spyOn(process.stdout, 'write');
-      logging.logger.info('hi', {key: 'value'});
-      expect(process.stdout.write).toHaveBeenCalled();
-      var firstLog = process.stdout.write.calls.first().args[0];
-      expect(firstLog).toEqual(JSON.stringify({key: 'value', level: 'info', message: 'hi' })+'\n');
-      delete process.env.VERBOSE;
-      return reconfigureServer({
-        jsonLogs: false
-      });
-    }).then(() => {
-      done();
-    });
-  });
+  // xit('should enable JSON logs', (done) => {
+  //   // Force console transport
+  //   process.env.VERBOSE=1;
+  //   reconfigureServer({
+  //     logsFolder: null,
+  //     jsonLogs: true
+  //   }).then(() => {
+  //     let spy = spyOn(process.stdout, 'write');
+  //     logging.logger.info('hi', {key: 'value'});
+  //     expect(process.stdout.write).toHaveBeenCalled();
+  //     var firstLog = process.stdout.write.calls.first().args[0];
+  //     expect(firstLog).toEqual(JSON.stringify({key: 'value', level: 'info', message: 'hi' })+'\n');
+  //     delete process.env.VERBOSE;
+  //     return reconfigureServer({
+  //       jsonLogs: false
+  //     });
+  //   }).then(() => {
+  //     done();
+  //   });
+  // });
 });
