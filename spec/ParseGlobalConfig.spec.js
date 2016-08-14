c9818173cc37a750d0b36819cdfdb60ef3243d07
Florent Vilmart <florentvilmart@me.com>
2016-08-13 20:46:15 -0400

    Mark GlobalConfig is incompatible with PG
    - Problem is with nested updates (param.prop = value)

--- spec/ParseGlobalConfig.spec.js
+++ spec/ParseGlobalConfig.spec.js
@@ -4,7 +4,7 @@ var request = require('request');
 var Parse = require('parse/node').Parse;
 let Config = require('../src/Config');
 
-describe('a GlobalConfig', () => {
+describe_only_db('mongo')('a GlobalConfig', () => {
   beforeEach(done => {
     let config = new Config('test');
     config.database.adapter.upsertOneObject(
@@ -15,7 +15,7 @@ describe('a GlobalConfig', () => {
     ).then(done, done);
   });
 
-  it_exclude_dbs(['postgres'])('can be retrieved', (done) => {
+  it('can be retrieved', (done) => {
     request.get({
       url    : 'http://localhost:8378/1/config',
       json   : true,
@@ -32,7 +32,7 @@ describe('a GlobalConfig', () => {
     });
   });
 
-  it_exclude_dbs(['postgres'])('can be updated when a master key exists', (done) => {
+  it('can be updated when a master key exists', (done) => {
     request.put({
       url    : 'http://localhost:8378/1/config',
       json   : true,
@@ -48,7 +48,7 @@ describe('a GlobalConfig', () => {
     });
   });
 
-  it_exclude_dbs(['postgres'])('properly handles delete op', (done) => {
+  it('properly handles delete op', (done) => {
     request.put({
       url    : 'http://localhost:8378/1/config',
       json   : true,
@@ -79,7 +79,7 @@ describe('a GlobalConfig', () => {
     });
   });
 
-  it_exclude_dbs(['postgres'])('fail to update if master key is missing', (done) => {
+  it('fail to update if master key is missing', (done) => {
     request.put({
       url    : 'http://localhost:8378/1/config',
       json   : true,
@@ -95,7 +95,7 @@ describe('a GlobalConfig', () => {
     });
   });
 
-  it_exclude_dbs(['postgres'])('failed getting config when it is missing', (done) => {
+  it('failed getting config when it is missing', (done) => {
     let config = new Config('test');
     config.database.adapter.deleteObjectsByQuery(
       '_GlobalConfig',
