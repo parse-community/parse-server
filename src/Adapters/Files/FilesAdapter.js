e1f8f670248db24eb00edc2ec2388543ac0fb0a3
Arthur Cinader <arthur@sugarinc.com>
2016-08-16 10:07:49 -0700

    Fix capitalization, typo, and grammar mistake

--- src/Adapters/Files/FilesAdapter.js
+++ src/Adapters/Files/FilesAdapter.js
@@ -12,7 +12,7 @@
 // database adapter.
 
 export class FilesAdapter {
-  /* this method is responsible to store the file in order to be retrived later by it's file name
+  /* This method is responsible to store the file in order to be retrieved later by its file name
    *
    * @param filename the filename to save
    * @param data the buffer of data from the file
