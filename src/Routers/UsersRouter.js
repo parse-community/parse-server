4cc12f9dbd17c8d437e7d15d3d2c083e4b5e3cc3
Florent Vilmart <florentvilmart@me.com>
2016-08-21 10:21:09 -0400

    Makes sure routes don't overlap and yield a header set error

--- src/Routers/UsersRouter.js
+++ src/Routers/UsersRouter.js
@@ -18,6 +18,9 @@ export class UsersRouter extends ClassesRouter {
   }
 
   handleGet(req) {
+    if (req.params.objectId === 'me') {
+      return this.handleMe(req);
+    }
     req.params.className = '_User';
     return super.handleGet(req);
   }
@@ -198,7 +201,6 @@ export class UsersRouter extends ClassesRouter {
   mountRoutes() {
     this.route('GET', '/users', req => { return this.handleFind(req); });
     this.route('POST', '/users', req => { return this.handleCreate(req); });
-    this.route('GET', '/users/me', req => { return this.handleMe(req); });
     this.route('GET', '/users/:objectId', req => { return this.handleGet(req); });
     this.route('PUT', '/users/:objectId', req => { return this.handleUpdate(req); });
     this.route('DELETE', '/users/:objectId', req => { return this.handleDelete(req); });
