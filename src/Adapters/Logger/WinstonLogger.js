dd4c22bd770f07ebe70b21c75e74e20ea62af56e
Florent Vilmart <florentvilmart@me.com>
2016-09-03 09:49:40 -0400

    Make sure the error level options is enforced

--- src/Adapters/Logger/WinstonLogger.js
+++ src/Adapters/Logger/WinstonLogger.js
@@ -18,16 +18,15 @@ function updateTransports(options) {
       delete transports['parse-server-error'];
     } else if (!_.isUndefined(options.dirname)) {
       transports['parse-server'] = new (DailyRotateFile)(
-        Object.assign({
+        Object.assign({}, {
           filename: 'parse-server.info',
           name: 'parse-server',
         }, options));
       transports['parse-server-error'] = new (DailyRotateFile)(
-        Object.assign({
+        Object.assign({}, {
           filename: 'parse-server.err',
           name: 'parse-server-error',
-          level: 'error'
-        }, options));
+        }, options, { level: 'error'}));
     }
 
     transports.console = new (winston.transports.Console)(
