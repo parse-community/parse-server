4c2600e914e7387fdb7dc78f49092ab1559ad50f
Kulshekhar Kabra <kulshekhar@users.noreply.github.com>
2016-10-16 01:43:36 +0530

    Remove unnecessary try/catch

--- src/Adapters/Storage/Postgres/ClientCreator.js
+++ src/Adapters/Storage/Postgres/ClientCreator.js
@@ -6,25 +6,20 @@ export function createClient(uri, databaseOptions) {
   let dbOptions = {};
   databaseOptions = databaseOptions || {};
 
-  try {
-    if (uri) {
-      dbOptions = parser.getDatabaseOptionsFromURI(uri);
+  if (uri) {
+    dbOptions = parser.getDatabaseOptionsFromURI(uri);
+  }
+
+  for (const key in databaseOptions) {
+    dbOptions[key] = databaseOptions[key];
+  }
+
+  client = pgp(dbOptions);
+
+  if (dbOptions.pgOptions) {
+    for (const key in dbOptions.pgOptions) {
+      client.pg.defaults[key] = dbOptions.pgOptions[key];
     }
-
-    for (const key in databaseOptions){
-      dbOptions[key] = databaseOptions[key];
-    }
-
-    client = pgp(dbOptions);
-
-    if (dbOptions.pgOptions) {
-      for (const key in dbOptions.pgOptions) {
-        client.pg.defaults[key] = dbOptions.pgOptions[key]; 
-      }
-    }
-
-  } catch (e) { 
-    throw e;
   }
 
   return client;
