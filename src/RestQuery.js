a41faad6bb32b17936cd2116f3cf5625e4dd57f1
Tyler Brock <tyler.brock@gmail.com>
2016-10-24 15:37:00 -0700

    Do not run find when responding to count

--- src/RestQuery.js
+++ src/RestQuery.js
@@ -388,7 +388,7 @@ RestQuery.prototype.replaceDontSelect = function() {
 // Returns a promise for whether it was successful.
 // Populates this.response with an object that only has 'results'.
 RestQuery.prototype.runFind = function(options = {}) {
-  if (this.findOptions.limit === 0) {
+  if (this.findOptions.limit === 0 || this.doCount) {
     this.response = {results: []};
     return Promise.resolve();
   }
