0fe629fa60d2b344f14dad9c8cee21e8e061c96b
Florent Vilmart <florentvilmart@me.com>
2016-11-12 11:42:15 -0500

    Moves test runner logic to helper

--- spec/helper.js
+++ spec/helper.js
@@ -1,7 +1,11 @@
 "use strict"
 // Sets up a Parse API server for testing.
+const SpecReporter = require('jasmine-spec-reporter');
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = process.env.PARSE_SERVER_TEST_TIMEOUT || 5000;
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new SpecReporter()); 
 
 global.on_db = (db, callback, elseCallback) => {
   if (process.env.PARSE_SERVER_TEST_DB == db) {
