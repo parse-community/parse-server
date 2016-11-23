ed31b23d66978e54998d0ff38ddda88530bc5738
Lenart Rudel <lenart.rudel@gmail.com>
2016-11-23 09:42:56 +0100

    Rename method for building link and pass config to it.

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -119,7 +119,7 @@ export class UserController extends AdaptableController {
     // We may need to fetch the user in case of update email
     this.getUserIfNeeded(user).then((user) => {
       const username = encodeURIComponent(user.username);
-      const link = buildVerificationLink(this.config.verifyEmailURL, username, token);
+      const link = buildEmailLink(this.config.verifyEmailURL, username, token, this.config);
       const options = {
         appName: this.config.appName,
         link: link,
@@ -154,7 +154,7 @@ export class UserController extends AdaptableController {
       const token = encodeURIComponent(user._perishable_token);
       const username = encodeURIComponent(user.username);
 
-      const link = buildVerificationLink(this.config.requestResetPasswordURL, username, token);
+      const link = buildEmailLink(this.config.requestResetPasswordURL, username, token, this.config);
       const options = {
         appName: this.config.appName,
         link: link,
@@ -215,12 +215,12 @@ function updateUserPassword(userId, password, config) {
   });
 }
 
-function buildVerificationLink(destination, username, token) {
+function buildEmailLink(destination, username, token, config) {
   let usernameAndToken = `token=${token}&username=${username}`
 
-  if (this.config.parseFrameURL) {
-    let destinationWithoutHost = destination.replace(this.config.publicServerURL, '');
-    return `${this.config.parseFrameURL}?link=${encodeURIComponent(destinationWithoutHost)}&${usernameAndToken}`;
+  if (config.parseFrameURL) {
+    let destinationWithoutHost = destination.replace(config.publicServerURL, '');
+    return `${config.parseFrameURL}?link=${encodeURIComponent(destinationWithoutHost)}&${usernameAndToken}`;
   } else {
     return `${destination}?${usernameAndToken}`;
   }
