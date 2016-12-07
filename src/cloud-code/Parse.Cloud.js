20f87397cc06ac5f3631f9d21cd9af814ea35fdb
Florent Vilmart <flovilmart@users.noreply.github.com>
2016-12-07 18:11:44 -0500

    Update Parse.Cloud.js

--- src/cloud-code/Parse.Cloud.js
+++ src/cloud-code/Parse.Cloud.js
@@ -60,7 +60,7 @@ ParseCloud._removeAllHooks = () => {
 }
 
 ParseCloud.useMasterKey = () => {
-  // eslint-disable-next-line instead
+  // eslint-disable-next-line
   console.warn("Parse.Cloud.useMasterKey is deprecated (and has no effect anymore) on parse-server, please refer to the cloud code migration notes: https://github.com/ParsePlatform/parse-server/wiki/Compatibility-with-Hosted-Parse#cloud-code")
 }
 
