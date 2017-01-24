145a1fe23b6f20567580e54101a20bcbc7ecf446
Arthur Cinader <arthur@sugarinc.com>
2017-01-24 11:30:37 -0800

    Log Parse Errors so they are intelligible.
    The problem this pr is trying to solve:
    
    When an error occurs on the server, a message should
    be returned to the client, and a message should be logged.
    
    Currently, on the server, the log is just [object, object]
    
    This pr will stop calling the default express error handler
    which causes two problems: 1. it writes to console instead of log file
    2. the output is completely useless! :)
    
    Instead, we'll log the error ourselves using the ParseServer's logger.
    
    fixes: #661

--- spec/FilesController.spec.js
+++ spec/FilesController.spec.js
@@ -1,7 +1,17 @@
-var GridStoreAdapter = require("../src/Adapters/Files/GridStoreAdapter").GridStoreAdapter;
-var Config = require("../src/Config");
-var FilesController = require('../src/Controllers/FilesController').default;
+const LoggerController = require('../src/Controllers/LoggerController').LoggerController;
+const WinstonLoggerAdapter = require('../src/Adapters/Logger/WinstonLoggerAdapter').WinstonLoggerAdapter;
+const GridStoreAdapter = require("../src/Adapters/Files/GridStoreAdapter").GridStoreAdapter;
+const Config = require("../src/Config");
+const FilesController = require('../src/Controllers/FilesController').default;
 
+const mockAdapter = {
+  createFile: () => {
+    return Parse.Promise.reject(new Error('it failed'));
+  },
+  deleteFile: () => { },
+  getFileData: () => { },
+  getFileLocation: () => 'xyz'
+}
 
 // Small additional tests to improve overall coverage
 describe("FilesController",() =>{
@@ -26,5 +36,25 @@ describe("FilesController",() =>{
     expect(anObject.aFile.url).toEqual("http://an.url");
 
     done();
-  })
+  });
+
+  it('should create a server log on failure', done => {
+    const logController = new LoggerController(new WinstonLoggerAdapter());
+
+    reconfigureServer({ filesAdapter: mockAdapter })
+      .then(() => new Promise(resolve => setTimeout(resolve, 1000)))
+      .then(() => new Parse.File("yolo.txt", [1,2,3], "text/plain").save())
+      .then(
+        () => done.fail('should not succeed'),
+        () => setImmediate(() => Parse.Promise.as('done'))
+      )
+      .then(() => logController.getLogs({ from: Date.now() - 500, size: 1000 }))
+      .then((logs) => {
+        const log = logs.pop();
+        expect(log.level).toBe('error');
+        expect(log.code).toBe(130);
+        expect(log.message).toBe('Could not store file.');
+        done();
+      });
+  });
 });
