493d5d347b0ac5ae389b03788f2e90dae768f6e6
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2017-02-18 15:39:38 +0000

    Update index.js
    trying to fix indentation.

--- src/Adapters/Storage/Postgres/sql/index.js
+++ src/Adapters/Storage/Postgres/sql/index.js
@@ -4,29 +4,29 @@ var QueryFile = require('pg-promise').QueryFile;
 var path = require('path');
 
 module.exports = {
-    array: {
-        add: sql('array/add.sql'),
-        addUnique: sql('array/add-unique.sql'),
-        contains: sql('array/contains.sql'),
-        containsAll: sql('array/contains-all.sql'),
-        remove: sql('array/remove.sql')
-    },
-    misc: {
-        jsonObjectSetKeys: sql('misc/json-object-set-keys.sql')
-    }
+  array: {
+    add: sql('array/add.sql'),
+    addUnique: sql('array/add-unique.sql'),
+    contains: sql('array/contains.sql'),
+    containsAll: sql('array/contains-all.sql'),
+    remove: sql('array/remove.sql')
+  },
+  misc: {
+    jsonObjectSetKeys: sql('misc/json-object-set-keys.sql')
+  }
 };
 
 ///////////////////////////////////////////////
 // Helper for linking to external query files;
 function sql(file) {
 
-    var fullPath = path.join(__dirname, file); // generating full path;
+  var fullPath = path.join(__dirname, file); // generating full path;
 
-    var qf = new QueryFile(fullPath, {minify: true});
+  var qf = new QueryFile(fullPath, {minify: true});
 
-    if (qf.error) {
-        console.error(qf.error);
-    }
+  if (qf.error) {
+    console.error(qf.error);
+  }
 
-    return qf;
+  return qf;
 }
