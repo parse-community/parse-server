9dbb89a2e4fdef30ca65e540a660dc6fb8ea71a0
Florent Vilmart <flovilmart@users.noreply.github.com>
2017-05-16 12:06:17 -0400

    Fixes postgres flaky test (#3822)
    * Changes expected error code
    
    * nits

--- spec/PostgresInitOptions.spec.js
+++ spec/PostgresInitOptions.spec.js
@@ -1,7 +1,7 @@
 const Parse = require('parse/node').Parse;
 const PostgresStorageAdapter = require('../src/Adapters/Storage/Postgres/PostgresStorageAdapter');
 const postgresURI = 'postgres://localhost:5432/parse_server_postgres_adapter_test_database';
-
+const Config = require('../src/Config');
 //public schema
 const databaseOptions1 = {
   initOptions: {
@@ -28,33 +28,40 @@ const GameScore = Parse.Object.extend({
   className: "GameScore"
 });
 
-describe('Postgres database init options', () => {
+describe_only_db('postgres')('Postgres database init options', () => {
   it('should create server with public schema databaseOptions', (done) => {
+    const config = new Config('test');
+    // Close the current DB before continueing
+    config.database.adapter._pgp.end();
     reconfigureServer({
       databaseAdapter: new PostgresStorageAdapter({
         uri: postgresURI, collectionPrefix: 'test_',
         databaseOptions: databaseOptions1
       })
-    }).then(done, fail);
+    }).then(done, done.fail);
   });
 
   it("save new GameScore in public schema", function (done) {
     var score = new GameScore({ "score": 1337, "playerName": "Sean Plott", "cheatMode": false });
-    score.save().then(done, fail);
+    score.save().then(done, done.fail);
   });
 
   it('should fail to create server if schema databaseOptions does not exist', (done) => {
+    const config = new Config('test');
+    // Close the current DB before continueing
+    config.database.adapter._pgp.end();
     reconfigureServer({
       databaseAdapter: new PostgresStorageAdapter({
         uri: postgresURI, collectionPrefix: 'test_',
         databaseOptions: databaseOptions2
       })
     }).then(() => {
+      done.fail('Should not succeed');
+    }, error => {
+      // INVALID_SCHEMA error 3F000
+      // https://www.postgresql.org/docs/9.5/static/errcodes-appendix.html
+      expect(error.code).toEqual('3F000');
       done();
-    })
-      .catch(error => {
-        expect(error.code).toEqual('42P01');
-        done();
-      });
+    });
   });
 });
