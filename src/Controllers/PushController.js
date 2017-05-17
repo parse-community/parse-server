11e2a9f60d12af70a49e3a610d6d7a7e8108ef07
Felipe Andrade <felipe@i2mobile.com.br>
2017-05-17 15:56:39 -0300

    Fix in PushController and add tests
    Add a test to check push_time format and if it should schedule push
    when the parse-server is configured

--- src/Controllers/PushController.js
+++ src/Controllers/PushController.js
@@ -53,7 +53,7 @@ export class PushController {
       onPushStatusSaved(pushStatus.objectId);
       return badgeUpdate();
     }).then(() => {
-      if (body.push_time && config.hasPushScheduledSupport) {
+      if (body.hasOwnProperty('push_time') && config.hasPushScheduledSupport) {
         return Promise.resolve();
       }
       return config.pushControllerQueue.enqueue(body, where, config, auth, pushStatus);
