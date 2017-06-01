6167beb272ddccc8741daad23ab8d5e39041c833
David Richard <d2391@me.com>
2017-05-31 22:24:27 -0400

    added the allowClientClassCreation config option

--- src/defaults.js
+++ src/defaults.js
@@ -32,5 +32,6 @@ export default {
   expireInactiveSessions: true,
   revokeSessionOnPasswordReset: true,
   schemaCacheTTL: 5000, // in ms
-  userSensitiveFields: ['email']
+  userSensitiveFields: ['email'],
+  authenticatedFileRetrieval: false,
 }
