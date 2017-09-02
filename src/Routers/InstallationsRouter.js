3b857a4f1c42d104346fb80823f788ca8830d5b0
Florent Vilmart <florent@flovilmart.com>
2017-09-01 20:15:41 -0400

    Makes InstallationRouter like others

--- src/Routers/InstallationsRouter.js
+++ src/Routers/InstallationsRouter.js
@@ -1,34 +1,11 @@
 // InstallationsRouter.js
 
 import ClassesRouter from './ClassesRouter';
-import rest from '../rest';
 
 export class InstallationsRouter extends ClassesRouter {
   handleFind(req) {
-    const body = Object.assign(req.body, ClassesRouter.JSONFromQuery(req.query));
-    var options = {};
-
-    if (body.skip) {
-      options.skip = Number(body.skip);
-    }
-    if (body.limit || body.limit === 0) {
-      options.limit = Number(body.limit);
-    }
-    if (body.order) {
-      options.order = String(body.order);
-    }
-    if (body.count) {
-      options.count = true;
-    }
-    if (body.include) {
-      options.include = String(body.include);
-    }
-
-    return rest.find(req.config, req.auth,
-      '_Installation', body.where, options, req.info.clientSDK)
-      .then((response) => {
-        return {response: response};
-      });
+    req.params.className = '_Installation';
+    return super.handleFind(req);
   }
 
   handleGet(req) {
