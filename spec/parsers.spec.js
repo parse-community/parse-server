cd476a1e7c1e0addf826c725f833d48fd581a241
Florent Vilmart <florent@flovilmart.com>
2017-09-01 23:19:51 -0400

    Improves coverage of parsers

--- spec/parsers.spec.js
+++ spec/parsers.spec.js
@@ -2,6 +2,10 @@ import {
   numberParser,
   numberOrBoolParser,
   booleanParser,
+  objectParser,
+  arrayParser,
+  moduleOrObjectParser,
+  nullParser,
 } from '../src/cli/utils/parsers';
 
 describe('parsers', () => {
@@ -30,5 +34,34 @@ describe('parsers', () => {
     expect(parser('false')).toEqual(false);
     expect(parser(1)).toEqual(true);
     expect(parser(2)).toEqual(false);
+  });
+
+  it('parses correctly with objectParser', () => {
+    const parser = objectParser;
+    expect(parser({hello: 'world'})).toEqual({hello: 'world'});
+    expect(parser('{"hello": "world"}')).toEqual({hello: 'world'});
+    expect(() => {parser('string')}).toThrow();
+  });
+
+  it('parses correctly with moduleOrObjectParser', () => {
+    const parser = moduleOrObjectParser;
+    expect(parser({hello: 'world'})).toEqual({hello: 'world'});
+    expect(parser('{"hello": "world"}')).toEqual({hello: 'world'});
+    expect(parser('string')).toEqual('string');
+  });
+
+  it('parses correctly with arrayParser', () => {
+    const parser = arrayParser;
+    expect(parser([1,2,3])).toEqual([1,2,3]);
+    expect(parser('{"hello": "world"}')).toEqual(['{"hello": "world"}']);
+    expect(parser('1,2,3')).toEqual(['1','2','3']);
+    expect(() => {parser(1)}).toThrow();
+  });
+
+  it('parses correctly with nullParser', () => {
+    const parser = nullParser;
+    expect(parser('null')).toEqual(null);
+    expect(parser(1)).toEqual(1);
+    expect(parser('blabla')).toEqual('blabla');
   });
 });
