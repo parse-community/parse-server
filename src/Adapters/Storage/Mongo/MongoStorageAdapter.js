5aafc93476c72260f0f40616576a3f644bda0166
Diamond Lewis <findlewis@gmail.com>
2017-09-02 11:31:19 -0500

    Text Index Support (#4081)
    * add text index support
    
    * additional validation
    
    * multiple text index error
    
    * rename function
    
    * Delete text indexes message

--- src/Adapters/Storage/Mongo/MongoStorageAdapter.js
+++ src/Adapters/Storage/Mongo/MongoStorageAdapter.js
@@ -343,8 +343,10 @@ export class MongoStorageAdapter {
       memo[transformKey(className, key, schema)] = 1;
       return memo;
     }, {});
+
     readPreference = this._parseReadPreference(readPreference);
-    return this._adaptiveCollection(className)
+    return this.createTextIndexesIfNeeded(className, query)
+      .then(() => this._adaptiveCollection(className))
       .then(collection => collection.find(mongoWhere, {
         skip,
         limit,
@@ -437,6 +439,27 @@ export class MongoStorageAdapter {
         [fieldName]: '2dsphere'
       };
       return this.createIndex(className, index);
+    }
+    return Promise.resolve();
+  }
+
+  createTextIndexesIfNeeded(className, query) {
+    for(const fieldName in query) {
+      if (!query[fieldName] || !query[fieldName].$text) {
+        continue;
+      }
+      const index = {
+        [fieldName]: 'text'
+      };
+      return this.createIndex(className, index)
+        .catch((error) => {
+          if (error.code === 85) {
+            throw new Parse.Error(
+              Parse.Error.INTERNAL_SERVER_ERROR,
+              'Only one text index is supported, please delete all text indexes to use new field.');
+          }
+          throw error;
+        });
     }
     return Promise.resolve();
   }
