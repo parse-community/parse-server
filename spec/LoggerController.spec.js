ec698de003a34e1d46752f9397f239fc331c7240
Florent Vilmart <florent@flovilmart.com>
2017-09-04 20:14:03 -0400

    reconfiguring winston would override the transports levels and make subsequent tests fail

--- spec/LoggerController.spec.js
+++ spec/LoggerController.spec.js
@@ -90,8 +90,8 @@ describe('LoggerController', () => {
   });
 
   it('should replace implementations with verbose', (done) => {
-    const adapter = new WinstonLoggerAdapter({ verbose: true});
-    const logger = new LoggerController(adapter, null, {verbose: true});
+    const adapter = new WinstonLoggerAdapter();
+    const logger = new LoggerController(adapter, null, {verbose: true });
     spyOn(adapter, "log");
     logger.silly('yo!');
     expect(adapter.log).not.toHaveBeenCalled();
@@ -99,7 +99,7 @@ describe('LoggerController', () => {
   });
 
   it('should replace implementations with logLevel', (done) => {
-    const adapter = new WinstonLoggerAdapter({ logLevel: 'error' });
+    const adapter = new WinstonLoggerAdapter();
     const logger = new LoggerController(adapter, null, { logLevel: 'error' });
     spyOn(adapter, "log");
     logger.warn('yo!');
