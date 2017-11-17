13f4b3358ab1082cbdb42c373cfcd8c5422da08d
張子晏 <try_love_tom@icloud.com>
2017-11-17 17:24:38 +0800

    Finished Testing

--- spec/ParseServer.spec.js
+++ spec/ParseServer.spec.js
@@ -33,16 +33,4 @@ describe('Server Url Checks', () => {
       done();
     });
   });
-
-  it('handleShutdown', (done) => {
-    const parseServer = ParseServer.start(defaultConfiguration)
-
-    parseServer.handleShutdown();
-    parseServer.server.close((err) => {
-      if (err) {
-        done.fail('Close server err');
-      }
-      done();
-    })
-  })
 });
