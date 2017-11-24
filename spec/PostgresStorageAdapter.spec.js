9ed3536ebdc3a9d0356bd802e49a85def96b086a
Paulo Reis <paulovitin@gmail.com>
2017-11-24 16:27:37 -0300

    ESLint fix and PostgresStorageAdapter.schemaUpgrade spec test
    Adds:
      - Add PostgresStorageAdapter.schemaUpgrade spec tests: creates a table,
        simulates the addition of a new field and checks if its present in the database
    Chores:
      - ESLint eol-last fix;

--- spec/PostgresStorageAdapter.spec.js
+++ spec/PostgresStorageAdapter.spec.js
@@ -1,6 +1,17 @@
 const PostgresStorageAdapter = require('../src/Adapters/Storage/Postgres/PostgresStorageAdapter');
 const databaseURI = 'postgres://localhost:5432/parse_server_postgres_adapter_test_database';
 
+const getColumns = (client, className) => {
+  return client.any(`SELECT column_name FROM information_schema.columns WHERE table_name = '${className}'`)
+    .then(columns => {
+      if (!columns) {
+        return [];
+      }
+
+      return columns.map(item => item.column_name);
+    });
+};
+
 describe_only_db('postgres')('PostgresStorageAdapter', () => {
   beforeEach(done => {
     const adapter = new PostgresStorageAdapter({ uri: databaseURI })
@@ -18,5 +29,30 @@ describe_only_db('postgres')('PostgresStorageAdapter', () => {
     adapter.handleShutdown();
     expect(adapter._client.$pool.ending).toEqual(true);
     done();
+  });
+
+  it('schemaUpgrade, upgrade the database schema when schema change', done => {
+    const adapter = new PostgresStorageAdapter({ uri: databaseURI });
+    const client = adapter._client;
+    const className = '_PushStatus';
+    const schema = {
+      fields: {
+        "pushTime": { type: 'String' },
+        "source": { type: 'String' }, // rest or webui
+        "query": { type: 'String' }, // the stringified JSON query
+      },
+    };
+
+    adapter.createTable(className, schema)
+      .then(() => {
+        schema.fields.expiration_interval = { type:'Number' };
+        return adapter.schemaUpgrade(className, schema);
+      })
+      .then(() => getColumns(client, className))
+      .then(columns => {
+        expect(columns).toContain('expiration_interval');
+        done();
+      })
+      .catch(error => done.fail(error));
   });
 });
