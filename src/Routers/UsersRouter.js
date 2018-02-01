40301ef53228422a29b7acb21c6e3abc572d492f
Benjamin Wilson Friedman <friedman.benjamin@gmail.com>
2018-02-01 12:48:05 -0800

    Remove password field when saving on postgres

--- src/Routers/UsersRouter.js
+++ src/Routers/UsersRouter.js
@@ -268,6 +268,9 @@ export class UsersRouter extends ClassesRouter {
         throw new Parse.Error(Parse.Error.EMAIL_NOT_FOUND, `No user found with email ${email}`);
       }
       const user = results[0];
+      
+      // remove password field, messes with saving on postgres
+      delete user.password;
 
       if (user.emailVerified) {
         throw new Parse.Error(Parse.Error.OTHER_CAUSE, `Email ${email} is already verified.`);
