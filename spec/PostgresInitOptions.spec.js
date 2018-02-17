1f741b45bf7c3d3f708227fa842bb6a706bbdebf
Florent Vilmart <florent@flovilmart.com>
2018-02-16 20:15:53 -0500

    Use same adapter for PG tests, drop table to ensure the tests dont side effect

--- spec/PostgresInitOptions.spec.js
+++ spec/PostgresInitOptions.spec.js
@@ -77,6 +77,6 @@ describe_only_db('postgres')('Postgres database init options', () => {
       databaseOptions: databaseOptions2
     })
 
-    createParseServer({ databaseAdapter: adapter }).then(done.fail, done);
+    createParseServer({ databaseAdapter: adapter }).then(done.fail, () => done());
   });
 });
