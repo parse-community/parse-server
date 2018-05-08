717aa4640db3090cad471f7bd01348a16e039e20
Johnny Dimas <johnny.dimas@plejd.com>
2018-05-08 21:58:08 +0200

    Refactored handleLogin and handleVerifyPassword to use shared helper function to validate the password provided in the request.

--- src/Routers/UsersRouter.js
+++ src/Routers/UsersRouter.js
@@ -1,11 +1,11 @@
 // These methods handle the User-related routes.
 
-import Parse          from 'parse/node';
-import Config         from '../Config';
+import Parse from 'parse/node';
+import Config from '../Config';
 import AccountLockout from '../AccountLockout';
-import ClassesRouter  from './ClassesRouter';
-import rest           from '../rest';
-import Auth           from '../Auth';
+import ClassesRouter from './ClassesRouter';
+import rest from '../rest';
+import Auth from '../Auth';
 import passwordCrypto from '../password';
 
 export class UsersRouter extends ClassesRouter {
@@ -18,7 +18,7 @@ export class UsersRouter extends ClassesRouter {
    * Removes all "_" prefixed properties from an object, except "__type"
    * @param {Object} obj An object.
    */
-  static removeHiddenProperties (obj) {
+  static removeHiddenProperties(obj) {
     for (var key in obj) {
       if (obj.hasOwnProperty(key)) {
         // Regexp comes from Parse.Object.prototype.validate
@@ -26,6 +26,123 @@ export class UsersRouter extends ClassesRouter {
           delete obj[key];
         }
       }
+    }
+  }
+
+  /**
+   * Validates a password request in login and verifyPassword
+   * @param {Object} req The request
+   * @returns {Object} User object
+   * @private
+   */
+  _handleValidatePassword(req) {
+    // Use query parameters instead if provided in url
+    let payload = req.body;
+    if (!payload.username && req.query.username || !payload.email && req.query.email) {
+      payload = req.query;
+    }
+    const {
+      username,
+      email,
+      password,
+    } = payload;
+
+    // TODO: use the right error codes / descriptions.
+    if (!username && !email) {
+      throw new Parse.Error(Parse.Error.USERNAME_MISSING, 'username/email is required.');
+    }
+    if (!password) {
+      throw new Parse.Error(Parse.Error.PASSWORD_MISSING, 'password is required.');
+    }
+    if (typeof password !== 'string'
+      || email && typeof email !== 'string'
+      || username && typeof username !== 'string') {
+      throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
+    }
+
+    let user;
+    let isValidPassword = false;
+    let query;
+    if (email && username) {
+      query = { email, username };
+    } else if (email) {
+      query = { email };
+    } else {
+      query = { $or: [{ username }, { email: username }] };
+    }
+
+    try {
+      return req.config.database.find('_User', query)
+        .then((results) => {
+          if (!results.length) {
+            throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
+          }
+
+          if (results.length > 1) { // corner case where user1 has username == user2 email
+            req.config.loggerController.warn('There is a user which email is the same as another user\'s username, logging in based on username');
+            user = results.filter((user) => user.username === username)[0];
+          } else {
+            user = results[0];
+          }
+
+          return passwordCrypto.compare(password, user.password);
+        })
+        .then((correct) => {
+          isValidPassword = correct;
+          const accountLockoutPolicy = new AccountLockout(user, req.config);
+          return accountLockoutPolicy.handleLoginAttempt(isValidPassword);
+        })
+        .then(() => {
+          if (!isValidPassword) {
+            throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
+          }
+          if (req.config.verifyUserEmails && req.config.preventLoginWithUnverifiedEmail && !user.emailVerified) {
+            throw new Parse.Error(Parse.Error.EMAIL_NOT_FOUND, 'User email is not verified.');
+          }
+          // handle password expiry policy
+          if (req.config.passwordPolicy && req.config.passwordPolicy.maxPasswordAge) {
+            let changedAt = user._password_changed_at;
+
+            if (!changedAt) {
+              // password was created before expiry policy was enabled.
+              // simply update _User object so that it will start enforcing from now
+              changedAt = new Date();
+              req.config.database.update('_User', { username: user.username },
+                { _password_changed_at: Parse._encode(changedAt) });
+            } else {
+              // check whether the password has expired
+              if (changedAt.__type == 'Date') {
+                changedAt = new Date(changedAt.iso);
+              }
+              // Calculate the expiry time.
+              const expiresAt = new Date(changedAt.getTime() + 86400000 * req.config.passwordPolicy.maxPasswordAge);
+              if (expiresAt < new Date()) // fail of current time is past password expiry time
+                throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Your password has expired. Please reset your password.');
+            }
+          }
+
+          delete user.password;
+
+          // Remove hidden properties.
+          UsersRouter.removeHiddenProperties(user);
+
+          // Sometimes the authData still has null on that keys
+          // https://github.com/parse-community/parse-server/issues/935
+          if (user.authData) {
+            Object.keys(user.authData).forEach((provider) => {
+              if (user.authData[provider] === null) {
+                delete user.authData[provider];
+              }
+            });
+            if (Object.keys(user.authData).length == 0) {
+              delete user.authData;
+            }
+          }
+
+          return user;
+        });
+    } catch (error) {
+      return error;
     }
   }
 
@@ -56,126 +173,49 @@ export class UsersRouter extends ClassesRouter {
   }
 
   handleLogIn(req) {
-    // Use query parameters instead if provided in url
-    let payload = req.body;
-    if (!payload.username && req.query.username || !payload.email && req.query.email) {
-      payload = req.query;
-    }
-    const {
-      username,
-      email,
-      password,
-    } = payload;
+    try {
+      let user;
+      return this._handleValidatePassword(req)
+        .then((res) => {
 
-    // TODO: use the right error codes / descriptions.
-    if (!username && !email) {
-      throw new Parse.Error(Parse.Error.USERNAME_MISSING, 'username/email is required.');
-    }
-    if (!password) {
-      throw new Parse.Error(Parse.Error.PASSWORD_MISSING, 'password is required.');
-    }
-    if (typeof password !== 'string'
-        || email && typeof email !== 'string'
-        || username && typeof username !== 'string') {
-      throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
-    }
+          user = res;
 
-    let user;
-    let isValidPassword = false;
-    let query;
-    if (email && username) {
-      query = { email, username };
-    } else if (email) {
-      query = { email };
-    } else {
-      query = { $or: [{ username } , { email: username }] };
-    }
-    return req.config.database.find('_User', query)
-      .then((results) => {
-        if (!results.length) {
-          throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
-        }
-
-        if (results.length > 1) { // corner case where user1 has username == user2 email
-          req.config.loggerController.warn('There is a user which email is the same as another user\'s username, logging in based on username');
-          user = results.filter((user) =>  user.username === username)[0];
-        } else {
-          user = results[0];
-        }
-
-        return passwordCrypto.compare(password, user.password);
-      })
-      .then((correct) => {
-        isValidPassword = correct;
-        const accountLockoutPolicy = new AccountLockout(user, req.config);
-        return accountLockoutPolicy.handleLoginAttempt(isValidPassword);
-      })
-      .then(() => {
-        if (!isValidPassword) {
-          throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
-        }
-        if (req.config.verifyUserEmails && req.config.preventLoginWithUnverifiedEmail && !user.emailVerified) {
-          throw new Parse.Error(Parse.Error.EMAIL_NOT_FOUND, 'User email is not verified.');
-        }
-        // handle password expiry policy
-        if (req.config.passwordPolicy && req.config.passwordPolicy.maxPasswordAge) {
-          let changedAt = user._password_changed_at;
-
-          if (!changedAt) {
-            // password was created before expiry policy was enabled.
-            // simply update _User object so that it will start enforcing from now
-            changedAt = new Date();
-            req.config.database.update('_User', {username: user.username},
-              {_password_changed_at: Parse._encode(changedAt)});
-          } else {
-            // check whether the password has expired
-            if (changedAt.__type == 'Date') {
-              changedAt = new Date(changedAt.iso);
-            }
-            // Calculate the expiry time.
-            const expiresAt = new Date(changedAt.getTime() + 86400000 * req.config.passwordPolicy.maxPasswordAge);
-            if (expiresAt < new Date()) // fail of current time is past password expiry time
-              throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Your password has expired. Please reset your password.');
-          }
-        }
-
-        delete user.password;
-
-        // Remove hidden properties.
-        UsersRouter.removeHiddenProperties(user);
-
-        // Sometimes the authData still has null on that keys
-        // https://github.com/parse-community/parse-server/issues/935
-        if (user.authData) {
-          Object.keys(user.authData).forEach((provider) => {
-            if (user.authData[provider] === null) {
-              delete user.authData[provider];
-            }
+          const {
+            sessionData,
+            createSession
+          } = Auth.createSession(req.config, {
+            userId: user.objectId, createdWith: {
+              'action': 'login',
+              'authProvider': 'password'
+            }, installationId: req.info.installationId
           });
-          if (Object.keys(user.authData).length == 0) {
-            delete user.authData;
-          }
-        }
-        const {
-          sessionData,
-          createSession
-        } = Auth.createSession(req.config, { userId: user.objectId, createdWith: {
-          'action': 'login',
-          'authProvider': 'password'
-        }, installationId: req.info.installationId });
 
-        user.sessionToken = sessionData.sessionToken;
+          user.sessionToken = sessionData.sessionToken;
 
-        req.config.filesController.expandFilesInObject(req.config, user);
+          req.config.filesController.expandFilesInObject(req.config, user);
 
-        return createSession();
-      }).then(() => {
-        return { response: user };
-      });
+          return createSession();
+        }).then(() => {
+          return { response: user };
+        });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  handleVerifyPassword(req) {
+    try {
+      return this._handleValidatePassword(req)
+        .then((user) => {
+          return { response: user };
+        });
+    } catch (error) {
+      throw error;
+    }
   }
 
   handleLogOut(req) {
-    const success = {response: {}};
+    const success = { response: {} };
     if (req.info && req.info.sessionToken) {
       return rest.find(req.config, Auth.master(req.config), '_Session',
         { sessionToken: req.info.sessionToken }, undefined, req.info.clientSDK
@@ -265,78 +305,6 @@ export class UsersRouter extends ClassesRouter {
         return { response: {} };
       });
     });
-  }
-
-  handleVerifyPassword(req) {
-    // Use query parameters instead if provided in url
-    let payload = req.body;
-    if (!payload.username && req.query.username || !payload.email && req.query.email) {
-      payload = req.query;
-    }
-    const {
-      username,
-      email,
-      password,
-    } = payload;
-
-    // TODO: use the right error codes / descriptions.
-    if (!username && !email) {
-      throw new Parse.Error(Parse.Error.USERNAME_MISSING, 'username/email is required.');
-    }
-    if (!password) {
-      throw new Parse.Error(Parse.Error.PASSWORD_MISSING, 'password is required.');
-    }
-    if (typeof password !== 'string'
-        || email && typeof email !== 'string'
-        || username && typeof username !== 'string') {
-      throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
-    }
-
-    let user;
-    let isValidPassword = false;
-    let query;
-    if (email && username) {
-      query = { email, username };
-    } else if (email) {
-      query = { email };
-    } else {
-      query = { $or: [{ username } , { email: username }] };
-    }
-    return req.config.database.find('_User', query)
-      .then((results) => {
-        if (!results.length) {
-          throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
-        }
-
-        if (results.length > 1) { // corner case where user1 has username == user2 email
-          req.config.loggerController.warn('There is a user which email is the same as another user\'s username, logging in based on username');
-          user = results.filter((user) =>  user.username === username)[0];
-        } else {
-          user = results[0];
-        }
-
-        return passwordCrypto.compare(password, user.password);
-      })
-      .then((correct) => {
-        isValidPassword = correct;
-        const accountLockoutPolicy = new AccountLockout(user, req.config);
-        return accountLockoutPolicy.handleLoginAttempt(isValidPassword);
-      })
-      .then(() => {
-        if (!isValidPassword) {
-          throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid username/password.');
-        }
-        if (req.config.verifyUserEmails && req.config.preventLoginWithUnverifiedEmail && !user.emailVerified) {
-          throw new Parse.Error(Parse.Error.EMAIL_NOT_FOUND, 'User email is not verified.');
-        }
-
-        delete user.password;
-
-        // Remove hidden properties.
-        UsersRouter.removeHiddenProperties(user);
-
-        return { response: user };
-      });
   }
 
   mountRoutes() {
