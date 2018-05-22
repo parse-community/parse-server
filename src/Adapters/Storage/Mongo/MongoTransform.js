e44e33e9de1cfee6e715325c7cab774e96738f94
Mads Bjerre <mads@madsb.com>
2018-05-22 18:06:43 +0200

    Adds withinPolygon support for Polygon object (#4067)
    * Whitespace
    
    * Add Polygon type to $polygon query
    
    * Add tests
    
    Polygon object in $polygon query
    $geoIntersects queries
    
    * Refactor
    
    * Postgres support
    
    * More tests
    
    * Remove duplicate test
    
    * Missing semicolon
    
    * fix tests

--- src/Adapters/Storage/Mongo/MongoTransform.js
+++ src/Adapters/Storage/Mongo/MongoTransform.js
@@ -905,19 +905,34 @@ function transformConstraint(constraint, field) {
 
     case '$geoWithin': {
       const polygon = constraint[key]['$polygon'];
-      if (!(polygon instanceof Array)) {
+      let points;
+      if (typeof polygon === 'object' && polygon.__type === 'Polygon') {
+        if (!polygon.coordinates || polygon.coordinates.length < 3) {
+          throw new Parse.Error(
+            Parse.Error.INVALID_JSON,
+            'bad $geoWithin value; Polygon.coordinates should contain at least 3 lon/lat pairs'
+          );
+        }
+        points = polygon.coordinates;
+      } else if (polygon instanceof Array) {
+        if (polygon.length < 3) {
+          throw new Parse.Error(
+            Parse.Error.INVALID_JSON,
+            'bad $geoWithin value; $polygon should contain at least 3 GeoPoints'
+          );
+        }
+        points = polygon;
+      } else {
         throw new Parse.Error(
           Parse.Error.INVALID_JSON,
-          'bad $geoWithin value; $polygon should contain at least 3 GeoPoints'
+          'bad $geoWithin value; $polygon should be Polygon object or Array of Parse.GeoPoint\'s'
         );
       }
-      if (polygon.length < 3) {
-        throw new Parse.Error(
-          Parse.Error.INVALID_JSON,
-          'bad $geoWithin value; $polygon should contain at least 3 GeoPoints'
-        );
-      }
-      const points = polygon.map((point) => {
+      points = points.map((point) => {
+        if (point instanceof Array && point.length === 2) {
+          Parse.GeoPoint._validate(point[1], point[0]);
+          return point;
+        }
         if (!GeoPointCoder.isValidJSON(point)) {
           throw new Parse.Error(Parse.Error.INVALID_JSON, 'bad $geoWithin value');
         } else {
