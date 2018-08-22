97f0fdced7b53510188464682cac218495ffd73a
Florent Vilmart <364568+flovilmart@users.noreply.github.com>
2018-08-22 19:13:21 -0400

    Adds common constraints on Number and String

--- src/graphql/types/QueryConstraint.js
+++ src/graphql/types/QueryConstraint.js
@@ -1,16 +1,11 @@
 import {
-  GraphQLScalarType,
   Kind
 } from 'graphql'
 
 const supportedOperators = ['eq', 'ne', 'in', 'nin', 'exists', 'select', 'dontSelect']
 
-export const QueryConstraint = new GraphQLScalarType({
-  name: 'QueryConstraint',
-  description: `Generic Query Constraint
-  Supported constraints:
-  
-  Equal To:
+function description() {
+  return `Equal To:
   - key: "value"
   - key: {eq: "value"}
   
@@ -30,28 +25,32 @@ export const QueryConstraint = new GraphQLScalarType({
 
   Requires that a key’s value not match a value for a key in the result of a different query
   - key: {dontSelect: {"query": {"className":"Team","where":{"winPct":{"$gt":0.5}}},"key":"city"}}}
-  `,
-  serialize: () => {
-    throw "QueryConstraint serialize not implemented"
-  },
-  parseValue: () => {
-    throw "QueryConstraint parseValue not implemented"
-  },
-  parseLiteral: (ast) => {
-    if (ast.kind == Kind.OBJECT) {
-      const fields = ast.fields;
-      return fields.reduce((memo, field) => {
-        const operator = field.name.value;
-        if (supportedOperators.indexOf(operator) > -1) {
-          const value = field.value.value;
-          memo['$' + operator] = value;
-        }
-        return memo;
-      }, {});
-    } else if (ast.kind == Kind.STRING) {
-      return ast.value;
-    } else {
-      throw 'Invalid literal for QueryConstraint';
+  `;
+}
+
+const parseFields = (fields) => {
+  return fields.reduce((memo, field) => {
+    const operator = field.name.value;
+    if (supportedOperators.indexOf(operator) > -1) {
+      const value = field.value.value;
+      memo['$' + operator] = value;
     }
+    return memo;
+  }, {});
+}
+
+const parseLiteral = (ast) => {
+  if (ast.kind == Kind.OBJECT) {
+    return parseFields(ast.fields);
+  } else if (ast.kind == Kind.STRING) {
+    return ast.value;
+  } else {
+    throw 'Invalid literal for QueryConstraint';
   }
-});
+};
+
+export default {
+  description,
+  parseLiteral,
+  parseFields,
+}
