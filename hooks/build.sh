a21c2ecde59decae8e0f1206628af1239e90047f
Barak Ben-David <barakbd@outlook.com>
2018-12-17 16:04:33 -0800

    change echo

--- hooks/build.sh
+++ hooks/build.sh
@@ -2,5 +2,5 @@
 # https://docs.docker.com/docker-hub/builds/advanced/#environment-variables-for-building-and-testing
 
 #!/bin/bash
-echo "=> Building the binary"
+echo "=> Building the binary with label"
 docker build --label SOURCE_COMMIT=$SOURCE_COMMIT -f $DOCKERFILE_PATH -t $IMAGE_NAME .
