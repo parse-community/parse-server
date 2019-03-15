63618a5ce65ca5317ee413331c8eebdacb49b196
Georges <jamousgeorges@outlook.com>
2019-03-15 11:15:49 +0200

    some tests

--- spec/RedisCacheAdapter.spec.js
+++ spec/RedisCacheAdapter.spec.js
@@ -58,3 +58,68 @@ describe_only(() => {
       .then(done);
   });
 });
+
+describe_only(() => {
+  return process.env.PARSE_SERVER_TEST_CACHE === 'redis';
+})('RedisCacheAdapter KeyPromiseQueue', function() {
+  const KEY1 = 'key1';
+  const KEY2 = 'key2';
+  const VALUE = 'hello';
+
+  // number of chained ops on a single key
+  function getQueueCountForKey(cache, key) {
+    return cache.queue.queue[key][0];
+  }
+
+  // total number of queued keys
+  function getQueueCount(cache) {
+    return Object.keys(cache.queue.queue).length;
+  }
+
+  it('it should clear completed operations', done => {
+    const cache = new RedisCacheAdapter({
+      ttl: NaN,
+    });
+
+    let promise = Promise.resolve();
+
+    // execute a bunch of operations
+    for (let index = 1; index < 100; index++) {
+      promise = promise.then(() => {
+        const key = `${index}`;
+        return cache
+          .put(key, VALUE)
+          .then(() => expect(getQueueCount(cache)).toEqual(0))
+          .then(() => cache.get(key))
+          .then(() => expect(getQueueCount(cache)).toEqual(0))
+          .then(() => cache.clear())
+          .then(() => expect(getQueueCount(cache)).toEqual(0));
+      });
+    }
+
+    // at the end the queue should be empty
+    promise.then(() => expect(getQueueCount(cache)).toEqual(0)).then(done);
+  });
+
+  it('it should count chained operations correctly', done => {
+    const cache = new RedisCacheAdapter({
+      ttl: NaN,
+    });
+
+    let promise1 = Promise.resolve();
+    let promise2 = Promise.resolve();
+
+    for (let index = 1; index < 100; index++) {
+      promise1 = cache.put(KEY1, VALUE);
+      promise2 = cache.put(KEY2, VALUE);
+      expect(getQueueCountForKey(cache, KEY1)).toEqual(index);
+      expect(getQueueCountForKey(cache, KEY2)).toEqual(index);
+      expect(getQueueCount(cache)).toEqual(2);
+    }
+
+    // at the end the queue should be empty
+    Promise.all([promise1, promise2])
+      .then(() => expect(getQueueCount(cache)).toEqual(0))
+      .then(done);
+  });
+});
