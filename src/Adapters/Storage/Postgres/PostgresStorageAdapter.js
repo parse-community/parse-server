ff360f712b2e0a6c910122db5b64a4ad700d339f
Diamond Lewis <findlewis@gmail.com>
2019-04-30 22:40:49 -0500

    Postgres: Query notEqualTo GeoPoint

--- src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
+++ src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
@@ -360,15 +360,26 @@ const buildWhereClause = ({ schema, query, index }): WhereClause => {
           continue;
         } else {
           // if not null, we need to manually exclude null
-          patterns.push(
-            `($${index}:name <> $${index + 1} OR $${index}:name IS NULL)`
-          );
+          if (fieldValue.$ne.__type === 'GeoPoint') {
+            patterns.push(
+              `($${index}:name <> $${index + 1}:raw OR $${index}:name IS NULL)`
+            );
+          } else {
+            patterns.push(
+              `($${index}:name <> $${index + 1} OR $${index}:name IS NULL)`
+            );
+          }
         }
       }
-
-      // TODO: support arrays
-      values.push(fieldName, fieldValue.$ne);
-      index += 2;
+      if (fieldValue.$ne.__type === 'GeoPoint') {
+        const point = fieldValue.$ne;
+        values.push(fieldName, `POINT(${point.longitude}, ${point.latitude})`);
+        index += 2;
+      } else {
+        // TODO: support arrays
+        values.push(fieldName, fieldValue.$ne);
+        index += 2;
+      }
     }
     if (fieldValue.$eq !== undefined) {
       if (fieldValue.$eq === null) {
@@ -730,15 +741,7 @@ const buildWhereClause = ({ schema, query, index }): WhereClause => {
     }
 
     if (fieldValue.__type === 'GeoPoint') {
-      patterns.push(
-        '$' +
-          index +
-          ':name ~= POINT($' +
-          (index + 1) +
-          ', $' +
-          (index + 2) +
-          ')'
-      );
+      patterns.push(`$${index}:name ~= POINT($${index + 1}, $${index + 2})`);
       values.push(fieldName, fieldValue.longitude, fieldValue.latitude);
       index += 3;
     }
