28fe08dbac10329e37aec9845181809fb588396c
= <adavimacedo@gmail.com>
2019-05-02 18:09:22 -0700

    Parse class delete mutation

--- src/GraphQL/loaders/parseClassMutations.js
+++ src/GraphQL/loaders/parseClassMutations.js
@@ -1,4 +1,4 @@
-import { GraphQLNonNull } from 'graphql';
+import { GraphQLNonNull, GraphQLID, GraphQLBoolean } from 'graphql';
 import * as defaultGraphQLTypes from './defaultGraphQLTypes';
 import rest from '../../rest';
 
@@ -19,6 +19,27 @@ const load = (parseGraphQLSchema, parseClass) => {
 
       return (await rest.create(config, auth, className, args, info.clientSDK))
         .response;
+    },
+  };
+
+  const deleteGraphQLMutationName = `delete${className}`;
+  parseGraphQLSchema.graphQLMutations[deleteGraphQLMutationName] = {
+    description: `The ${deleteGraphQLMutationName} mutation can be used to delete an object of the ${className} class.`,
+    args: {
+      objectId: {
+        description: 'This is the objectIt of the object to be deleted.',
+        type: new GraphQLNonNull(GraphQLID),
+      },
+    },
+    type: new GraphQLNonNull(GraphQLBoolean),
+    async resolve(_source, args, context) {
+      const { objectId } = args;
+
+      const { config, auth, info } = context;
+
+      await rest.del(config, auth, className, objectId, info.clientSDK);
+
+      return true;
     },
   };
 };
