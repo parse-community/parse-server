25b48f7c35b167e3729520d2cc0d1961c49fca06
= <adavimacedo@gmail.com>
2019-05-02 18:49:17 -0700

    Parse class update mutation

--- src/GraphQL/loaders/parseClassMutations.js
+++ src/GraphQL/loaders/parseClassMutations.js
@@ -5,10 +5,13 @@ import rest from '../../rest';
 const load = (parseGraphQLSchema, parseClass) => {
   const className = parseClass.className;
 
+  const classGraphQLCustomFields =
+    parseGraphQLSchema.parseClassTypes[className].classGraphQLCustomFields;
+
   const createGraphQLMutationName = `create${className}`;
   parseGraphQLSchema.graphQLMutations[createGraphQLMutationName] = {
     description: `The ${createGraphQLMutationName} mutation can be used to create a new object of the ${className} class.`,
-    args: defaultGraphQLTypes.classGraphQLCustomFields,
+    args: classGraphQLCustomFields,
     type: new GraphQLNonNull(defaultGraphQLTypes.CREATE_RESULT),
     async resolve(_source, args, context) {
       if (!args) {
@@ -19,6 +22,32 @@ const load = (parseGraphQLSchema, parseClass) => {
 
       return (await rest.create(config, auth, className, args, info.clientSDK))
         .response;
+    },
+  };
+
+  const updateGraphQLMutationName = `update${className}`;
+  parseGraphQLSchema.graphQLMutations[updateGraphQLMutationName] = {
+    description: `The ${updateGraphQLMutationName} mutation can be used to update an object of the ${className} class.`,
+    args: {
+      objectId: defaultGraphQLTypes.CLASS_FIELDS.objectId,
+      ...classGraphQLCustomFields,
+    },
+    type: new GraphQLNonNull(GraphQLBoolean),
+    async resolve(_source, args, context) {
+      const { objectId, ...fields } = args;
+
+      const { config, auth, info } = context;
+
+      await rest.update(
+        config,
+        auth,
+        className,
+        { objectId },
+        fields,
+        info.clientSDK
+      );
+
+      return true;
     },
   };
 
