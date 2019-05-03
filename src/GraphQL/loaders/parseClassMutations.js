57da6964cdbe49ae67473898bfd5e995b130688a
= <adavimacedo@gmail.com>
2019-05-02 17:28:54 -0700

    Adding more data types to the class

--- src/GraphQL/loaders/parseClassMutations.js
+++ src/GraphQL/loaders/parseClassMutations.js
@@ -1,50 +1,13 @@
-import {
-  GraphQLNonNull,
-  GraphQLString,
-  GraphQLFloat,
-  GraphQLBoolean,
-  GraphQLList,
-} from 'graphql';
+import { GraphQLNonNull } from 'graphql';
 import * as defaultGraphQLTypes from './defaultGraphQLTypes';
 import rest from '../../rest';
-
-const mapType = parseType => {
-  //console.log(parseType);
-  switch (parseType) {
-    case 'String':
-      return GraphQLString;
-    case 'Number':
-      return GraphQLFloat;
-    case 'Boolean':
-      return GraphQLBoolean;
-    case 'Array':
-      return GraphQLList;
-    case 'Object':
-      return defaultGraphQLTypes.OBJECT;
-    case 'Date':
-      return defaultGraphQLTypes.DATE;
-  }
-};
 
 const load = (parseGraphQLSchema, parseClass) => {
   const className = parseClass.className;
   const createGraphQLMutationName = `create${className}`;
   parseGraphQLSchema.graphQLMutations[createGraphQLMutationName] = {
     description: `The ${createGraphQLMutationName} mutation can be used to create a new object of the ${className} class.`,
-    args: Object.keys(parseClass.fields)
-      .filter(
-        field => !['objectId', 'ACL', 'createdAt', 'updatedAt'].includes(field)
-      )
-      .reduce(
-        (args, field) => ({
-          ...args,
-          [field]: {
-            description: `This is the object ${field}.`,
-            type: mapType(parseClass.fields[field].type),
-          },
-        }),
-        {}
-      ),
+    args: defaultGraphQLTypes.classGraphQLCustomFields,
     type: new GraphQLNonNull(defaultGraphQLTypes.CREATE_RESULT),
     async resolve(_source, args, context) {
       if (!args) {
--- src/GraphQL/loaders/parseClassTypes.js
+++ src/GraphQL/loaders/parseClassTypes.js
@@ -1,8 +1,46 @@
-import { GraphQLObjectType } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLBoolean,
+  GraphQLList,
+} from 'graphql';
 import * as defaultGraphQLTypes from './defaultGraphQLTypes';
+
+const mapType = parseType => {
+  switch (parseType) {
+    case 'String':
+      return GraphQLString;
+    case 'Number':
+      return GraphQLFloat;
+    case 'Boolean':
+      return GraphQLBoolean;
+    case 'Array':
+      return GraphQLList;
+    case 'Object':
+      return defaultGraphQLTypes.OBJECT;
+    case 'Date':
+      return defaultGraphQLTypes.DATE;
+  }
+};
 
 const load = (parseGraphQLSchema, parseClass) => {
   const className = parseClass.className;
+
+  const classGraphQLCustomFields = Object.keys(parseClass.fields)
+    .filter(
+      field => !Object.keys(defaultGraphQLTypes.CLASS_FIELDS).includes(field)
+    )
+    .reduce(
+      (args, field) => ({
+        ...args,
+        [field]: {
+          description: `This is the object ${field}.`,
+          type: mapType(parseClass.fields[field].type),
+        },
+      }),
+      {}
+    );
 
   const classGraphQLTypeName = `${className}Class`;
   const classGraphQLType = new GraphQLObjectType({
@@ -11,12 +49,14 @@ const load = (parseGraphQLSchema, parseClass) => {
     interfaces: [defaultGraphQLTypes.CLASS],
     fields: {
       ...defaultGraphQLTypes.CLASS_FIELDS,
+      ...classGraphQLCustomFields,
     },
   });
 
   parseGraphQLSchema.parseClassTypes = {
     [className]: {
       classGraphQLType,
+      classGraphQLCustomFields,
     },
   };
 
