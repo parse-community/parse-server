f106cdf627375600b05da080eba17c87a285c2cd
= <adavimacedo@gmail.com>
2019-05-23 16:25:16 -0700

    Error handler for default mutations

--- src/GraphQL/loaders/defaultGraphQLMutations.js
+++ src/GraphQL/loaders/defaultGraphQLMutations.js
@@ -7,102 +7,116 @@ import {
 import * as defaultGraphQLTypes from './defaultGraphQLTypes';
 import rest from '../../rest';
 
-const CREATE = {
-  description:
-    'The create mutation can be used to create a new object of a certain class.',
-  args: {
-    className: {
-      description: 'This is the class name of the new object.',
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    fields: {
-      description: 'These are the fields to be attributed to the new object.',
-      type: defaultGraphQLTypes.OBJECT,
-    },
-  },
-  type: new GraphQLNonNull(defaultGraphQLTypes.CREATE_RESULT),
-  async resolve(_source, args, context) {
-    const { className } = args;
-    let { fields } = args;
-    const { config, auth, info } = context;
-
-    if (!fields) {
-      fields = {};
-    }
-
-    return (await rest.create(config, auth, className, fields, info.clientSDK))
-      .response;
-  },
-};
-
-const UPDATE = {
-  description:
-    'The update mutation can be used to update an object of a certain class.',
-  args: {
-    className: {
-      description: 'This is the class name of the object that will be updated.',
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    objectId: {
-      description: 'This is the objectId of the object that will be updated',
-      type: new GraphQLNonNull(GraphQLID),
-    },
-    fields: {
-      description:
-        'These are the fields to be attributed to the object in the update process.',
-      type: defaultGraphQLTypes.OBJECT,
-    },
-  },
-  type: new GraphQLNonNull(defaultGraphQLTypes.UPDATE_RESULT),
-  async resolve(_source, args, context) {
-    const { className, objectId } = args;
-    let { fields } = args;
-    const { config, auth, info } = context;
-
-    if (!fields) {
-      fields = {};
-    }
-
-    return (await rest.update(
-      config,
-      auth,
-      className,
-      { objectId },
-      fields,
-      info.clientSDK
-    )).response;
-  },
-};
-
-const DELETE = {
-  description:
-    'The delete mutation can be used to delete an object of a certain class.',
-  args: {
-    className: {
-      description: 'This is the class name of the object to be deleted.',
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    objectId: {
-      description: 'This is the objectIt of the object to be deleted.',
-      type: new GraphQLNonNull(GraphQLID),
-    },
-  },
-  type: new GraphQLNonNull(GraphQLBoolean),
-  async resolve(_source, args, context) {
-    const { className, objectId } = args;
-
-    const { config, auth, info } = context;
-
-    await rest.del(config, auth, className, objectId, info.clientSDK);
-
-    return true;
-  },
-};
-
 const load = parseGraphQLSchema => {
-  parseGraphQLSchema.graphQLMutations.create = CREATE;
-  parseGraphQLSchema.graphQLMutations.update = UPDATE;
-  parseGraphQLSchema.graphQLMutations.delete = DELETE;
+  parseGraphQLSchema.graphQLMutations.create = {
+    description:
+      'The create mutation can be used to create a new object of a certain class.',
+    args: {
+      className: {
+        description: 'This is the class name of the new object.',
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      fields: {
+        description: 'These are the fields to be attributed to the new object.',
+        type: defaultGraphQLTypes.OBJECT,
+      },
+    },
+    type: new GraphQLNonNull(defaultGraphQLTypes.CREATE_RESULT),
+    async resolve(_source, args, context) {
+      try {
+        const { className } = args;
+        let { fields } = args;
+        const { config, auth, info } = context;
+
+        if (!fields) {
+          fields = {};
+        }
+
+        return (await rest.create(
+          config,
+          auth,
+          className,
+          fields,
+          info.clientSDK
+        )).response;
+      } catch (e) {
+        parseGraphQLSchema.handleError(e);
+      }
+    },
+  };
+
+  parseGraphQLSchema.graphQLMutations.update = {
+    description:
+      'The update mutation can be used to update an object of a certain class.',
+    args: {
+      className: {
+        description:
+          'This is the class name of the object that will be updated.',
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      objectId: {
+        description: 'This is the objectId of the object that will be updated',
+        type: new GraphQLNonNull(GraphQLID),
+      },
+      fields: {
+        description:
+          'These are the fields to be attributed to the object in the update process.',
+        type: defaultGraphQLTypes.OBJECT,
+      },
+    },
+    type: new GraphQLNonNull(defaultGraphQLTypes.UPDATE_RESULT),
+    async resolve(_source, args, context) {
+      try {
+        const { className, objectId } = args;
+        let { fields } = args;
+        const { config, auth, info } = context;
+
+        if (!fields) {
+          fields = {};
+        }
+
+        return (await rest.update(
+          config,
+          auth,
+          className,
+          { objectId },
+          fields,
+          info.clientSDK
+        )).response;
+      } catch (e) {
+        parseGraphQLSchema.handleError(e);
+      }
+    },
+  };
+
+  parseGraphQLSchema.graphQLMutations.delete = {
+    description:
+      'The delete mutation can be used to delete an object of a certain class.',
+    args: {
+      className: {
+        description: 'This is the class name of the object to be deleted.',
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      objectId: {
+        description: 'This is the objectIt of the object to be deleted.',
+        type: new GraphQLNonNull(GraphQLID),
+      },
+    },
+    type: new GraphQLNonNull(GraphQLBoolean),
+    async resolve(_source, args, context) {
+      try {
+        const { className, objectId } = args;
+
+        const { config, auth, info } = context;
+
+        await rest.del(config, auth, className, objectId, info.clientSDK);
+
+        return true;
+      } catch (e) {
+        parseGraphQLSchema.handleError(e);
+      }
+    },
+  };
 };
 
 export { load };
