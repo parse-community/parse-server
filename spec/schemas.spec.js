2d8d13bb3609c77f3feda6db09c440427a760f59
= <adavimacedo@gmail.com>
2019-06-13 10:40:13 -0700

    Fix 5651

--- spec/schemas.spec.js
+++ spec/schemas.spec.js
@@ -239,9 +239,13 @@ describe('schemas', () => {
             ],
           };
           expect(
-            response.data.results.sort((s1, s2) =>
-              s1.className.localeCompare(s2.className)
-            )
+            response.data.results
+              .sort((s1, s2) => s1.className.localeCompare(s2.className))
+              .map(s => {
+                const withoutIndexes = Object.assign({}, s);
+                delete withoutIndexes.indexes;
+                return withoutIndexes;
+              })
           ).toEqual(
             expected.results.sort((s1, s2) =>
               s1.className.localeCompare(s2.className)
