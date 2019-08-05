7ebca63d3b2426d04378a05e114ae6d95a2b461e
Antoine Cormouls <contact.antoine.cormouls@gmail.com>
2019-08-05 02:15:58 +0200

    Add Native Type to avoid collision

--- src/GraphQL/transformers/className.js
+++ src/GraphQL/transformers/className.js
@@ -3,8 +3,18 @@ import logger from '../../logger';
 const parseMap = {
   Role: '_Role',
   User: '_User',
-  Files: 'Files',
   Session: '_Session',
+  // Add native type to avoid name collision
+  Files: 'Files',
+  String: 'String',
+  Boolean: 'Boolean',
+  Number: 'Number',
+  Array: 'Array',
+  Object: 'Object',
+  Date: 'Date',
+  GeoPoint: 'GeoPoint',
+  Polygon: 'Polygon',
+  Bytes: 'Bytes',
 };
 const reverseParseMap = {};
 // Dynamically fill reverseParseMap based on parseMap
@@ -13,7 +23,7 @@ Object.keys(parseMap).forEach(key => (reverseParseMap[parseMap[key]] = key));
 const transformClassNameToParse = className => {
   if (reverseParseMap[className])
     logger.error(
-      `Native Parse Class collision detected, please change the name of your class ${className}`
+      `Class collision detected, please change the name of your class ${className}`
     );
   return parseMap[className] ? parseMap[className] : className;
 };
@@ -21,7 +31,7 @@ const transformClassNameToParse = className => {
 const transformClassNameToGraphQL = className => {
   if (parseMap[className])
     logger.error(
-      `Native Parse Class collision detected, please change the name of your class ${className}`
+      `Class collision detected, please change the name of your class ${className}`
     );
   return reverseParseMap[className] ? reverseParseMap[className] : className;
 };
