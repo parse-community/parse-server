23123c93f72189566edd6430c2a4439f3d99b32d
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2019-08-07 23:11:06 -0700

    Create file mutation

--- src/GraphQL/loaders/filesMutations.js
+++ src/GraphQL/loaders/filesMutations.js
@@ -1,5 +1,6 @@
 import { GraphQLObjectType, GraphQLNonNull } from 'graphql';
 import { GraphQLUpload } from 'graphql-upload';
+import { mutationWithClientMutationId } from 'graphql-relay';
 import Parse from 'parse/node';
 import * as defaultGraphQLTypes from './defaultGraphQLTypes';
 import logger from '../../logger';
@@ -7,74 +8,94 @@ import logger from '../../logger';
 const load = parseGraphQLSchema => {
   const fields = {};
 
-  fields.create = {
-    description:
-      'The create mutation can be used to create and upload a new file.',
-    args: {
-      file: {
-        description: 'This is the new file to be created and uploaded',
-        type: new GraphQLNonNull(GraphQLUpload),
-      },
-    },
-    type: new GraphQLNonNull(defaultGraphQLTypes.FILE_INFO),
-    async resolve(_source, args, context) {
-      try {
-        const { file } = args;
-        const { config } = context;
-
-        const { createReadStream, filename, mimetype } = await file;
-        let data = null;
-        if (createReadStream) {
-          const stream = createReadStream();
-          data = await new Promise((resolve, reject) => {
-            let data = '';
-            stream
-              .on('error', reject)
-              .on('data', chunk => (data += chunk))
-              .on('end', () => resolve(data));
-          });
-        }
-
-        if (!data || !data.length) {
-          throw new Parse.Error(
-            Parse.Error.FILE_SAVE_ERROR,
-            'Invalid file upload.'
-          );
-        }
-
-        if (filename.length > 128) {
-          throw new Parse.Error(
-            Parse.Error.INVALID_FILE_NAME,
-            'Filename too long.'
-          );
-        }
-
-        if (!filename.match(/^[_a-zA-Z0-9][a-zA-Z0-9@\.\ ~_-]*$/)) {
-          throw new Parse.Error(
-            Parse.Error.INVALID_FILE_NAME,
-            'Filename contains invalid characters.'
-          );
-        }
-
-        try {
-          return await config.filesController.createFile(
-            config,
-            filename,
-            data,
-            mimetype
-          );
-        } catch (e) {
-          logger.error('Error creating a file: ', e);
-          throw new Parse.Error(
-            Parse.Error.FILE_SAVE_ERROR,
-            `Could not store file: ${filename}.`
-          );
-        }
-      } catch (e) {
-        parseGraphQLSchema.handleError(e);
-      }
+  const description =
+    'The create mutation can be used to create and upload a new file.';
+  const args = {
+    file: {
+      description: 'This is the new file to be created and uploaded',
+      type: new GraphQLNonNull(GraphQLUpload),
     },
   };
+  const type = new GraphQLNonNull(defaultGraphQLTypes.FILE_INFO);
+  const resolve = async (_source, args, context) => {
+    try {
+      const { file } = args;
+      const { config } = context;
+
+      const { createReadStream, filename, mimetype } = await file;
+      let data = null;
+      if (createReadStream) {
+        const stream = createReadStream();
+        data = await new Promise((resolve, reject) => {
+          let data = '';
+          stream
+            .on('error', reject)
+            .on('data', chunk => (data += chunk))
+            .on('end', () => resolve(data));
+        });
+      }
+
+      if (!data || !data.length) {
+        throw new Parse.Error(
+          Parse.Error.FILE_SAVE_ERROR,
+          'Invalid file upload.'
+        );
+      }
+
+      if (filename.length > 128) {
+        throw new Parse.Error(
+          Parse.Error.INVALID_FILE_NAME,
+          'Filename too long.'
+        );
+      }
+
+      if (!filename.match(/^[_a-zA-Z0-9][a-zA-Z0-9@\.\ ~_-]*$/)) {
+        throw new Parse.Error(
+          Parse.Error.INVALID_FILE_NAME,
+          'Filename contains invalid characters.'
+        );
+      }
+
+      try {
+        return await config.filesController.createFile(
+          config,
+          filename,
+          data,
+          mimetype
+        );
+      } catch (e) {
+        logger.error('Error creating a file: ', e);
+        throw new Parse.Error(
+          Parse.Error.FILE_SAVE_ERROR,
+          `Could not store file: ${filename}.`
+        );
+      }
+    } catch (e) {
+      parseGraphQLSchema.handleError(e);
+    }
+  };
+
+  let createField;
+  if (parseGraphQLSchema.graphQLSchemaIsRelayStyle) {
+    createField = mutationWithClientMutationId({
+      name: 'CreateFile',
+      inputFields: args,
+      outputFields: {
+        fileInfo: { type },
+      },
+      mutateAndGetPayload: (object, ctx) => resolve(undefined, object, ctx),
+    });
+    parseGraphQLSchema.graphQLTypes.push(createField.args.input.type);
+    parseGraphQLSchema.graphQLTypes.push(createField.type);
+  } else {
+    createField = {
+      description,
+      args,
+      type,
+      resolve,
+    };
+  }
+  fields.create = createField;
 
   const filesMutation = new GraphQLObjectType({
     name: 'FilesMutation',
