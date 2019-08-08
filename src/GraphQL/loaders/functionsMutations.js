bde9a588a44fe49385192c2529710917545f81b9
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2019-08-08 13:56:19 -0700

    Relay style for functions mutations

--- src/GraphQL/loaders/functionsMutations.js
+++ src/GraphQL/loaders/functionsMutations.js
@@ -1,43 +1,64 @@
 import { GraphQLObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
+import { mutationWithClientMutationId } from 'graphql-relay';
 import { FunctionsRouter } from '../../Routers/FunctionsRouter';
 import * as defaultGraphQLTypes from './defaultGraphQLTypes';
 
 const load = parseGraphQLSchema => {
   const fields = {};
 
-  fields.call = {
-    description:
-      'The call mutation can be used to invoke a cloud code function.',
-    args: {
-      functionName: {
-        description: 'This is the name of the function to be called.',
-        type: new GraphQLNonNull(GraphQLString),
-      },
-      params: {
-        description: 'These are the params to be passed to the function.',
-        type: defaultGraphQLTypes.OBJECT,
-      },
+  const description =
+    'The call mutation can be used to invoke a cloud code function.';
+  const args = {
+    functionName: {
+      description: 'This is the name of the function to be called.',
+      type: new GraphQLNonNull(GraphQLString),
     },
-    type: defaultGraphQLTypes.ANY,
-    async resolve(_source, args, context) {
-      try {
-        const { functionName, params } = args;
-        const { config, auth, info } = context;
-
-        return (await FunctionsRouter.handleCloudFunction({
-          params: {
-            functionName,
-          },
-          config,
-          auth,
-          info,
-          body: params,
-        })).response.result;
-      } catch (e) {
-        parseGraphQLSchema.handleError(e);
-      }
+    params: {
+      description: 'These are the params to be passed to the function.',
+      type: defaultGraphQLTypes.OBJECT,
     },
   };
+  const type = defaultGraphQLTypes.ANY;
+  const resolve = async (_source, args, context) => {
+    try {
+      const { functionName, params } = args;
+      const { config, auth, info } = context;
+
+      return (await FunctionsRouter.handleCloudFunction({
+        params: {
+          functionName,
+        },
+        config,
+        auth,
+        info,
+        body: params,
+      })).response.result;
+    } catch (e) {
+      parseGraphQLSchema.handleError(e);
+    }
+  };
+
+  let callField;
+  if (parseGraphQLSchema.graphQLSchemaIsRelayStyle) {
+    callField = mutationWithClientMutationId({
+      name: 'CallFunction',
+      inputFields: args,
+      outputFields: {
+        result: { type },
+      },
+      mutateAndGetPayload: async (object, ctx) => ({
+        fileInfo: resolve(undefined, object, ctx),
+      }),
+    });
+  } else {
+    callField = {
+      description,
+      args,
+      type,
+      resolve,
+    };
+  }
+  fields.call = callField;
 
   const functionsMutation = new GraphQLObjectType({
     name: 'FunctionsMutation',
