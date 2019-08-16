3aa4ffb98a5ad98f72255b461a22294135503dc2
Diamond Lewis <findlewis@gmail.com>
2019-08-15 22:38:18 -0500

    Postgres fix

--- src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
+++ src/Adapters/Storage/Postgres/PostgresStorageAdapter.js
@@ -498,6 +498,16 @@ const buildWhereClause = ({ schema, query, index }): WhereClause => {
       }
       values.push(fieldName, JSON.stringify(fieldValue.$all));
       index += 2;
+    } else if (Array.isArray(fieldValue.$all)) {
+      // Handle Pointer permissions
+      if (fieldValue.$all.length === 1) {
+        patterns.push(`$${index}:name = $${index + 1}`);
+        values.push(fieldName, fieldValue.$all[0].objectId);
+      } else {
+        patterns.push(`array_contains($${index}:name, $${index + 1})`);
+        values.push(fieldName, JSON.stringify(fieldValue.$all));
+      }
+      index += 2;
     }
 
     if (typeof fieldValue.$exists !== 'undefined') {
