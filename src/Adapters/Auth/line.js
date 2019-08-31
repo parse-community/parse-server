ff0a25e48c9e8ffff50f156d37d8aee9166253ff
saimoomsafayet <saimoom.akash@gmail.com>
2019-08-31 23:17:52 +0600

    fixed linting issues

--- src/Adapters/Auth/line.js
+++ src/Adapters/Auth/line.js
@@ -4,38 +4,36 @@ const httpsRequest = require('./httpsRequest');
 
 // Returns a promise that fulfills iff this user id is valid.
 function validateAuthData(authData) {
-    return request('profile', authData.access_token).then(
-        response => {
-            if (response && response.userId && response.userId === authData.id) {
-                return;
-            }
-            throw new Parse.Error(
-                Parse.Error.OBJECT_NOT_FOUND,
-                'Line auth is invalid for this user.'
-            );
-        }
+  return request('profile', authData.access_token).then(response => {
+    if (response && response.userId && response.userId === authData.id) {
+      return;
+    }
+    throw new Parse.Error(
+      Parse.Error.OBJECT_NOT_FOUND,
+      'Line auth is invalid for this user.'
     );
+  });
 }
 
 // Returns a promise that fulfills iff this app id is valid.
 function validateAppId() {
-    return Promise.resolve();
+  return Promise.resolve();
 }
 
 // A promisey wrapper for api requests
 function request(path, access_token) {
-    var options = {
-        host: 'api.line.me',
-        path: '/v2/' + path,
-        method: 'GET',
-        headers: {
-            Authorization: 'Bearer ' + access_token
-        }
-    };
-    return httpsRequest.get(options);
+  var options = {
+    host: 'api.line.me',
+    path: '/v2/' + path,
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + access_token,
+    },
+  };
+  return httpsRequest.get(options);
 }
 
 module.exports = {
-    validateAppId: validateAppId,
-    validateAuthData: validateAuthData,
+  validateAppId: validateAppId,
+  validateAuthData: validateAuthData,
 };
