63cabb8423cca0ba307b03e2002ba82a2c2f1abd
Diamond Lewis <findlewis@gmail.com>
2019-09-11 09:34:39 -0500

    Stream video with GridFSBucketAdapter (implements byte-range requests) (#6028)
    * Stream video with GridFSBucketAdapter (implements byte-range requests)
    
    Closes: https://github.com/parse-community/parse-server/issues/5834
    
    Similar to https://github.com/parse-community/parse-server/pull/2437
    
    I ran into this issue while trying to view a mov file in safari from the dashboard.
    
    * Rename getFileStream to handleFileStream

--- src/Adapters/Files/GridStoreAdapter.js
+++ src/Adapters/Files/GridStoreAdapter.js
@@ -94,13 +94,14 @@ export class GridStoreAdapter extends FilesAdapter {
     );
   }
 
-  getFileStream(filename: string) {
-    return this._connect().then(database => {
+  async handleFileStream(filename: string, req, res, contentType) {
+    const stream = await this._connect().then(database => {
       return GridStore.exist(database, filename).then(() => {
         const gridStore = new GridStore(database, filename, 'r');
         return gridStore.open();
       });
     });
+    handleRangeRequest(stream, req, res, contentType);
   }
 
   handleShutdown() {
@@ -109,6 +110,75 @@ export class GridStoreAdapter extends FilesAdapter {
     }
     return this._client.close(false);
   }
+}
+
+// handleRangeRequest is licensed under Creative Commons Attribution 4.0 International License (https://creativecommons.org/licenses/by/4.0/).
+// Author: LEROIB at weightingformypizza (https://weightingformypizza.wordpress.com/2015/06/24/stream-html5-media-content-like-video-audio-from-mongodb-using-express-and-gridstore/).
+function handleRangeRequest(stream, req, res, contentType) {
+  const buffer_size = 1024 * 1024; //1024Kb
+  // Range request, partial stream the file
+  const parts = req
+    .get('Range')
+    .replace(/bytes=/, '')
+    .split('-');
+  let [start, end] = parts;
+  const notEnded = !end && end !== 0;
+  const notStarted = !start && start !== 0;
+  // No end provided, we want all bytes
+  if (notEnded) {
+    end = stream.length - 1;
+  }
+  // No start provided, we're reading backwards
+  if (notStarted) {
+    start = stream.length - end;
+    end = start + end - 1;
+  }
+
+  // Data exceeds the buffer_size, cap
+  if (end - start >= buffer_size) {
+    end = start + buffer_size - 1;
+  }
+
+  const contentLength = end - start + 1;
+
+  res.writeHead(206, {
+    'Content-Range': 'bytes ' + start + '-' + end + '/' + stream.length,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType,
+  });
+
+  stream.seek(start, function() {
+    // Get gridFile stream
+    const gridFileStream = stream.stream(true);
+    let bufferAvail = 0;
+    let remainingBytesToWrite = contentLength;
+    let totalBytesWritten = 0;
+    // Write to response
+    gridFileStream.on('data', function(data) {
+      bufferAvail += data.length;
+      if (bufferAvail > 0) {
+        // slice returns the same buffer if overflowing
+        // safe to call in any case
+        const buffer = data.slice(0, remainingBytesToWrite);
+        // Write the buffer
+        res.write(buffer);
+        // Increment total
+        totalBytesWritten += buffer.length;
+        // Decrement remaining
+        remainingBytesToWrite -= data.length;
+        // Decrement the available buffer
+        bufferAvail -= buffer.length;
+      }
+      // In case of small slices, all values will be good at that point
+      // we've written enough, end...
+      if (totalBytesWritten >= contentLength) {
+        stream.close();
+        res.end();
+        this.destroy();
+      }
+    });
+  });
 }
 
 export default GridStoreAdapter;
