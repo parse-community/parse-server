5212ebb8297a61a896a78e8ce7d6dce48f861b73
Alann Maulana <alann_it06@yahoo.co.id>
2019-09-18 21:32:00 +0700

    fix indentation

--- spec/MicrosoftAuth.spec.js
+++ spec/MicrosoftAuth.spec.js
@@ -1,20 +1,20 @@
 const microsoft = require('../lib/Adapters/Auth/microsoft');
 
 describe('Microsoft Auth', () => {
-    it('should fail to validate Microsoft Graph auth with bad token', done => {
-        const authData = {
-            id: 'fake-id',
-            mail: 'fake@mail.com',
-            access_token: 'very.long.bad.token',
-        };
-        try {
-            microsoft.validateAuthData(
-                authData
-            );
-        } catch (error) {
-            jequal(error.code, 101);
-            jequal(error.message, 'Microsoft Graph auth is invalid for this user.');
-            done();
-        }
-    });
+  it('should fail to validate Microsoft Graph auth with bad token', done => {
+    const authData = {
+      id: 'fake-id',
+      mail: 'fake@mail.com',
+      access_token: 'very.long.bad.token',
+    };
+    try {
+      microsoft.validateAuthData(
+        authData
+      );
+    } catch (error) {
+      jequal(error.code, 101);
+      jequal(error.message, 'Microsoft Graph auth is invalid for this user.');
+      done();
+    }
+  });
 });
