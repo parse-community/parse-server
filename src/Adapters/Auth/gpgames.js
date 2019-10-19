10d59f8fb669723a6d25456110a2128d853f8b11
Diamond Lewis <findlewis@gmail.com>
2019-10-19 11:33:59 -0500

    clean up

--- src/Adapters/Auth/gpgames.js
+++ src/Adapters/Auth/gpgames.js
@@ -10,21 +10,28 @@ const { Parse } = require('parse/node');
 const httpsRequest = require('./httpsRequest');
 
 // Returns a promise that fulfills if this user id is valid.
-async function validateAuthData(authData) {
-  const response = await httpsRequest.get(
-    `https://www.googleapis.com/games/v1/players/${authData.id}?access_token=${authData.access_token}`
+function validateAuthData(authData) {
+  return request(`${authData.id}?access_token=${authData.access_token}`).then(
+    response => {
+      if (!(response && response.playerId === authData.id)) {
+        throw new Parse.Error(
+          Parse.Error.OBJECT_NOT_FOUND,
+          'Google Play Games Services - authData is invalid for this user.'
+        );
+      }
+    }
   );
-  if (!(response && response.playerId === authData.id)) {
-    throw new Parse.Error(
-      Parse.Error.OBJECT_NOT_FOUND,
-      'Google Play Games Services - authData is invalid for this user.'
-    );
-  }
 }
 
 // Returns a promise that fulfills if this app id is valid.
 function validateAppId() {
   return Promise.resolve();
+}
+
+function request(path) {
+  return httpsRequest.get(
+    `https://www.googleapis.com/games/v1/players/${path}`
+  );
 }
 
 module.exports = {
