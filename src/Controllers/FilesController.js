c9f5aefb05d39b1b3cd1450327fd666afa801f5f
Mike Patnode <mpatnode@popsugar.com>
2019-10-26 19:15:21 -0700

    Move filename validation out of the Router and into the FilesAdaptor (#6157)
    * Move filename validation out of the Router and into the FilesAdaptor
    
    * Address PR comments
    
    * Update unittests to handle FilesAdapter interface change
    
    * Make validateFilename optional

--- src/Controllers/FilesController.js
+++ src/Controllers/FilesController.js
@@ -1,7 +1,7 @@
 // FilesController.js
 import { randomHexString } from '../cryptoUtils';
 import AdaptableController from './AdaptableController';
-import { FilesAdapter } from '../Adapters/Files/FilesAdapter';
+import { validateFilename, FilesAdapter } from '../Adapters/Files/FilesAdapter';
 import path from 'path';
 import mime from 'mime';
 
@@ -94,6 +94,13 @@ export class FilesController extends AdaptableController {
 
   handleFileStream(config, filename, req, res, contentType) {
     return this.adapter.handleFileStream(filename, req, res, contentType);
+  }
+
+  validateFilename(filename) {
+    if (typeof this.adapter.validateFilename === 'function') {
+      return this.adapter.validateFilename(filename);
+    }
+    return validateFilename(filename);
   }
 }
 
