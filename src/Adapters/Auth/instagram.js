1441216dc6633025a7a0e5d7e6e915d6f7a25ddc
Maravilho Singa <maravilhosinga@hotmail.com>
2020-02-14 02:23:49 +0100

    Update instagram.js
    Instagram API was updated and is not allowing anymore to setup new projects to use the old style but it is still working for the ones that have it already setup.
    
    New docs are listed here: https://developers.facebook.com/docs/instagram-basic-display-api/
    
    I've added support for both old and new API
    To use new API just add new field "api_type" = "new_api" in client side.
    
    For old API just no changes needed.

--- src/Adapters/Auth/instagram.js
+++ src/Adapters/Auth/instagram.js
@@ -4,30 +4,56 @@ const httpsRequest = require('./httpsRequest');
 
 // Returns a promise that fulfills iff this user id is valid.
 function validateAuthData(authData) {
-  return request('me?fields=&access_token=' + authData.access_token).then(
-    response => {
-      if (response && response.data && response.data.id == authData.id) {
-        return;
-      }
-      throw new Parse.Error(
-        Parse.Error.OBJECT_NOT_FOUND,
-        'Instagram auth is invalid for this user.'
-      );
+
+    if (authData.api_type != null && authData.api_type == "new_api") {
+
+        return requestNew('me?fields=&access_token=' + authData.access_token).then(
+            response => {
+                if (response && response.data && response.data.id == authData.id) {
+                    return;
+                }
+                throw new Parse.Error(
+                    Parse.Error.OBJECT_NOT_FOUND,
+                    'Instagram auth is invalid for this user.'
+                );
+            }
+        );
+
+    } else {
+
+        return requestOld('users/self/?access_token=' + authData.access_token).then(
+            response => {
+                if (response && response.data && response.data.id == authData.id) {
+                    return;
+                }
+                throw new Parse.Error(
+                    Parse.Error.OBJECT_NOT_FOUND,
+                    'Instagram auth is invalid for this user.'
+                );
+            }
+        );
+
     }
-  );
+
 }
 
 // Returns a promise that fulfills iff this app id is valid.
 function validateAppId() {
-  return Promise.resolve();
+    return Promise.resolve();
 }
 
-// A promisey wrapper for api requests
-function request(path) {
-  return httpsRequest.get('https://graph.instagram.com/' + path);
+// A promisey wrapper for old api requests
+function requestOld(path) {
+    return httpsRequest.get('https://api.instagram.com/v1/' + path);
 }
+
+// A promisey wrapper for new api requests
+function requestNew(path) {
+    return httpsRequest.get('https://graph.instagram.com/' + path);
+}
+
 
 module.exports = {
-  validateAppId: validateAppId,
-  validateAuthData: validateAuthData,
+    validateAppId: validateAppId,
+    validateAuthData: validateAuthData,
 };
