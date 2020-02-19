0e750c4a1ee03f0b0d90add3ef17934699d8a9b9
n.andryukhin <n.andryukhin@mediasoft.team>
2020-02-19 18:25:24 +0400

    fixing enableExpressErrorHandler logic

--- src/middlewares.js
+++ src/middlewares.js
@@ -321,6 +321,11 @@ export function allowMethodOverride(req, res, next) {
 export function handleParseErrors(err, req, res, next) {
   const log = (req.config && req.config.loggerController) || defaultLogger;
   if (err instanceof Parse.Error) {
+
+    if (req.config && req.config.enableExpressErrorHandler) {
+      return next(err);
+    }
+
     let httpStatus;
     // TODO: fill out this mapping
     switch (err.code) {
@@ -333,13 +338,10 @@ export function handleParseErrors(err, req, res, next) {
       default:
         httpStatus = 400;
     }
-
     res.status(httpStatus);
     res.json({ code: err.code, error: err.message });
     log.error('Parse error: ', err);
-    if (req.config && req.config.enableExpressErrorHandler) {
-      next(err);
-    }
+
   } else if (err.status && err.message) {
     res.status(err.status);
     res.json({ error: err.message });
