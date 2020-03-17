2ceff98cd4e5b14c5eb8353a341078b34489fbb6
Corey MacBook Pro <coreyearleon@icloud.com>
2020-03-17 16:55:13 -0400

    I think using travis default postgres port of 5433 will allow us to not have to remove anything from the image

--- scripts/before_install_postgres.sh
+++ scripts/before_install_postgres.sh
@@ -10,28 +10,28 @@ nvm install $NODE_VERSION
 nvm use $NODE_VERSION
 npm install -g greenkeeper-lockfile@1
 
-sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
+#sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
 
-if [[ $POSTGRES_MAJOR_VERSION -lt 11 ]]; then
+#if [[ $POSTGRES_MAJOR_VERSION -lt 11 ]]; then
   # Setup postgres 9 or 10
-  sudo service postgresql stop
+  #sudo service postgresql stop
     
   # Remove correct version of postgres
-  if [[ $POSTGRES_MAJOR_VERSION -lt 10 ]]; then
-    sudo apt-get remove -q 'postgresql-10.*'
-  else
-    sudo apt-get remove -q 'postgresql-9.*'
-  fi
+  #if [[ $POSTGRES_MAJOR_VERSION -lt 10 ]]; then
+  #  sudo apt-get remove -q 'postgresql-10.*'
+  #else
+  #  sudo apt-get remove -q 'postgresql-9.*'
+  #fi
 
-  sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
+  #sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
 
 
-else 
-  # Setup postgres 11 or higher
-  sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
-  sudo service postgresql stop
-  # Remove previous versions of postgres
-  sudo apt-get remove -q 'postgresql-9.*' 'postgresql-10.*'
-  sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
-fi
+#else 
+# Setup postgres 11 or higher
+sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
+sudo service postgresql stop
+# Remove previous versions of postgres
+#sudo apt-get remove -q 'postgresql-9.*' 'postgresql-10.*'
+sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
+#fi
 
