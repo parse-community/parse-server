4ffee37d514a3ae24629f78bf3ee6fbb10e5463d
Corey MacBook Pro <coreyearleon@icloud.com>
2020-03-17 17:32:56 -0400

    reverting back to working way with removing postgres from image

--- scripts/before_install_postgres.sh
+++ scripts/before_install_postgres.sh
@@ -10,28 +10,27 @@ nvm install $NODE_VERSION
 nvm use $NODE_VERSION
 npm install -g greenkeeper-lockfile@1
 
-#sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
+sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
 
-#if [[ $POSTGRES_MAJOR_VERSION -lt 11 ]]; then
-#  # Setup postgres 9 or 10
-#  #sudo service postgresql stop
+if [[ $POSTGRES_MAJOR_VERSION -lt 11 ]]; then
+  # Setup postgres 9 or 10
+  sudo service postgresql stop
     
-# Remove correct version of postgres
-#  #if [[ $POSTGRES_MAJOR_VERSION -lt 10 ]]; then
-#  #  sudo apt-get remove -q 'postgresql-10.*'
-#  #else
-#  #  sudo apt-get remove -q 'postgresql-9.*'
-#  #fi
+  # Remove correct version of postgres
+  if [[ $POSTGRES_MAJOR_VERSION -lt 10 ]]; then
+    sudo apt-get remove -q 'postgresql-10.*'
+  else
+    sudo apt-get remove -q 'postgresql-9.*'
+  fi
 
-#  #sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
+  sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
 
+else 
 
-#else 
-# Setup postgres 11 or higher
-sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
-sudo service postgresql stop
-# Remove previous versions of postgres
-#sudo apt-get remove -q 'postgresql-9.*' 'postgresql-10.*'
-sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
-#fi
-
+  # Setup postgres 11 or higher
+  sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
+  sudo service postgresql stop
+  # Remove previous versions of postgres
+  sudo apt-get remove -q 'postgresql-9.*' 'postgresql-10.*'
+  sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
+fi
