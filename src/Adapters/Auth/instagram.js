b65be8a076920ddb37ff7472eb5473c303c4119c
Diamond Lewis <findlewis@gmail.com>
2020-03-21 21:16:31 -0500

    support api url

--- src/Adapters/Auth/instagram.js
+++ src/Adapters/Auth/instagram.js
@@ -1,59 +1,29 @@
 // Helper functions for accessing the instagram API.
 var Parse = require('parse/node').Parse;
 const httpsRequest = require('./httpsRequest');
+const defaultURL = 'https://api.instagram.com/v1/';
 
 // Returns a promise that fulfills iff this user id is valid.
 function validateAuthData(authData) {
-
-    if (authData.api_type != null && authData.api_type == "new_api") {
-
-        return requestNew('me?fields=&access_token=' + authData.access_token).then(
-            response => {
-                if (response && response.data && response.data.id == authData.id) {
-                    return;
-                }
-                throw new Parse.Error(
-                    Parse.Error.OBJECT_NOT_FOUND,
-                    'Instagram auth is invalid for this user.'
-                );
-            }
-        );
-
-    } else {
-
-        return requestOld('users/self/?access_token=' + authData.access_token).then(
-            response => {
-                if (response && response.data && response.data.id == authData.id) {
-                    return;
-                }
-                throw new Parse.Error(
-                    Parse.Error.OBJECT_NOT_FOUND,
-                    'Instagram auth is invalid for this user.'
-                );
-            }
-        );
-
+  const apiURL = authData.apiURL || defaultURL;
+  const path = `${apiURL}users/self/?access_token=${authData.access_token}`;
+  return httpsRequest.get(path).then(response => {
+    if (response && response.data && response.data.id == authData.id) {
+      return;
     }
-
+    throw new Parse.Error(
+      Parse.Error.OBJECT_NOT_FOUND,
+      'Instagram auth is invalid for this user.'
+    );
+  });
 }
 
 // Returns a promise that fulfills iff this app id is valid.
 function validateAppId() {
-    return Promise.resolve();
+  return Promise.resolve();
 }
-
-// A promisey wrapper for old api requests
-function requestOld(path) {
-    return httpsRequest.get('https://api.instagram.com/v1/' + path);
-}
-
-// A promisey wrapper for new api requests
-function requestNew(path) {
-    return httpsRequest.get('https://graph.instagram.com/' + path);
-}
-
 
 module.exports = {
-    validateAppId: validateAppId,
-    validateAuthData: validateAuthData,
+  validateAppId,
+  validateAuthData,
 };
