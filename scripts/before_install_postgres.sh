4cd60dfdda31de13b104d7c0d371329f09aa92e0
Corey MacBook Pro <coreyearleon@icloud.com>
2020-03-25 16:19:10 -0400

    seeing if stopping/starting postgres back-to-back was the issue

--- scripts/before_install_postgres.sh
+++ scripts/before_install_postgres.sh
@@ -6,6 +6,8 @@ source ~/.nvm/nvm.sh
 
 echo "[SCRIPT] Before Install Script :: Setup Postgres ${POSTGRES_MAJOR_VERSION}"
 
+#Stop the default service since changes are needed to the config file
+sudo service postgresql stop
 nvm install $NODE_VERSION
 nvm use $NODE_VERSION
 npm install -g greenkeeper-lockfile@1
@@ -17,9 +19,6 @@ npm install -g greenkeeper-lockfile@1
 if [[ $POSTGRES_MAJOR_VERSION -lt 11 ]]; then
   # Setup postgres 9 or 10
 
-  #Stop the default service since changes are needed to the config file
-  sudo service postgresql stop
-  
   #Switched installed default port to 5433 since URI is is using this port
   sudo sed -i 's/port = 5432/port = 5433/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
 
@@ -29,9 +28,7 @@ else
 
   # Setup postgres 11 or higher
   sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
-  sudo service postgresql stop
   sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
 
 fi
-
 
