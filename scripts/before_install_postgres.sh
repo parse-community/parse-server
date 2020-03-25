160f5a511232142fa82f09c57f7617ab100fe4e0
Corey MacBook Pro <coreyearleon@icloud.com>
2020-03-25 19:28:44 -0400

    switch ports after new configs are made

--- scripts/before_install_postgres.sh
+++ scripts/before_install_postgres.sh
@@ -31,10 +31,9 @@ else
   # Setup postgres 11 or higher
   sudo rm -rf /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main
   sudo -u postgres /usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/initdb -D /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main -E SQL_ASCII --no-locale
-  
   sudo sed -i 's/port = 5432/port = 5433/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
   #sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
-  sudo -u postgres /usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_ctl -D /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main -l /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main/serverlog start
+  sudo -u postgres /usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_ctl -D /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main -l /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main/serverlog -o "-F -p 5433" start
   # Remove previous versions of postgres
   #sudo apt-get remove -q 'postgresql-9.*' 'postgresql-10.*'
   #sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
