2eb79875fc299ada84fa435a9cc5b74e193bb241
Corey MacBook Pro <coreyearleon@icloud.com>
2020-03-25 19:38:14 -0400

    went back to removing to get it to start properly

--- scripts/before_install_postgres.sh
+++ scripts/before_install_postgres.sh
@@ -18,23 +18,19 @@ if [[ $POSTGRES_MAJOR_VERSION -lt 11 ]]; then
   sudo sed -i 's/port = 5432/port = 5433/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
 
   # Remove correct version of postgres
-  #if [[ $POSTGRES_MAJOR_VERSION -lt 10 ]]; then
-  #  sudo apt-get remove -q 'postgresql-10.*'
-  #else
-  #  sudo apt-get remove -q 'postgresql-9.*'
-  #fi
+  if [[ $POSTGRES_MAJOR_VERSION -lt 10 ]]; then
+    sudo apt-get remove -q 'postgresql-10.*'
+  else
+    sudo apt-get remove -q 'postgresql-9.*'
+  fi
 
-  #sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
+  sudo service postgresql start
 
 else 
 
   # Setup postgres 11 or higher
-  sudo rm -rf /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main
-  sudo -u postgres /usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/initdb -D /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main -E SQL_ASCII --no-locale
-  sudo sed -i 's/port = 5432/port = 5433/' /etc/postgresql/${POSTGRES_MAJOR_VERSION}/main/postgresql.conf
-  #sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
-  sudo -u postgres /usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_ctl -D /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main -l /var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main/serverlog -o "-F -p 5433" start
+  sudo cp /etc/postgresql/{10,${POSTGRES_MAJOR_VERSION}}/main/pg_hba.conf
   # Remove previous versions of postgres
-  #sudo apt-get remove -q 'postgresql-9.*' 'postgresql-10.*'
-  #sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
+  sudo apt-get remove -q 'postgresql-9.*' 'postgresql-10.*'
+  sudo service postgresql start ${POSTGRES_MAJOR_VERSION}
 fi
