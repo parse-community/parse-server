3499dd98060282373596e501f44aeefa3e75d968
Manuel Trezza <trezza.m@gmail.com>
2020-03-29 06:39:07 +0200

    removed error output to console
    This reverts commit da81c515cbf8cb6edfd82f09ca3087457ac8c727.

--- src/Adapters/Storage/Mongo/MongoStorageAdapter.js
+++ src/Adapters/Storage/Mongo/MongoStorageAdapter.js
@@ -884,10 +884,7 @@ export class MongoStorageAdapter implements StorageAdapter {
           mongoObjectToParseObject(className, object, schema)
         )
       )
-      .catch((err) => {
-        console.log(err);
-        this.handleError(err);
-      });
+      .catch(err => this.handleError(err));
   }
 
   // This function will recursively traverse the pipeline and convert any Pointer or Date columns.
