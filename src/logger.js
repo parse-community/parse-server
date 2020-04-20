08773b7cca8fd3d73f43a41f7819966b6d148877
andyemre-com <emreagarta@gmail.com>
2020-04-21 01:50:00 +0300

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,14 +1,16 @@
 'use strict';
 import defaults from './defaults';
 import { WinstonLoggerAdapter } from './Adapters/Logger/WinstonLoggerAdapter';
-import { LoggerController }     from './Controllers/LoggerController';
+import { LoggerController } from './Controllers/LoggerController';
 
+// Used for Separate Live Query Server
 function defaultLogger() {
   const options = {
     logsFolder: defaults.logsFolder,
     jsonLogs: defaults.jsonLogs,
     verbose: defaults.verbose,
-    silent: defaults.silent };
+    silent: defaults.silent,
+  };
   const adapter = new WinstonLoggerAdapter(options);
   return new LoggerController(adapter, null, options);
 }
@@ -25,10 +27,10 @@ export function getLogger() {
 
 // for: `import logger from './logger'`
 Object.defineProperty(module.exports, 'default', {
-  get: getLogger
+  get: getLogger,
 });
 
 // for: `import { logger } from './logger'`
 Object.defineProperty(module.exports, 'logger', {
-  get: getLogger
+  get: getLogger,
 });
