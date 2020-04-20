96a6f70c4e4b27c1e8a8dbd0b320c8a3f71e3c95
andyemre-com <emreagarta@gmail.com>
2020-04-21 01:49:15 +0300

    Update PublicAPIRouter.js

--- src/Routers/PublicAPIRouter.js
+++ src/Routers/PublicAPIRouter.js
@@ -4,18 +4,21 @@ import express from 'express';
 import path from 'path';
 import fs from 'fs';
 import qs from 'querystring';
+import { Parse } from 'parse/node';
 
-const public_html = path.resolve(__dirname, "../../public_html");
+const public_html = path.resolve(__dirname, '../../public_html');
 const views = path.resolve(__dirname, '../../views');
 
 export class PublicAPIRouter extends PromiseRouter {
-
   verifyEmail(req) {
-    const { token, username } = req.query;
+    const { username, token: rawToken } = req.query;
+    const token =
+      rawToken && typeof rawToken !== 'string' ? rawToken.toString() : rawToken;
+
     const appId = req.params.appId;
     const config = Config.get(appId);
 
-    if(!config){
+    if (!config) {
       this.invalidRequest();
     }
 
@@ -28,15 +31,18 @@ export class PublicAPIRouter extends PromiseRouter {
     }
 
     const userController = config.userController;
-    return userController.verifyEmail(username, token).then(() => {
-      const params = qs.stringify({username});
-      return Promise.resolve({
-        status: 302,
-        location: `${config.verifyEmailSuccessURL}?${params}`
-      });
-    }, ()=> {
-      return this.invalidVerificationLink(req);
-    })
+    return userController.verifyEmail(username, token).then(
+      () => {
+        const params = qs.stringify({ username });
+        return Promise.resolve({
+          status: 302,
+          location: `${config.verifyEmailSuccessURL}?${params}`,
+        });
+      },
+      () => {
+        return this.invalidVerificationLink(req);
+      }
+    );
   }
 
   resendVerificationEmail(req) {
@@ -44,7 +50,7 @@ export class PublicAPIRouter extends PromiseRouter {
     const appId = req.params.appId;
     const config = Config.get(appId);
 
-    if(!config){
+    if (!config) {
       this.invalidRequest();
     }
 
@@ -58,51 +64,60 @@ export class PublicAPIRouter extends PromiseRouter {
 
     const userController = config.userController;
 
-    return userController.resendVerificationEmail(username).then(() => {
-      return Promise.resolve({
-        status: 302,
-        location: `${config.linkSendSuccessURL}`
-      });
-    }, ()=> {
-      return Promise.resolve({
-        status: 302,
-        location: `${config.linkSendFailURL}`
-      });
-    })
+    return userController.resendVerificationEmail(username).then(
+      () => {
+        return Promise.resolve({
+          status: 302,
+          location: `${config.linkSendSuccessURL}`,
+        });
+      },
+      () => {
+        return Promise.resolve({
+          status: 302,
+          location: `${config.linkSendFailURL}`,
+        });
+      }
+    );
   }
 
   changePassword(req) {
     return new Promise((resolve, reject) => {
       const config = Config.get(req.query.id);
 
-      if(!config){
+      if (!config) {
         this.invalidRequest();
       }
 
       if (!config.publicServerURL) {
         return resolve({
           status: 404,
-          text: 'Not found.'
+          text: 'Not found.',
         });
       }
       // Should we keep the file in memory or leave like that?
-      fs.readFile(path.resolve(views, "choose_password"), 'utf-8', (err, data) => {
-        if (err) {
-          return reject(err);
+      fs.readFile(
+        path.resolve(views, 'choose_password'),
+        'utf-8',
+        (err, data) => {
+          if (err) {
+            return reject(err);
+          }
+          data = data.replace(
+            'PARSE_SERVER_URL',
+            `'${config.publicServerURL}'`
+          );
+          resolve({
+            text: data,
+          });
         }
-        data = data.replace("PARSE_SERVER_URL", `'${config.publicServerURL}'`);
-        resolve({
-          text: data
-        })
-      });
+      );
     });
   }
 
   requestResetPassword(req) {
-
     const config = req.config;
 
-    if(!config){
+    if (!config) {
       this.invalidRequest();
     }
 
@@ -110,28 +125,37 @@ export class PublicAPIRouter extends PromiseRouter {
       return this.missingPublicServerURL();
     }
 
-    const { username, token } = req.query;
+    const { username, token: rawToken } = req.query;
+    const token =
+      rawToken && typeof rawToken !== 'string' ? rawToken.toString() : rawToken;
 
     if (!username || !token) {
       return this.invalidLink(req);
     }
 
-    return config.userController.checkResetTokenValidity(username, token).then(() => {
-      const params = qs.stringify({token, id: config.applicationId, username, app: config.appName, });
-      return Promise.resolve({
-        status: 302,
-        location: `${config.choosePasswordURL}?${params}`
-      })
-    }, () => {
-      return this.invalidLink(req);
-    })
+    return config.userController.checkResetTokenValidity(username, token).then(
+      () => {
+        const params = qs.stringify({
+          token,
+          id: config.applicationId,
+          username,
+          app: config.appName,
+        });
+        return Promise.resolve({
+          status: 302,
+          location: `${config.choosePasswordURL}?${params}`,
+        });
+      },
+      () => {
+        return this.invalidLink(req);
+      }
+    );
   }
 
   resetPassword(req) {
-
     const config = req.config;
 
-    if(!config){
+    if (!config) {
       this.invalidRequest();
     }
 
@@ -139,46 +163,91 @@ export class PublicAPIRouter extends PromiseRouter {
       return this.missingPublicServerURL();
     }
 
-    const {
-      username,
-      token,
-      new_password
-    } = req.body;
+    const { username, new_password, token: rawToken } = req.body;
+    const token =
+      rawToken && typeof rawToken !== 'string' ? rawToken.toString() : rawToken;
 
-    if (!username || !token || !new_password) {
+    if ((!username || !token || !new_password) && req.xhr === false) {
       return this.invalidLink(req);
     }
 
-    return config.userController.updatePassword(username, token, new_password).then(() => {
-      const params = qs.stringify({username: username});
-      return Promise.resolve({
-        status: 302,
-        location: `${config.passwordResetSuccessURL}?${params}`
-      });
-    }, (err) => {
-      const params = qs.stringify({username: username, token: token, id: config.applicationId, error:err, app:config.appName});
-      return Promise.resolve({
-        status: 302,
-        location: `${config.choosePasswordURL}?${params}`
-      });
-    });
+    if (!username) {
+      throw new Parse.Error(Parse.Error.USERNAME_MISSING, 'Missing username');
+    }
 
+    if (!token) {
+      throw new Parse.Error(Parse.Error.OTHER_CAUSE, 'Missing token');
+    }
+
+    if (!new_password) {
+      throw new Parse.Error(Parse.Error.PASSWORD_MISSING, 'Missing password');
+    }
+
+    return config.userController
+      .updatePassword(username, token, new_password)
+      .then(
+        () => {
+          return Promise.resolve({
+            success: true,
+          });
+        },
+        err => {
+          return Promise.resolve({
+            success: false,
+            err,
+          });
+        }
+      )
+      .then(result => {
+        const params = qs.stringify({
+          username: username,
+          token: token,
+          id: config.applicationId,
+          error: result.err,
+          app: config.appName,
+        });
+
+        if (req.xhr) {
+          if (result.success) {
+            return Promise.resolve({
+              status: 200,
+              response: 'Password successfully reset',
+            });
+          }
+          if (result.err) {
+            throw new Parse.Error(Parse.Error.OTHER_CAUSE, `${result.err}`);
+          }
+        }
+
+        const encodedUsername = encodeURIComponent(username);
+        const location = result.success
+          ? `${config.passwordResetSuccessURL}?username=${encodedUsername}`
+          : `${config.choosePasswordURL}?${params}`;
+
+        return Promise.resolve({
+          status: 302,
+          location,
+        });
+      });
   }
 
   invalidLink(req) {
     return Promise.resolve({
       status: 302,
-      location: req.config.invalidLinkURL
+      location: req.config.invalidLinkURL,
     });
   }
 
   invalidVerificationLink(req) {
     const config = req.config;
     if (req.query.username && req.params.appId) {
-      const params = qs.stringify({username: req.query.username, appId: req.params.appId});
+      const params = qs.stringify({
+        username: req.query.username,
+        appId: req.params.appId,
+      });
       return Promise.resolve({
         status: 302,
-        location: `${config.invalidVerificationLinkURL}?${params}`
+        location: `${config.invalidVerificationLinkURL}?${params}`,
       });
     } else {
       return this.invalidLink(req);
@@ -187,15 +256,15 @@ export class PublicAPIRouter extends PromiseRouter {
 
   missingPublicServerURL() {
     return Promise.resolve({
-      text:  'Not found.',
-      status: 404
+      text: 'Not found.',
+      status: 404,
     });
   }
 
   invalidRequest() {
     const error = new Error();
     error.status = 403;
-    error.message = "unauthorized";
+    error.message = 'unauthorized';
     throw error;
   }
 
@@ -205,30 +274,59 @@ export class PublicAPIRouter extends PromiseRouter {
   }
 
   mountRoutes() {
-    this.route('GET','/apps/:appId/verify_email',
-      req => { this.setConfig(req) },
-      req => { return this.verifyEmail(req); });
+    this.route(
+      'GET',
+      '/apps/:appId/verify_email',
+      req => {
+        this.setConfig(req);
+      },
+      req => {
+        return this.verifyEmail(req);
+      }
+    );
 
-    this.route('POST', '/apps/:appId/resend_verification_email',
-      req => { this.setConfig(req); },
-      req => { return this.resendVerificationEmail(req); });
+    this.route(
+      'POST',
+      '/apps/:appId/resend_verification_email',
+      req => {
+        this.setConfig(req);
+      },
+      req => {
+        return this.resendVerificationEmail(req);
+      }
+    );
 
-    this.route('GET','/apps/choose_password',
-      req => { return this.changePassword(req); });
+    this.route('GET', '/apps/choose_password', req => {
+      return this.changePassword(req);
+    });
 
-    this.route('POST','/apps/:appId/request_password_reset',
-      req => { this.setConfig(req) },
-      req => { return this.resetPassword(req); });
+    this.route(
+      'POST',
+      '/apps/:appId/request_password_reset',
+      req => {
+        this.setConfig(req);
+      },
+      req => {
+        return this.resetPassword(req);
+      }
+    );
 
-    this.route('GET','/apps/:appId/request_password_reset',
-      req => { this.setConfig(req) },
-      req => { return this.requestResetPassword(req); });
+    this.route(
+      'GET',
+      '/apps/:appId/request_password_reset',
+      req => {
+        this.setConfig(req);
+      },
+      req => {
+        return this.requestResetPassword(req);
+      }
+    );
   }
 
   expressRouter() {
     const router = express.Router();
-    router.use("/apps", express.static(public_html));
-    router.use("/", super.expressRouter());
+    router.use('/apps', express.static(public_html));
+    router.use('/', super.expressRouter());
     return router;
   }
 }
