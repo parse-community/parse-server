ec487631d7c535a2566cc933b2d5b3afa2174dfa
andyemre-com <emreagarta@gmail.com>
2020-04-21 01:49:46 +0300

    Update ParseMessageQueue.js

--- src/ParseMessageQueue.js
+++ src/ParseMessageQueue.js
@@ -1,26 +1,30 @@
 import { loadAdapter } from './Adapters/AdapterLoader';
-import {
-  EventEmitterMQ
-} from './Adapters/MessageQueue/EventEmitterMQ';
+import { EventEmitterMQ } from './Adapters/MessageQueue/EventEmitterMQ';
 
 const ParseMessageQueue = {};
 
 ParseMessageQueue.createPublisher = function(config: any): any {
-  const adapter = loadAdapter(config.messageQueueAdapter, EventEmitterMQ, config);
+  const adapter = loadAdapter(
+    config.messageQueueAdapter,
+    EventEmitterMQ,
+    config
+  );
   if (typeof adapter.createPublisher !== 'function') {
     throw 'pubSubAdapter should have createPublisher()';
   }
   return adapter.createPublisher(config);
-}
+};
 
 ParseMessageQueue.createSubscriber = function(config: any): void {
-  const adapter = loadAdapter(config.messageQueueAdapter, EventEmitterMQ, config)
+  const adapter = loadAdapter(
+    config.messageQueueAdapter,
+    EventEmitterMQ,
+    config
+  );
   if (typeof adapter.createSubscriber !== 'function') {
     throw 'messageQueueAdapter should have createSubscriber()';
   }
   return adapter.createSubscriber(config);
-}
+};
 
-export {
-  ParseMessageQueue
-}
+export { ParseMessageQueue };
