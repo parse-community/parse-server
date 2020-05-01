66a702ead380bce8a3ddb14f7c35599ddd1a6a9b
dblythy <daniel-blyth@live.com.au>
2020-05-01 15:33:25 +1000

    Update triggers.js

--- src/triggers.js
+++ src/triggers.js
@@ -754,6 +754,7 @@ export async function maybeRunConnectTrigger(triggerType, request) {
   if (!trigger) {
     return;
   }
+  console.log(trigger);
   if (request.sessionToken) {
     try {
      const user = await userForSessionToken(request.sessionToken);
