46f19b07954e4a6c0d188f497f983edaf1ea7621
Manuel Trezza <trezza.m@gmail.com>
2020-06-25 02:15:18 +0200

    temporarily disabling index creation to fix postgres issue

--- src/Controllers/DatabaseController.js
+++ src/Controllers/DatabaseController.js
@@ -1811,40 +1811,40 @@ class DatabaseController {
         throw error;
       });
 
-    const idempotencyRequestIdUniqueness = idempotencyClassPromise
-      .then(() => {
-        return this.adapter.ensureUniqueness(
-          '_Idempotency',
-          requiredIdempotencyFields,
-          ['reqId']
-        );
-      })
-      .catch((error) => {
-        logger.warn(
-          'Unable to ensure uniqueness for idempotency request ID: ',
-          error
-        );
-        throw error;
-      });
-
-    // const idempotencyExpireIndex = idempotencyClassPromise
+    // const idempotencyRequestIdUniqueness = idempotencyClassPromise
     //   .then(() =>
-    //     this.adapter.ensureIndex(
+    //     this.adapter.ensureUniqueness(
     //       '_Idempotency',
     //       requiredIdempotencyFields,
-    //       ['expire'],
-    //       'ttl',
-    //       false,
-    //       { ttl: 0 },
+    //       ['reqId']
     //     )
     //   )
     //   .catch((error) => {
     //     logger.warn(
-    //       'Unable to create TTL index for idempotency expire date: ',
+    //       'Unable to ensure uniqueness for idempotency request ID: ',
     //       error
     //     );
     //     throw error;
     //   });
+
+    const idempotencyExpireIndex = idempotencyClassPromise
+      .then(() =>
+        this.adapter.ensureIndex(
+          '_Idempotency',
+          requiredIdempotencyFields,
+          ['expire'],
+          'ttl',
+          false,
+          { ttl: 0 },
+        )
+      )
+      .catch((error) => {
+        logger.warn(
+          'Unable to create TTL index for idempotency expire date: ',
+          error
+        );
+        throw error;
+      });
 
     const roleUniqueness = roleClassPromise
       .then(() =>
@@ -1867,8 +1867,8 @@ class DatabaseController {
       emailUniqueness,
       emailCaseInsensitiveIndex,
       roleUniqueness,
-      idempotencyRequestIdUniqueness,
-      // idempotencyExpireIndex,
+      // idempotencyRequestIdUniqueness,
+      idempotencyExpireIndex,
       adapterInit,
       indexPromise,
     ]);
