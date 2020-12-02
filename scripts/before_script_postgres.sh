884fdfdc3a4b3ace65c7acefa2cd320c4e0e6523
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2020-12-01 20:42:33 -0800

    Still trying to fix postgres connection

--- scripts/before_script_postgres.sh
+++ scripts/before_script_postgres.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "[SCRIPT] Before Script :: Setup Parse DB for Postgres ${POSTGRES_MAJOR_VERSION}"
 
-psql -v ON_ERROR_STOP=1 -h postgres -W postgres <<-EOSQL
+psql -v ON_ERROR_STOP=1 -h localhost -W postgres <<-EOSQL
     CREATE DATABASE parse_server_postgres_adapter_test_database;
     \c parse_server_postgres_adapter_test_database;
     CREATE EXTENSION postgis;
