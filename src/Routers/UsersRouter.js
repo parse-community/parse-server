476774101b547b09d0786998b8f4e85f70fb34b0
Jason Posthuma <jason.posthuma@bruce.work>
2021-02-10 15:28:08 +0100

    Added support for `req.userFromJwt` in login sequence

--- src/Routers/UsersRouter.js
+++ src/Routers/UsersRouter.js
@@ -139,6 +139,32 @@ export class UsersRouter extends ClassesRouter {
     });
   }
 
+  /**
+   * Validates JWT bearer token and looks up user `req.userFromJwt`. CRITICALLY IMPORTANT that the JWT has already been validated by this point (eg: express middleware, AWS API Gateway Authorizer)
+   * @param {Object} req The request
+   * @returns {Object} User object
+   */
+  _authenticateUserFromRequestWithJwt(req) {
+    return new Promise((resolve, reject) => {
+      if (!req.userFromJwt) {
+        throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid credentials.');
+      }
+
+      const query = { objectId: req.userFromJwt.objectId };
+      return req.config.database
+        .find('_User', query)
+        .then(results => {
+          if (!results.length)
+            throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid credentials.');
+          const user = results[0];
+          resolve(user);
+        })
+        .catch(error => {
+          return reject(error);
+        });
+    });
+  }
+
   handleMe(req) {
     if (!req.info || !req.info.sessionToken) {
       throw new Parse.Error(Parse.Error.INVALID_SESSION_TOKEN, 'Invalid session token');
@@ -171,10 +197,17 @@ export class UsersRouter extends ClassesRouter {
   }
 
   async handleLogIn(req) {
-    const user = await this._authenticateUserFromRequest(req);
+    let userFromJwt;
+    if (req.userFromJwt) {
+      // Could be just used `req.userFromJwt`, but the forced lookup
+      // Ensures the user hasn't been deleted since the the JWT was granted
+      userFromJwt = await this._authenticateUserFromRequestWithJwt(req);
+    }
 
-    // handle password expiry policy
-    if (req.config.passwordPolicy && req.config.passwordPolicy.maxPasswordAge) {
+    const user = userFromJwt || (await this._authenticateUserFromRequest(req));
+
+    // handle password expiry policy - ignore if user is managed in SSO (provided by JWT)
+    if (!userFromJwt && req.config.passwordPolicy && req.config.passwordPolicy.maxPasswordAge) {
       let changedAt = user._password_changed_at;
 
       if (!changedAt) {
