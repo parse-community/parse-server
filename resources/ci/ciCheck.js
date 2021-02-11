7e0c2265c83b80aa4ca673fa8b16907f1bfbdc4d
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2021-02-11 05:04:02 +0100

    added nodejs version check

---
+++ resources/ci/ciCheck.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const CiVersionCheck = require('./CiVersionCheck');
+const mongoVersionList = require('mongodb-version-list');
+const allNodeVersions = require('all-node-versions');
+
+async function check() {
+  // Run checks
+  await checkMongoDbVersions();
+  await checkNodeVersions();
+}
+
+/**
+ * Check the MongoDB versions used in test environments.
+ */
+async function checkMongoDbVersions() {
+
+  const releasedVersions = await new Promise((resolve, reject) => {
+    mongoVersionList(function(error, versions) {
+      if (error) {
+        reject(error);
+      }
+      resolve(versions);
+    });
+  });
+
+  await new CiVersionCheck({
+    packageName: 'MongoDB',
+    packageSupportUrl: 'https://www.mongodb.com/support-policy',
+    yamlFilePath: './.github/workflows/ci.yml',
+    ciEnvironmentsKeyPath: 'jobs.check-mongo.strategy.matrix.include',
+    ciVersionKey: 'MONGODB_VERSION',
+    releasedVersions,
+    latestComponent: CiVersionCheck.versionComponents.path,
+    ignoreReleasedVersions: [
+      '~4.7.0', // This is a development release according to MongoDB support
+      '<3.6.0' // These versions have reached their MongoDB end-of-life support date
+    ],
+  }).check();
+}
+
+/**
+ * Check the Nodejs versions used in test environments.
+ */
+async function checkNodeVersions() {
+
+  const allVersions = await allNodeVersions();
+  const releasedVersions = allVersions.versions;
+
+  await new CiVersionCheck({
+    packageName: 'Nodejs',
+    packageSupportUrl: 'https://github.com/nodejs/node/blob/master/CHANGELOG.md',
+    yamlFilePath: './.github/workflows/ci.yml',
+    ciEnvironmentsKeyPath: 'jobs.check-mongo.strategy.matrix.include',
+    ciVersionKey: 'NODE_VERSION',
+    releasedVersions,
+    latestComponent: CiVersionCheck.versionComponents.minor,
+    ignoreReleasedVersions: [
+      '<10.0.0', // These versions have reached their end-of-life support date
+      '>=11.0.0 <12.0.0', // These versions have reached their end-of-life support date
+      '>=13.0.0 <14.0.0', // These versions have reached their end-of-life support date
+      '>=15.0.0 <16.0.0', // This version is not in long-term-support yet
+    ],
+  }).check();
+}
+
+check();
