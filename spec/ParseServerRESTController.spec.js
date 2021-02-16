bbf77696fbee36fbeff55185f44dd150352ba530
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2021-02-15 21:03:19 -0800

    Fix the other transactions related tests that became flaky because now Parse Server tries to submit the transaction multilpe times in the case of TransientError

--- spec/ParseServerRESTController.spec.js
+++ spec/ParseServerRESTController.spec.js
@@ -219,10 +219,13 @@ describe('ParseServerRESTController', () => {
               expect(response[1].success.createdAt).toBeDefined();
               const query = new Parse.Query('MyObject');
               return query.find().then(results => {
-                expect(databaseAdapter.createObject.calls.count()).toBe(2);
-                expect(databaseAdapter.createObject.calls.argsFor(0)[3]).toBe(
-                  databaseAdapter.createObject.calls.argsFor(1)[3]
-                );
+                expect(databaseAdapter.createObject.calls.count() % 2).toBe(0);
+                expect(databaseAdapter.createObject.calls.count() > 0).toEqual(true);
+                for (let i = 0; i + 1 < databaseAdapter.createObject.calls.length; i = i + 2) {
+                  expect(databaseAdapter.createObject.calls.argsFor(i)[3]).toBe(
+                    databaseAdapter.createObject.calls.argsFor(i + 1)[3]
+                  );
+                }
                 expect(results.map(result => result.get('key')).sort()).toEqual([
                   'value1',
                   'value2',
@@ -347,7 +350,7 @@ describe('ParseServerRESTController', () => {
           });
       });
 
-      it('should generate separate session for each call', async () => {
+      fit('should generate separate session for each call', async () => {
         const myObject = new Parse.Object('MyObject'); // This is important because transaction only works on pre-existing collections
         await myObject.save();
         await myObject.destroy();
@@ -514,18 +517,18 @@ describe('ParseServerRESTController', () => {
         const results3 = await query3.find();
         expect(results3.map(result => result.get('key')).sort()).toEqual(['value1', 'value2']);
 
-        expect(databaseAdapter.createObject.calls.count()).toBe(13);
+        expect(databaseAdapter.createObject.calls.count() >= 13).toEqual(true);
         let transactionalSession;
         let transactionalSession2;
         let myObjectDBCalls = 0;
         let myObject2DBCalls = 0;
         let myObject3DBCalls = 0;
-        for (let i = 0; i < 13; i++) {
+        for (let i = 0; i < databaseAdapter.createObject.calls.count(); i++) {
           const args = databaseAdapter.createObject.calls.argsFor(i);
           switch (args[0]) {
             case 'MyObject':
               myObjectDBCalls++;
-              if (!transactionalSession) {
+              if (!transactionalSession || (myObjectDBCalls - 1) % 2 === 0) {
                 transactionalSession = args[3];
               } else {
                 expect(transactionalSession).toBe(args[3]);
@@ -536,7 +539,7 @@ describe('ParseServerRESTController', () => {
               break;
             case 'MyObject2':
               myObject2DBCalls++;
-              if (!transactionalSession2) {
+              if (!transactionalSession2 || (myObject2DBCalls - 1) % 9 === 0) {
                 transactionalSession2 = args[3];
               } else {
                 expect(transactionalSession2).toBe(args[3]);
@@ -551,9 +554,12 @@ describe('ParseServerRESTController', () => {
               break;
           }
         }
-        expect(myObjectDBCalls).toEqual(2);
-        expect(myObject2DBCalls).toEqual(9);
-        expect(myObject3DBCalls).toEqual(2);
+        expect(myObjectDBCalls % 2).toEqual(0);
+        expect(myObjectDBCalls > 0).toEqual(true);
+        expect(myObject2DBCalls % 9).toEqual(0);
+        expect(myObject2DBCalls > 0).toEqual(true);
+        expect(myObject3DBCalls % 2).toEqual(0);
+        expect(myObject3DBCalls > 0).toEqual(true);
       });
     });
   }
