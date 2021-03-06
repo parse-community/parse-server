c91142d249ad7a7735446f1df5259c1616da6447
Corey's iMac <coreyearleon@icloud.com>
2021-03-06 17:32:14 -0500

    update keys

--- src/Routers/ClassesRouter.js
+++ src/Routers/ClassesRouter.js
@@ -57,7 +57,7 @@ export class ClassesRouter extends PromiseRouter {
       }
     }
 
-    if (body.keys || body.keys === '') {
+    if (body.keys != null) {
       options.keys = String(body.keys);
     }
     if (body.include) {
@@ -187,7 +187,7 @@ export class ClassesRouter extends PromiseRouter {
     if (body.count) {
       options.count = true;
     }
-    if (body.keys || body.keys === '') {
+    if (body.keys != null) {
       options.keys = String(body.keys);
     }
     if (body.excludeKeys) {
