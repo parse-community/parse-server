4f3ac7fc2ca4166de0a98ae360d481fa5d9a6e27
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2021-03-29 03:36:29 +0200

    adds deprecator

---
+++ spec/Deprecator.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Deprecator = require('../lib/Deprecator/Deprecator');
+
+describe('Deprecator', () => {
+  let deprecations = [];
+
+  beforeEach(async () => {
+    deprecations = [{ optionKey: 'exampleKey', changeNewDefault: 'exampleNewDefault' }];
+  });
+
+  it('deprecations are an array', async () => {
+    expect(Deprecator.getDeprecations()).toBeInstanceOf(Array);
+  });
+
+  it('logs deprecation for new default', async () => {
+    deprecations = [{ optionKey: 'exampleKey', changeNewDefault: 'exampleNewDefault' }];
+
+    spyOn(Deprecator, '_getDeprecations').and.callFake(() => deprecations);
+    const logger = require('../lib/logger').logger;
+    const logSpy = spyOn(logger, 'warn').and.callThrough();
+
+    await reconfigureServer();
+    expect(logSpy.calls.all()[0].args[0]).toContain(deprecations[0].optionKey);
+    expect(logSpy.calls.all()[0].args[0]).toContain(deprecations[0].changeNewDefault);
+  });
+
+  fit('does not log deprecation for new default if option is set manually', async () => {
+    deprecations = [{ optionKey: 'exampleKey', changeNewDefault: 'exampleNewDefault' }];
+
+    spyOn(Deprecator, '_getDeprecations').and.callFake(() => deprecations);
+    const logger = require('../lib/logger').logger;
+    const logSpy = spyOn(logger, 'warn').and.callThrough();
+
+    await reconfigureServer({ [deprecations[0].optionKey]: 'manuallySet' });
+    expect(logSpy).not.toHaveBeenCalled();
+  });
+});
