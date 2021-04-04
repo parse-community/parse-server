439f7ec95e1ef4b093d0a4329cfd96feb055a3d7
dblythy <daniel-blyth@live.com.au>
2021-04-05 00:16:03 +1000

    Update Deprecations.js

--- src/Deprecator/Deprecations.js
+++ src/Deprecator/Deprecations.js
@@ -2,13 +2,13 @@
  * The deprecations.
  *
  * Add deprecations to the array using the following keys:
- * - `optionKey`: The option key incl. its path, e.g. `security.enableCheck`.
- * - `envKey`: The environment key, e.g. `PARSE_SERVER_SECURITY`.
- * - `changeNewKey`: Set the new key name if the current key will be replaced,
+ * - `optionKey` {String}: The option key incl. its path, e.g. `security.enableCheck`.
+ * - `envKey` {String}: The environment key, e.g. `PARSE_SERVER_SECURITY`.
+ * - `changeNewKey` {String}: Set the new key name if the current key will be replaced,
  * or set to an empty string if the current key will be removed without replacement.
- * - `changeNewDefault`: Set the new default value if the key's default value
+ * - `changeNewDefault` {String}: Set the new default value if the key's default value
  * will change in a future version.
  *
  * If there are no deprecations this must return an empty array anyway.
  */
-module.exports = [{ optionKey: 'enforcePrivateUsers', changeNewDefault: true }];
+module.exports = [{ optionKey: 'enforcePrivateUsers', changeNewDefault: 'true' }];
