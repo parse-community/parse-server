8f1fd5d74f2bc5199d4d05ad2299ec654111119d
Samuel Denis-D'Ortun <sam@sddproductions.com>
2021-04-10 23:15:27 -0400

    Tests with flow-to-ts

---
+++ resources/flow-to-ts.js
@@ -0,0 +1,44 @@
+const glob = require("glob");
+const path = require("path");
+const fs = require("fs");
+const jsc = require("jscodeshift");
+const flowParser = require("jscodeshift/parser/flow");
+const {convert} = require('@khanacademy/flow-to-ts/dist/convert.bundle');
+
+const files = glob.sync("src/**/*.js", {
+  root: "./",
+  ignore: ["src/**/__tests__/*.js"]
+});
+
+files.forEach(file => {
+  console.log(`Processing: ${file}`)
+
+  const flowCode = fs.readFileSync(file, "utf-8");
+
+  const ast = jsc(flowCode, {
+    parser: flowParser()
+  });
+
+  ast
+    .find(jsc.GenericTypeAnnotation, {
+      id: { name: "TimeoutID" }
+    })
+    .replaceWith("number");
+
+  const transformedCode = ast.toSource();
+
+  const typescriptCode = convert(transformedCode, {
+    printWidth: 80,
+    singleQuote: true,
+    semi: false,
+    prettier: true,
+  })
+
+  const outFile = `./tmp-ts/${file.replace('.js', '.ts')}`
+  const outPath = path.dirname(outFile)
+  if(!fs.existsSync(outPath)){
+    fs.mkdirSync(outPath , {recursive:true})
+  }
+
+  fs.writeFileSync(outFile, typescriptCode, (err) => {})
+});
