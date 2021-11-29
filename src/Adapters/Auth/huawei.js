64206ecd0894c7bfca2e26acfdaf87a8aa595c4c
Kim ki seop <rltjqdl1138@naver.com>
2021-11-29 16:24:21 +0900

    Fix: Error message and etc for PR
    1. Run eslint
    eslint src/Adapters/Auth/huawei.js --fix
    
    2. Reformat error message
    3. Add new line to last of page

--- src/Adapters/Auth/huawei.js
+++ src/Adapters/Auth/huawei.js
@@ -3,8 +3,8 @@
 const https = require('https');
 var Parse = require('parse/node').Parse;
 
-const TOKEN_ISSUER = "https://accounts.huawei.com"
-const HUAWEI_URL=   "https://oauth-login.cloud.huawei.com/oauth2/v3/tokeninfo"
+const TOKEN_ISSUER = "https://accounts.huawei.com";
+const HUAWEI_URL =   "https://oauth-login.cloud.huawei.com/oauth2/v3/tokeninfo";
 
 
 function getDataFromHuawei(id_token){
@@ -15,7 +15,7 @@ function getDataFromHuawei(id_token){
         data += chunk.toString('utf8');
       });
       res.on('end', () => {
-        resolve( JSON.parse(data) );
+        resolve(JSON.parse(data));
       });
     }).on('error', reject);
   });
@@ -24,35 +24,34 @@ function getDataFromHuawei(id_token){
 
 async function verifyIdToken({ id_token: token, id }) {
   if (!token) {
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, `id token is missing`);
+    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, `id token is invalid for this user.`);
   }
   const huaweiData = await getDataFromHuawei(token);
   // Error from huawei
-  if(huaweiData.error){
+  if(!huaweiData || huaweiData.error){
     throw new Parse.Error(Parse.Error.OTHER_CAUSE,
-        huaweiData.error_description ? 
-          `${huaweiData.error_description} (${huaweiData.error}::${huaweiData.sub_error})` :
-          `undefined Error from huawei (${huaweiData.error}::${huaweiData.sub_error || "No SubError"}})`
-      )
+      huaweiData.error_description ?
+        `${huaweiData.error_description} (${huaweiData.error}::${huaweiData.sub_error})` :
+        `undefined Error from huawei (${huaweiData.error}::${huaweiData.sub_error || "No SubError"}})`
+    );
   }
-  const { sub, iss, exp, iat } = huaweiData
-  
+  const { sub, iss, exp, iat } = huaweiData;
+
   // Content Check
-  if( iss !== TOKEN_ISSUER )
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'id token not issued by correct provider')
-  if( id !== sub )
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'auth data is invalid for this user.')
-  
+  if(iss !== TOKEN_ISSUER)
+    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'ID token not issued by correct provider.');
+  if(id !== sub)
+    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'auth data is invalid for this user.');
+
   // Expiration Check
-  if(exp < Date.now()/1000)
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'expired token')
-  if(iat > Date.now()/1000)
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'id token not issued by correct provider or method')
+  if(exp < Date.now() / 1000)
+    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'ID token is expired');
+  if(iat > Date.now() / 1000)
+    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'ID token not issued by correct provider or method');
 } // Returns a promise that fulfills if this user id is valid.
 
 
-
-function validateAuthData(authData, options={}) {
+function validateAuthData(authData, options = {}) {
   return verifyIdToken(authData, options);
 }
 
