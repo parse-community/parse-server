f0e4c55b9dffdc37560fda1c3d0ee998b6592d52
Corey's iMac <coreyearleon@icloud.com>
2021-12-29 17:29:01 -0500

    fix gcenter method to act it's original way

--- spec/AuthenticationAdapters.spec.js
+++ spec/AuthenticationAdapters.spec.js
@@ -1704,7 +1704,7 @@ describe('Apple Game Center Auth adapter', () => {
       await gcenter.validateAuthData(authData);
       fail();
     } catch (e) {
-      expect(e.message).toBeDefined();
+      expect(e.message).toBe('Apple Game Center - invalid publicKeyUrl: invalid.com');
     }
   });
 });
