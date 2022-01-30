3a610a47812113b9cb9fbc3fbbb1e04f5a3daa7b
Antoine Cormouls <contact.antoine.cormouls@gmail.com>
2022-01-30 10:44:28 +0100

    fix: typo

--- src/Adapters/Files/GridStoreAdapter.js
+++ src/Adapters/Files/GridStoreAdapter.js
@@ -1,4 +1,4 @@
 // Note: GridStore was replaced by GridFSBucketAdapter by default in 2018 by @flovilmart
 throw new Error(
-  'GrideStoreAdapter: GrideStore is no longer supported by parse server and mongodb, use GridFSBucketAdapter instead.'
+  'GridStoreAdapter: GridStore is no longer supported by parse server and mongodb, use GridFSBucketAdapter instead.'
 );
