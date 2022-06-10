92ce1cd7a766bde2fddb1697057426da05d92c65
dblythy <daniel-blyth@live.com.au>
2022-06-10 12:49:37 +1000

    Update RestWrite.js

--- src/RestWrite.js
+++ src/RestWrite.js
@@ -1326,7 +1326,9 @@ RestWrite.prototype.runDatabaseOperation = function () {
 
   if (this.className === '_Role') {
     this.config.cacheController.role.clear();
-    this.config.liveQueryController.clearCachedRoles(this.auth.user);
+    if (this.config.liveQueryController) {
+      this.config.liveQueryController.clearCachedRoles(this.auth.user);
+    }
   }
 
   if (this.className === '_User' && this.query && this.auth.isUnauthenticated()) {
