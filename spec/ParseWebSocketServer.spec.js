cf209c2e5cfa1b83c40885ca29dc2bdb2b23fdf1
Layne Bernardo <lmbernar@uark.edu>
2022-06-16 18:41:01 -0500

    Implement ping/pong test
    Signed-off-by: Layne Bernardo <lmbernar@uark.edu>

--- spec/ParseWebSocketServer.spec.js
+++ spec/ParseWebSocketServer.spec.js
@@ -76,6 +76,31 @@ describe('ParseWebSocketServer', function () {
     expect(wssError).toBe('Invalid Packet');
   });
 
+  it('responds to pong', async () => {
+    const onConnectCallback = jasmine.createSpy('onConnectCallback');
+    const http = require('http');
+    const server = http.createServer();
+    const parseWebSocketServer = new ParseWebSocketServer(server, onConnectCallback, {
+      websocketTimeout: 5,
+    }).server;
+
+    const ws = new EventEmitter();
+    parseWebSocketServer.onConnection(ws);
+
+    // Make sure callback is called
+    expect(onConnectCallback).toHaveBeenCalled();
+    // Check that `waitingForPong` toggles
+    expect(ws.waitingForPong).toBe(false);
+    await new Promise(resolve => setTimeout(resolve, 10));
+    expect(ws.ping).toHaveBeenCalled();
+    expect(ws.waitingForPong).toBe(true);
+    ws.emit('pong');
+    expect(ws.waitingForPong).toBe(false);
+    await new Promise(resolve => setTimeout(resolve, 10));
+    expect(ws.terminate).not.toHaveBeenCalled();
+    server.close();
+  });
+
   it('closes interrupted connection', async () => {
     const onConnectCallback = jasmine.createSpy('onConnectCallback');
     const http = require('http');
@@ -87,6 +112,7 @@ describe('ParseWebSocketServer', function () {
     ws.readyState = 0;
     ws.OPEN = 0;
     ws.ping = jasmine.createSpy('ping');
+    ws.waitingForPong = false;
     ws.terminate = jasmine.createSpy('terminate');
 
     parseWebSocketServer.onConnection(ws);
@@ -95,6 +121,7 @@ describe('ParseWebSocketServer', function () {
     expect(onConnectCallback).toHaveBeenCalled();
     await new Promise(resolve => setTimeout(resolve, 10));
     expect(ws.ping).toHaveBeenCalled();
+    expect(ws.waitingForPong).toBe(true);
     await new Promise(resolve => setTimeout(resolve, 10));
     expect(ws.terminate).toHaveBeenCalled();
     server.close();
