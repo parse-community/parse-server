f6c9c0af05ee12049b8001a1cf798e64fb5eb8da
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2022-06-30 11:42:42 +0200

    fix 2

--- src/LiveQuery/ParseLiveQueryServer.js
+++ src/LiveQuery/ParseLiveQueryServer.js
@@ -18,7 +18,7 @@ import {
   toJSONwithObjects,
 } from '../triggers';
 import { getAuthForSessionToken, Auth } from '../Auth';
-import { getCacheController } from '../Controllers';
+import { getCacheController, getDatabaseController } from '../Controllers';
 import LRU from 'lru-cache';
 import UserRouter from '../Routers/UsersRouter';
 import DatabaseController from '../Controllers/DatabaseController';
@@ -567,6 +567,16 @@ class ParseLiveQueryServer {
       if (!obj) {
         return;
       }
+      let protectedFields = classLevelPermissions?.protectedFields || [];
+      if (!client.hasMasterKey && !Array.isArray(protectedFields)) {
+        protectedFields = getDatabaseController(this.config).addProtectedFields(
+          classLevelPermissions,
+          res.object.className,
+          query,
+          aclGroup,
+          clientAuth
+        );
+      }
       return DatabaseController.filterSensitiveData(
         client.hasMasterKey,
         aclGroup,
@@ -574,7 +584,7 @@ class ParseLiveQueryServer {
         op,
         classLevelPermissions,
         res.object.className,
-        classLevelPermissions?.protectedFields,
+        protectedFields,
         obj,
         query
       );
