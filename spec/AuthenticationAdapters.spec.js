46d1042d3338aeea91f796a35dbffa7a6360f2c8
dblythy <daniel-blyth@live.com.au>
2022-09-29 15:24:53 +1000

    Update AuthenticationAdapters.spec.js

--- spec/AuthenticationAdapters.spec.js
+++ spec/AuthenticationAdapters.spec.js
@@ -1867,7 +1867,7 @@ describe('Apple Game Center Auth adapter', () => {
     );
 
     const duration = new Date().getTime() - previous.getTime();
-    expect(duration <= 1).toBe(true);
+    expect(duration).toEqual(0);
   });
 
   it('adapter should throw', async () => {
