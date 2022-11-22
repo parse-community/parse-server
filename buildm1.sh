07f1a04045dfbf92a58e3249f060fdf4ceb32525
captyup <>
2022-11-22 14:41:58 +0800

    add @aws-sdk/s3-request-presigner

--- buildm1.sh
+++ buildm1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 967556865693.dkr.ecr.ap-southeast-1.amazonaws.com
 
-docker build --platform linux/amd64 -t 967556865693.dkr.ecr.ap-southeast-1.amazonaws.com/parse-server:0.22 .
-docker push 967556865693.dkr.ecr.ap-southeast-1.amazonaws.com/parse-server:0.22
-docker rmi 967556865693.dkr.ecr.ap-southeast-1.amazonaws.com/parse-server:0.22
+docker build --platform linux/amd64 -t 967556865693.dkr.ecr.ap-southeast-1.amazonaws.com/parse-server:0.23 .
+docker push 967556865693.dkr.ecr.ap-southeast-1.amazonaws.com/parse-server:0.23
+docker rmi 967556865693.dkr.ecr.ap-southeast-1.amazonaws.com/parse-server:0.23
