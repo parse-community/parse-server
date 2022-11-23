68c6a4c46135abdac26384efb5c83aa0bfbc0d21
dblythy <daniel-blyth@live.com.au>
2022-11-23 17:39:12 +1100

    fix: update LiveQuery publisher to redis 4

---
+++ spec/ParseLiveQueryRedis.spec.js
@@ -0,0 +1,33 @@
+if (process.env.PARSE_SERVER_TEST_CACHE === 'redis') {
+  describe('ParseLiveQuery redis', () => {
+    it('can connect', async () => {
+      await reconfigureServer({
+        startLiveQueryServer: true,
+        liveQuery: {
+          classNames: ['TestObject'],
+          redisURL: 'redis://localhost:6379',
+        },
+        liveQueryServerOptions: {
+          redisURL: 'redis://localhost:6379',
+        },
+      });
+      const subscription = await new Parse.Query('TestObject').subscribe();
+      const [, object] = await Promise.all([
+        new Promise(resolve =>
+          subscription.on('create', () => {
+            resolve();
+          })
+        ),
+        new Parse.Object('TestObject').save(),
+      ]);
+      await Promise.all([
+        new Promise(resolve =>
+          subscription.on('delete', () => {
+            resolve();
+          })
+        ),
+        object.destroy(),
+      ]);
+    });
+  });
+}
