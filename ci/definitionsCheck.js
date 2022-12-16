c79ce0da0bf6507958dc15e62cff68f9e483deed
dblythy <daniel-blyth@live.com.au>
2022-12-16 12:10:11 +1100

    Update definitionsCheck.js

--- ci/definitionsCheck.js
+++ ci/definitionsCheck.js
@@ -2,23 +2,23 @@ const fs = require('fs').promises;
 const { exec } = require('child_process');
 const core = require('@actions/core');
 (async () => {
-  const dir = await fs.readdir('./src');
+  const dir = await fs.readdir('./src/options');
   console.log(dir);
   const [currentDefinitions, currentDocs] = await Promise.all([
-    fs.readFile('./options/Definitions.js', 'utf8'),
-    fs.readFile('./options/Docs.js', 'utf8'),
+    fs.readFile('./src/options/Definitions.js', 'utf8'),
+    fs.readFile('./src/options/Docs.js', 'utf8'),
   ]);
   await exec('npm run definitions');
   const [newDefinitions, newDocs] = await Promise.all([
-    fs.readFile('./options/Definitions.js', 'utf8'),
-    fs.readFile('./options/Docs.js', 'utf8'),
+    fs.readFile('./src/options/Definitions.js', 'utf8'),
+    fs.readFile('./src/options/Docs.js', 'utf8'),
   ]);
   if (currentDefinitions !== newDefinitions || currentDocs !== newDocs) {
     console.error(
       '\x1b[31m%s\x1b[0m',
       'Definitions files cannot be updated manually. Please update index.js then run npm run definitions to generate definitions.'
     );
-    core.setFailed('Definitions files cannot be updated manually. Please update index.js then run npm run definitions to generate definitions.');
+    core.error('Definitions files cannot be updated manually. Please update index.js then run npm run definitions to generate definitions.');
     process.exit(1);
   } else {
     process.exit(0);
