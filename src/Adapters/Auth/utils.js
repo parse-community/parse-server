6b49521420834a70e57dc0cfc3e82c85bffe8b74
dblythy <daniel-blyth@live.com.au>
2023-02-04 12:27:59 +1100

    Update utils.js

--- src/Adapters/Auth/utils.js
+++ src/Adapters/Auth/utils.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const Parse = require('parse/node').Parse;
 const getHeaderFromToken = token => {
-  const decodedToken = jwt.verify(token, { complete: true });
+  const decodedToken = jwt.decode(token, { complete: true });
   if (!decodedToken) {
     throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, `provided token does not decode as JWT`);
   }
