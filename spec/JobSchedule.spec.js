1fbd50cf62b2a3a3d43da11519319b3789b8ece9
dblythy <daniel-blyth@live.com.au>
2023-03-08 13:47:19 +1100

    Update JobSchedule.spec.js

--- spec/JobSchedule.spec.js
+++ spec/JobSchedule.spec.js
@@ -21,31 +21,6 @@ const masterKeyOptions = {
 };
 
 describe('JobSchedule', () => {
-  // beforeEach(async () => {
-  //   const jobs = await request(
-  //     Object.assign(
-  //       {
-  //         url: Parse.serverURL + '/cloud_code/jobs',
-  //       },
-  //       masterKeyOptions
-  //     )
-  //   );
-  //   if (jobs.data.length) {
-  //     await Promise.all(
-  //       jobs.data.map(({ objectId }) =>
-  //         request(
-  //           Object.assign(
-  //             {
-  //               method: 'DELETE',
-  //               url: `${Parse.serverURL}/cloud_code/jobs/${objectId}`,
-  //             },
-  //             masterKeyOptions
-  //           )
-  //         )
-  //       )
-  //     );
-  //   }
-  // });
   it('should create _JobSchedule with masterKey', done => {
     const jobSchedule = new Parse.Object('_JobSchedule');
     jobSchedule.set({
