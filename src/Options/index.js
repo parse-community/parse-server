6f885d36b94902fdfea873fc554dee83589e6029
Daniel <daniel-blyth@live.com.au>
2023-05-18 03:49:25 +1000

    feat: `extendSessionOnUse` to automatically renew Parse Sessions (#8505)

--- src/Options/index.js
+++ src/Options/index.js
@@ -203,6 +203,9 @@ export interface ParseServerOptions {
   /* Session duration, in seconds, defaults to 1 year
   :DEFAULT: 31536000 */
   sessionLength: ?number;
+  /* Whether Parse Server should automatically extend a valid session by the sessionLength
+  :DEFAULT: false */
+  extendSessionOnUse: ?boolean;
   /* Default value for limit option on queries, defaults to `100`.
   :DEFAULT: 100 */
   defaultLimit: ?number;
