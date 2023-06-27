29c98785c3ca354ef1aa02982e57be9a37577c26
dblythy <daniel-blyth@live.com.au>
2023-06-27 17:26:43 +1000

    Update MongoStorageAdapter.js

--- src/Adapters/Storage/Mongo/MongoStorageAdapter.js
+++ src/Adapters/Storage/Mongo/MongoStorageAdapter.js
@@ -282,7 +282,6 @@ export class MongoStorageAdapter implements StorageAdapter {
         indexOptions = field._options;
         delete field._options;
       }
-      console.log(existingIndexes[name], field);
       if (existingIndexes[name] && field.__op !== 'Delete') {
         throw new Parse.Error(Parse.Error.INVALID_QUERY, `Index ${name} exists, cannot update.`);
       }
