3f03bd3c6f27cc1edb7174d06645f517830790fd
Corey <coreyearleon@icloud.com>
2023-06-29 19:15:58 -0400

    refactor: Remove duplicate user index creation (#8662)

--- src/Controllers/DatabaseController.js
+++ src/Controllers/DatabaseController.js
@@ -1722,12 +1722,6 @@ class DatabaseController {
         logger.warn('Unable to create case insensitive username index: ', error);
         throw error;
       });
-    await this.adapter
-      .ensureIndex('_User', requiredUserFields, ['username'], 'case_insensitive_username', true)
-      .catch(error => {
-        logger.warn('Unable to create case insensitive username index: ', error);
-        throw error;
-      });
 
     await this.adapter.ensureUniqueness('_User', requiredUserFields, ['email']).catch(error => {
       logger.warn('Unable to ensure uniqueness for user email addresses: ', error);
