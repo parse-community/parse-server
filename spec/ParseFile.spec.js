f5e4205719b2f26f54cb16b4708744b99d5bc00a
Bartosz Marganiec <bartekm@post.pl>
2023-07-17 18:51:29 +0200

    Add wav file to test case

--- spec/ParseFile.spec.js
+++ spec/ParseFile.spec.js
@@ -1368,7 +1368,7 @@ describe('Parse.File testing', () => {
       await reconfigureServer({
         fileUpload: {
           enableForPublic: true,
-          fileExtensions: ['jpg', 'any'],
+          fileExtensions: ['jpg', 'wav'],
         },
       });
       await expectAsync(
@@ -1396,6 +1396,18 @@ describe('Parse.File testing', () => {
             _JavaScriptKey: 'test',
             _ContentType: 'image/jpg',
             base64: 'PGh0bWw+PC9odG1sPgo=',
+          }),
+        })
+      ).toBeResolved();
+      await expectAsync(
+        request({
+          method: 'POST',
+          url: 'http://localhost:8378/1/files/file',
+          body: JSON.stringify({
+            _ApplicationId: 'test',
+            _JavaScriptKey: 'test',
+            _ContentType: 'audio/wav',
+            base64: 'UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA',
           }),
         })
       ).toBeResolved();
