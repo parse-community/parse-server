b7ff5aba245e2b4c0d5bcf74e3775709be503fa7
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2024-07-09 02:08:58 +0200

    pass excluded packages as list

--- ci/uninstallDevDeps.sh
+++ ci/uninstallDevDeps.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# List of dev dependencies that are required by the Node Engine Check script
-exclusionList=(
-  "@actions/core"
-)
+# Read package exclusion list from arguments
+exclusionList=("$@")
 
 # Convert exclusion list to grep pattern
 exclusionPattern=$(printf "|%s" "${exclusionList[@]}")
-exclusionPattern=${exclusionPattern:1} # Remove leading pipe character
+exclusionPattern=${exclusionPattern:1}
 
 # Get list of all dev dependencies
 devDeps=$(jq -r '.devDependencies | keys | .[]' package.json)
@@ -15,7 +13,7 @@ devDeps=$(jq -r '.devDependencies | keys | .[]' package.json)
 # Filter out exclusion list
 depsToUninstall=$(echo "$devDeps" | grep -Ev "$exclusionPattern")
 
-# If there are dependencies to uninstall, uninstall them
+# If there are dependencies to uninstall then uninstall them
 if [ -n "$depsToUninstall" ]; then
   echo "Uninstalling dev dependencies: $depsToUninstall"
   npm uninstall $depsToUninstall
