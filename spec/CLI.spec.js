3d8f9c06e43819ace153a2ba7a183aed6ba95293
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2024-07-09 11:35:55 +0200

    Update CLI.spec.js

--- spec/CLI.spec.js
+++ spec/CLI.spec.js
@@ -220,7 +220,7 @@ fdescribe('execution', () => {
 
   it('should start Parse Server', done => {
     const env = { ...process.env };
-    env.NODE_OPTIONS = '--dns-result-order=ipv4first';
+    env.NODE_OPTIONS = '--dns-result-order=ipv4first --trace-deprecation';
     childProcess = spawn(
       binPath,
       ['--appId', 'test', '--masterKey', 'test', '--databaseURI', databaseURI, '--port', '1339'],
@@ -239,7 +239,7 @@ fdescribe('execution', () => {
 
   it('should start Parse Server with GraphQL', async done => {
     const env = { ...process.env };
-    env.NODE_OPTIONS = '--dns-result-order=ipv4first';
+    env.NODE_OPTIONS = '--dns-result-order=ipv4first --trace-deprecation';
     childProcess = spawn(
       binPath,
       [
@@ -271,7 +271,7 @@ fdescribe('execution', () => {
 
   it('should start Parse Server with GraphQL and Playground', async done => {
     const env = { ...process.env };
-    env.NODE_OPTIONS = '--dns-result-order=ipv4first';
+    env.NODE_OPTIONS = '--dns-result-order=ipv4first --trace-deprecation';
     childProcess = spawn(
       binPath,
       [
@@ -305,7 +305,7 @@ fdescribe('execution', () => {
 
   it('can start Parse Server with auth via CLI', done => {
     const env = { ...process.env };
-    env.NODE_OPTIONS = '--dns-result-order=ipv4first';
+    env.NODE_OPTIONS = '--dns-result-order=ipv4first --trace-deprecation';
     childProcess = spawn(
       binPath,
       ['--databaseURI', databaseURI, './spec/configs/CLIConfigAuth.json'],
