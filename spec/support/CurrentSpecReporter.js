4a1f227542b7361cb9ba39153bd82d7d1877422a
Manuel <5673677+mtrezza@users.noreply.github.com>
2024-07-09 13:13:11 +0200

    Update spec/support/CurrentSpecReporter.js
    Signed-off-by: Manuel <5673677+mtrezza@users.noreply.github.com>

--- spec/support/CurrentSpecReporter.js
+++ spec/support/CurrentSpecReporter.js
@@ -5,6 +5,8 @@ global.currentSpec = null;
 
 const timerMap = {};
 const duplicates = [];
+/** The min. execution time in seconds for a test to be considered slow. */
+const slowTestLimit = 2;
 
 class CurrentSpecReporter {
   specStarted(spec) {
