92b5ec59b1e181139b2a0ab13d0a807f12bf428d
Aidan Daly <74743624+dalyaidan1@users.noreply.github.com>
2024-08-18 17:20:00 -0400

    fix: actually upload the full files

--- spec/FilesRouter.spec.js
+++ spec/FilesRouter.spec.js
@@ -3,12 +3,20 @@ const path = require('path');
 
 describe('FilesRouter', () => {
   describe('File Uploads', () => {
+
+    beforeEach(async () => {
+      // Set the maxUploadSize to 1GB
+      await reconfigureServer({
+        maxUploadSize: '1GB',
+      });
+    });
+
     const V8_STRING_LIMIT_BYTES = 536_870_912;
 
     /**
      * Quick helper function to upload the file to the server via the REST API
      * We do this because creating a Parse.File object with a file over 512MB
-     * will try to use the Web API FileReader API, which will fail the test
+     * will try to use the Web FileReader API, which will fail the test
      *
      * @param {string} fileName the name of the file
      * @param {string} filePath the path to the file locally
@@ -22,24 +30,22 @@ describe('FilesRouter', () => {
         'Content-Type': 'multipart/form-data',
       };
 
-      // Create a FormData object to send the file
-      const formData = new FormData();
-      formData.append('file', fs.createReadStream(filePath));
+      const fileStream = fs.createReadStream(filePath);
 
       // Send the request
       const response = await fetch(url, {
         method: 'POST',
         headers,
-        body: formData,
+        body: fileStream,
+        duplex: 'half' // This is required to send a stream
       });
 
       return response;
     };
 
-
     it('should allow Parse.File uploads under 512MB', async done => {
       const filePath = path.join(__dirname, 'file.txt');
-      fs.writeFileSync(filePath, Buffer.alloc(1024 * 1024));
+      await fs.promises.writeFile(filePath, Buffer.alloc(1024 * 1024));
 
       const response = await postFile('file.txt', filePath);
       expect(response.ok).toBe(true);
@@ -50,7 +56,7 @@ describe('FilesRouter', () => {
 
     it('should allow Parse.File uploads exactly 512MB', async done => {
       const filePath = path.join(__dirname, 'file.txt');
-      fs.writeFileSync(filePath, Buffer.alloc(V8_STRING_LIMIT_BYTES));
+      await fs.promises.writeFile(filePath, Buffer.alloc(V8_STRING_LIMIT_BYTES));
 
       const response = await postFile('file.txt', filePath);
       expect(response.ok).toBe(true);
@@ -61,7 +67,7 @@ describe('FilesRouter', () => {
 
     it('should allow Parse.File uploads over 512MB', async done => {
       const filePath = path.join(__dirname, 'file.txt');
-      fs.writeFileSync(filePath, Buffer.alloc(V8_STRING_LIMIT_BYTES + 50 * 1024 * 1024));
+      await fs.promises.writeFile(filePath, Buffer.alloc(V8_STRING_LIMIT_BYTES + 50 * 1024 * 1024));
 
       const response = await postFile('file.txt', filePath);
       expect(response.ok).toBe(true);
