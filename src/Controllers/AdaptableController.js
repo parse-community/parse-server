1eb2780fe8e4ceca2ce3984b426a0fff0a9276cc
Daniel <daniel-blyth@live.com.au>
2025-01-10 12:36:56 +1100

    Update AdaptableController.js

--- src/Controllers/AdaptableController.js
+++ src/Controllers/AdaptableController.js
@@ -60,7 +60,6 @@ export class AdaptableController {
     }, {});
 
     if (Object.keys(mismatches).length > 0) {
-      console.error("Adapter prototype don't match expected prototype", mismatches);
       throw new Error("Adapter prototype don't match expected prototype", adapter, mismatches);
     }
   }
