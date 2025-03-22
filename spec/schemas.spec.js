496ad4c6a85a25c9d783b8b675e59ec0d4dc4973
Daniel <daniel-blyth@live.com.au>
2025-03-22 20:21:14 +1100

    Update schemas.spec.js

--- spec/schemas.spec.js
+++ spec/schemas.spec.js
@@ -2064,7 +2064,7 @@ describe('schemas', () => {
       },
     }).then(fail, response => {
       expect(response.data.error).toEqual(
-        "'1' is not a valid value for class level permissions find:*:1"
+        "'1' is not a valid value for class level permissions acl find:*"
       );
       done();
     });
