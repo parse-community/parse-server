27f2eb8bcbed67650769168018099a6b610d890f
Rahul Lanjewar <rahullanjewar93@gmail.com>
2025-09-09 20:25:30 +0530

    fix: minor

--- src/SchemaMigrations/Migrations.js
+++ src/SchemaMigrations/Migrations.js
@@ -6,6 +6,7 @@ export interface SchemaOptions {
   deleteExtraFields: ?boolean;
   recreateModifiedFields: ?boolean;
   lockSchemas: ?boolean;
+  dropUnknownIndexes: ?boolean;
   beforeMigration: ?() => void | Promise<void>;
   afterMigration: ?() => void | Promise<void>;
 }
