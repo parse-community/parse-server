02c2eaa6d4424e335d49cc7408c31ea111812759
coratgerl <73360179+coratgerl@users.noreply.github.com>
2025-10-27 17:53:59 +0100

    fix: revie

--- spec/ParseQuery.spec.js
+++ spec/ParseQuery.spec.js
@@ -8,6 +8,7 @@ const Parse = require('parse/node');
 const request = require('../lib/request');
 const ParseServerRESTController = require('../lib/ParseServerRESTController').ParseServerRESTController;
 const ParseServer = require('../lib/ParseServer').default;
+import Deprecator from '../lib/Deprecator/Deprecator';
 
 const masterKeyHeaders = {
   'X-Parse-Application-Id': 'test',
