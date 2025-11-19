7d436c96a9e8b03f429b49b1a00197004934be74
Manuel <5673677+mtrezza@users.noreply.github.com>
2025-11-19 13:46:19 +0100

    docs
    Signed-off-by: Manuel <5673677+mtrezza@users.noreply.github.com>

--- src/Routers/UsersRouter.js
+++ src/Routers/UsersRouter.js
@@ -483,7 +483,7 @@ export class UsersRouter extends ClassesRouter {
 
     if (userData) {
       this._sanitizeAuthData(userData);
-      // Useful to get User attached files in the trigger (photo picture for example)
+      // Get files attached to user
       await req.config.filesController.expandFilesInObject(req.config, userData);
 
       const user = inflate('_User', userData);
