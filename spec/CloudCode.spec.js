6418ad1ea0294ffd6ac7029277f607b6fca49454
Lucas Coratger <73360179+coratgerl@users.noreply.github.com>
2025-11-19 13:53:27 +0100

    feat: add comment on test

--- spec/CloudCode.spec.js
+++ spec/CloudCode.spec.js
@@ -4784,6 +4784,7 @@ describe('beforePasswordResetRequest hook', () => {
     await user.signUp();
     const base64 = 'V29ya2luZyBhdCBQYXJzZSBpcyBncmVhdCE=';
     const file = new Parse.File('myfile.txt', { base64 });
+    // Test that the hook not throw Save non encoded file
     await file.save();
     await user.save({ file });
 
