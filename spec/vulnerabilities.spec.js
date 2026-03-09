25804571cb4e1f4f0ee6a32661850cfeb5227e9c
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2026-03-09 21:08:54 +0000

    fix

--- spec/vulnerabilities.spec.js
+++ spec/vulnerabilities.spec.js
@@ -1021,6 +1021,168 @@ describe('(GHSA-qpr4-jrj4-6f27) SQL Injection via sort dot-notation field name',
   });
 });
 
+describe('(GHSA-v5hf-f4c3-m5rv) Stored XSS via .svgz, .xht, .xml, .xsl, .xslt file upload', () => {
+  const headers = {
+    'X-Parse-Application-Id': 'test',
+    'X-Parse-REST-API-Key': 'rest',
+  };
+
+  beforeEach(async () => {
+    await reconfigureServer({
+      fileUpload: {
+        enableForPublic: true,
+      },
+    });
+  });
+
+  it('blocks .svgz file upload by default', async () => {
+    const svgContent = Buffer.from(
+      '<svg xmlns="http://www.w3.org/2000/svg"><script>alert(1)</script></svg>'
+    ).toString('base64');
+    for (const extension of ['svgz', 'SVGZ', 'Svgz']) {
+      await expectAsync(
+        request({
+          method: 'POST',
+          headers,
+          url: `http://localhost:8378/1/files/malicious.${extension}`,
+          body: JSON.stringify({
+            _ApplicationId: 'test',
+            _JavaScriptKey: 'test',
+            _ContentType: 'image/svg+xml',
+            base64: svgContent,
+          }),
+        }).catch(e => {
+          throw new Error(e.data.error);
+        })
+      ).toBeRejectedWith(
+        new Parse.Error(
+          Parse.Error.FILE_SAVE_ERROR,
+          `File upload of extension ${extension} is disabled.`
+        )
+      );
+    }
+  });
+
+  it('blocks .xht file upload by default', async () => {
+    const xhtContent = Buffer.from(
+      '<?xml version="1.0"?><html xmlns="http://www.w3.org/1999/xhtml"><body><script>alert(1)</script></body></html>'
+    ).toString('base64');
+    for (const extension of ['xht', 'XHT', 'Xht']) {
+      await expectAsync(
+        request({
+          method: 'POST',
+          headers,
+          url: `http://localhost:8378/1/files/malicious.${extension}`,
+          body: JSON.stringify({
+            _ApplicationId: 'test',
+            _JavaScriptKey: 'test',
+            _ContentType: 'application/xhtml+xml',
+            base64: xhtContent,
+          }),
+        }).catch(e => {
+          throw new Error(e.data.error);
+        })
+      ).toBeRejectedWith(
+        new Parse.Error(
+          Parse.Error.FILE_SAVE_ERROR,
+          `File upload of extension ${extension} is disabled.`
+        )
+      );
+    }
+  });
+
+  it('blocks .xml file upload by default', async () => {
+    const xmlContent = Buffer.from(
+      '<?xml version="1.0"?><root><data>test</data></root>'
+    ).toString('base64');
+    for (const extension of ['xml', 'XML', 'Xml']) {
+      await expectAsync(
+        request({
+          method: 'POST',
+          headers,
+          url: `http://localhost:8378/1/files/malicious.${extension}`,
+          body: JSON.stringify({
+            _ApplicationId: 'test',
+            _JavaScriptKey: 'test',
+            _ContentType: 'application/xml',
+            base64: xmlContent,
+          }),
+        }).catch(e => {
+          throw new Error(e.data.error);
+        })
+      ).toBeRejectedWith(
+        new Parse.Error(
+          Parse.Error.FILE_SAVE_ERROR,
+          `File upload of extension ${extension} is disabled.`
+        )
+      );
+    }
+  });
+
+  it('blocks .xsl file upload by default', async () => {
+    const xslContent = Buffer.from(
+      '<?xml version="1.0"?><xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0"></xsl:stylesheet>'
+    ).toString('base64');
+    for (const extension of ['xsl', 'XSL', 'Xsl']) {
+      await expectAsync(
+        request({
+          method: 'POST',
+          headers,
+          url: `http://localhost:8378/1/files/malicious.${extension}`,
+          body: JSON.stringify({
+            _ApplicationId: 'test',
+            _JavaScriptKey: 'test',
+            _ContentType: 'application/xml',
+            base64: xslContent,
+          }),
+        }).catch(e => {
+          throw new Error(e.data.error);
+        })
+      ).toBeRejectedWith(
+        new Parse.Error(
+          Parse.Error.FILE_SAVE_ERROR,
+          `File upload of extension ${extension} is disabled.`
+        )
+      );
+    }
+  });
+
+  it('blocks .xslt file upload by default', async () => {
+    const xsltContent = Buffer.from(
+      '<?xml version="1.0"?><xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0"></xsl:stylesheet>'
+    ).toString('base64');
+    for (const extension of ['xslt', 'XSLT', 'Xslt']) {
+      await expectAsync(
+        request({
+          method: 'POST',
+          headers,
+          url: `http://localhost:8378/1/files/malicious.${extension}`,
+          body: JSON.stringify({
+            _ApplicationId: 'test',
+            _JavaScriptKey: 'test',
+            _ContentType: 'application/xslt+xml',
+            base64: xsltContent,
+          }),
+        }).catch(e => {
+          throw new Error(e.data.error);
+        })
+      ).toBeRejectedWith(
+        new Parse.Error(
+          Parse.Error.FILE_SAVE_ERROR,
+          `File upload of extension ${extension} is disabled.`
+        )
+      );
+    }
+  });
+
+  it('still allows common file types', async () => {
+    for (const type of ['txt', 'png', 'jpg', 'gif', 'pdf', 'doc']) {
+      const file = new Parse.File(`file.${type}`, { base64: 'ParseA==' });
+      await file.save();
+    }
+  });
+});
+
 describe('(GHSA-3jmq-rrxf-gqrg) Stored XSS via file serving', () => {
   it('sets X-Content-Type-Options: nosniff on file GET response', async () => {
     const file = new Parse.File('hello.txt', [1, 2, 3], 'text/plain');
