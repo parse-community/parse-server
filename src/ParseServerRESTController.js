0e56fdf545010c4b4fc21c83ac7d8239cc41730d
Yogendra Singh <yog27ray@gmail.com>
2026-04-16 09:01:31 +0530

    perf: replace JSON.parse(JSON.stringify()) with in-place stripUndefined
    Avoid full deep-copy on every request/response by mutating in-place.
    This eliminates unnecessary allocations and GC pressure for the common
    case where no undefined values exist.

--- src/ParseServerRESTController.js
+++ src/ParseServerRESTController.js
@@ -29,6 +29,30 @@ function getAuth(options = {}, config) {
   });
 }
 
+function stripUndefined(obj) {
+  if (obj === null || obj === undefined || typeof obj !== 'object') {
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i] === undefined) {
+        obj[i] = null;
+      } else {
+        stripUndefined(obj[i]);
+      }
+    }
+    return obj;
+  }
+  for (const key of Object.keys(obj)) {
+    if (obj[key] === undefined) {
+      delete obj[key];
+    } else {
+      stripUndefined(obj[key]);
+    }
+  }
+  return obj;
+}
+
 function ParseServerRESTController(applicationId, router) {
   function handleRequest(method, path, data = {}, options = {}, config) {
     // Store the arguments, for later use if internal fails
@@ -125,7 +149,7 @@ function ParseServerRESTController(applicationId, router) {
       }
       getAuth(options, config).then(auth => {
         const request = {
-          body: JSON.parse(JSON.stringify(data)),
+          body: stripUndefined(data),
           config,
           auth,
           info: {
@@ -134,7 +158,7 @@ function ParseServerRESTController(applicationId, router) {
             installationId: options.installationId,
             context: requestContext,
           },
-          query: query ? JSON.parse(JSON.stringify(query)) : query,
+          query: query ? stripUndefined(query) : query,
         };
         return Promise.resolve()
           .then(() => {
@@ -143,7 +167,7 @@ function ParseServerRESTController(applicationId, router) {
           .then(
             resp => {
               const { response, status, headers = {} } = resp;
-              const strippedResponse = JSON.parse(JSON.stringify(response));
+              const strippedResponse = stripUndefined(response);
               if (options.returnStatus) {
                 resolve({ ...strippedResponse, _status: status, _headers: headers });
               } else {
