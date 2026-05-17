56c159ec962d729df09ccaa5cc2537751511e375
Manuel <5673677+mtrezza@users.noreply.github.com>
2026-05-17 15:11:10 +0100

    fix: Pre-authentication denial of service via client version header regex backtracking ([GHSA-38m6-82c8-4xfm](https://github.com/parse-community/parse-server/security/advisories/GHSA-38m6-82c8-4xfm)) (#10463)

--- src/Routers/ClassesRouter.js
+++ src/Routers/ClassesRouter.js
@@ -43,7 +43,6 @@ export class ClassesRouter extends PromiseRouter {
         this.className(req),
         body.where,
         options,
-        req.info.clientSDK,
         req.info.context
       )
       .then(response => {
@@ -88,7 +87,6 @@ export class ClassesRouter extends PromiseRouter {
         this.className(req),
         req.params.objectId,
         options,
-        req.info.clientSDK,
         req.info.context
       )
       .then(response => {
@@ -123,7 +121,6 @@ export class ClassesRouter extends PromiseRouter {
       req.auth,
       this.className(req),
       req.body || {},
-      req.info.clientSDK,
       req.info.context
     );
   }
@@ -136,7 +133,6 @@ export class ClassesRouter extends PromiseRouter {
       this.className(req),
       where,
       req.body || {},
-      req.info.clientSDK,
       req.info.context
     );
   }
--- src/rest.js
+++ src/rest.js
@@ -31,7 +31,6 @@ async function runFindTriggers(
   className,
   restWhere,
   restOptions,
-  clientSDK,
   context,
   options = {}
 ) {
@@ -90,7 +89,6 @@ async function runFindTriggers(
           className,
           restWhere: refilterWhere,
           restOptions,
-          clientSDK,
           context,
           runBeforeFind: false,
           runAfterFind: false,
@@ -126,7 +124,6 @@ async function runFindTriggers(
     className,
     restWhere,
     restOptions,
-    clientSDK,
     context,
     runBeforeFind: false,
   });
@@ -135,7 +132,7 @@ async function runFindTriggers(
 }
 
 // Returns a promise for an object with optional keys 'results' and 'count'.
-const find = async (config, auth, className, restWhere, restOptions, clientSDK, context) => {
+const find = async (config, auth, className, restWhere, restOptions, context) => {
   enforceRoleSecurity('find', className, auth, config);
   return runFindTriggers(
     config,
@@ -143,14 +140,13 @@ const find = async (config, auth, className, restWhere, restOptions, clientSDK, 
     className,
     restWhere,
     restOptions,
-    clientSDK,
     context,
     { isGet: false }
   );
 };
 
 // get is just like find but only queries an objectId.
-const get = async (config, auth, className, objectId, restOptions, clientSDK, context) => {
+const get = async (config, auth, className, objectId, restOptions, context) => {
   enforceRoleSecurity('get', className, auth, config);
   return runFindTriggers(
     config,
@@ -158,7 +154,6 @@ const get = async (config, auth, className, objectId, restOptions, clientSDK, co
     className,
     { objectId },
     restOptions,
-    clientSDK,
     context,
     { isGet: true }
   );
@@ -264,16 +259,16 @@ function del(config, auth, className, objectId, context) {
 }
 
 // Returns a promise for a {response, status, location} object.
-function create(config, auth, className, restObject, clientSDK, context) {
+function create(config, auth, className, restObject, context) {
   enforceRoleSecurity('create', className, auth, config);
-  var write = new RestWrite(config, auth, className, null, restObject, null, clientSDK, context);
+  var write = new RestWrite(config, auth, className, null, restObject, null, context);
   return write.execute();
 }
 
 // Returns a promise that contains the fields of the update that the
 // REST API is supposed to return.
 // Usually, this is just updatedAt.
-function update(config, auth, className, restWhere, restObject, clientSDK, context) {
+function update(config, auth, className, restWhere, restObject, context) {
   enforceRoleSecurity('update', className, auth, config);
 
   return Promise.resolve()
@@ -313,7 +308,6 @@ function update(config, auth, className, restWhere, restObject, clientSDK, conte
         restWhere,
         restObject,
         originalRestObject,
-        clientSDK,
         context,
         'update'
       ).execute();
