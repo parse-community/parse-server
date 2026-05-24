8a17e2e32841193b4210649251a94829f9b6112f
Aidan Daly <74743624+dalyaidan1@users.noreply.github.com>
2026-05-24 10:30:51 -0400

    fix: validator callback types

--- src/Options/index.js
+++ src/Options/index.js
@@ -664,11 +664,10 @@ export interface PasswordPolicyOptions {
   <br><br>
   If used in combination with `validatorCallback`, the password must pass both to be accepted. */
   validatorPattern: ?string;
-  /*   */
   /* Set a callback function to validate a password to be accepted.
   <br><br>
   If used in combination with `validatorPattern`, the password must pass both to be accepted. */
-  validatorCallback: ?() => void;
+  validatorCallback: ?(password: string) => boolean;
   /* Set the error message to be sent.
   <br><br>
   Default is `Password does not meet the Password Policy requirements.` */
